5efd34d94d23dea01c5c993e67c30f41863d6b3a
= <andrew@scichart.com>
2020-08-10 11:00:20 +0100

    Added tutorial #1 - Create a SciChartSurface in JavaScript

---
+++ Tutorials/Tutorial 1 - Setting up a project with SciChart/src/index.js
@@ -0,0 +1,14 @@
+import {SciChartSurface} from "scichart/charting/Visuals/SciChartSurface";
+import {NumericAxis} from "scichart/charting/Visuals/Axis/NumericAxis";
+
+async function initSciChart() {
+    const {sciChartSurface, wasmContext} = await SciChartSurface.create("scichart-root");
+
+    const xAxis = new NumericAxis(wasmContext);
+    const yAxis = new NumericAxis(wasmContext);
+    
+    sciChartSurface.xAxes.add(xAxis);
+    sciChartSurface.yAxes.add(yAxis);
+}
+
+initSciChart();
