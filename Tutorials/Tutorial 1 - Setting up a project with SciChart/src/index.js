ac87202e0a7fcd2b3d5d319d667008adc8b090e6
= <andrew@scichart.com>
2020-08-10 11:18:17 +0100

    Just comments

--- Tutorials/Tutorial 1 - Setting up a project with SciChart/src/index.js
+++ Tutorials/Tutorial 1 - Setting up a project with SciChart/src/index.js
@@ -2,13 +2,19 @@ import {SciChartSurface} from "scichart/charting/Visuals/SciChartSurface";
 import {NumericAxis} from "scichart/charting/Visuals/Axis/NumericAxis";
 
 async function initSciChart() {
+    // Create the SciChartSurface in the div 'scichart-root'
+    // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
+    // instance must be passed to other types that exist on the same surface.
     const {sciChartSurface, wasmContext} = await SciChartSurface.create("scichart-root");
 
+    // Create an X,Y Axis and add to the chart
     const xAxis = new NumericAxis(wasmContext);
     const yAxis = new NumericAxis(wasmContext);
     
     sciChartSurface.xAxes.add(xAxis);
     sciChartSurface.yAxes.add(yAxis);
+
+    // That's it! You just created your first SciChartSurface!
 }
 
 initSciChart();
