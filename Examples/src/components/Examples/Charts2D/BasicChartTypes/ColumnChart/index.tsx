eec19a397c17ce29d9dfc66975705019201f8410
= <andrew@scichart.com>
2020-09-06 21:14:20 +0100

    Update index.tsx

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/ColumnChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/ColumnChart/index.tsx
@@ -15,6 +15,8 @@ import {
 import { IRenderableSeries } from "scichart/Charting/Visuals/RenderableSeries/IRenderableSeries";
 import { parseColorToUIntArgb } from "scichart/utils/parseColor";
 import {ExampleDataProvider} from "../../../ExampleDataProvider";
+import {EllipsePointMarker} from "scichart/Charting/Visuals/PointMarkers/EllipsePointMarker";
+import {NumberRange} from "scichart/Core/NumberRange";
 
 const divElementId = "chart";
 
@@ -24,7 +26,7 @@ const drawExample = async () => {
 
     // Add an X, Y Axis
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
-    sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { autoRange: EAutoRange.Always }));
+    sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { growBy: new NumberRange(0, 0.1) }));
 
     const yValues: number[] = [0.1, 0.2, 0.4, 0.8, 1.1, 1.5, 2.4, 4.6, 8.1, 11.7, 14.4,
         16.0, 13.7, 10.1, 6.4, 3.5, 2.5, 1.4, 0.4, 0.1];
@@ -32,7 +34,7 @@ const drawExample = async () => {
     // Append them to a dataSeries
     const dataSeries = new XyDataSeries(wasmContext);
     for(let i = 0; i < yValues.length; i++){
-        dataSeries.append(i, yValues[i]);
+        dataSeries.append(i,  yValues[i]);
     }
 
     // Create an add a column series
@@ -52,34 +54,6 @@ const drawExample = async () => {
 
     sciChartSurface.zoomExtents();
 };
-
-class MyPaletteProvider implements IFillPaletteProvider, IStrokePaletteProvider {
-    public readonly strokePaletteMode = EStrokePaletteMode.SOLID;
-
-    public onAttached(parentSeries: IRenderableSeries): void {
-        // Nothing to do
-    }
-
-    public onDetached(): void {
-        // Nothing to do
-    }
-
-    public overrideFillArgb(xValue: number, yValue: number, index: number): number {
-        const color = this.getColor(yValue);
-        return parseColorToUIntArgb(color);
-    }
-
-    public overrideStrokeArgb(xValue: number, yValue: number, index: number): number {
-        const color = this.getColor(yValue);
-        return parseColorToUIntArgb(color);
-    }
-
-    private getColor(value: number) {
-        if (value >= 0.95) return "#B22222";
-        if (value >= 0.8) return "#FFBF00";
-        return "#228B22";
-    }
-}
 
 export default function ColumnChart() {
     React.useEffect(() => {
