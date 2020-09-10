f62361d2d3cfb3a5eb28233bbd5397a56769b7a2
= <andrew@scichart.com>
2020-09-10 20:23:22 +0100

    Update index.tsx

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/ScatterChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/ScatterChart/index.tsx
@@ -21,10 +21,15 @@ import {parseColorToUIntArgb} from "scichart/utils/parseColor";
 const divElementId = "chart";
 
 const drawExample = async () => {
+    // Create a SciChartSurface
     const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId);
+
+    // Create X,Y Axis
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { growBy: new NumberRange(0.05, 0.05) }));
 
+    // Create a Scatter Series with EllipsePointMarker
+    // Multiple point-marker types are available including Square, Triangle, Cross and Sprite (custom)
     const scatterSeries = new XyScatterRenderableSeries(wasmContext, {
         pointMarker: new EllipsePointMarker(wasmContext, {
             width: 7,
@@ -33,25 +38,34 @@ const drawExample = async () => {
             fill: "steelblue",
             stroke: "LightSteelBlue",
         }),
+        // Optional: PaletteProvider feature allows coloring per-point based on a rule
         paletteProvider: new ScatterPaletteProvider()
     });
     sciChartSurface.renderableSeries.add(scatterSeries);
 
+    // Create some Xy data and assign to the Scatter Series
     const dataSeries = new XyDataSeries(wasmContext);
     for (let i = 0; i < 100; i++) {
         dataSeries.append(i, Math.sin(i * 0.1));
     }
     scatterSeries.dataSeries = dataSeries;
 
+    // Optional: Add Interactivity Modifiers
     sciChartSurface.chartModifiers.add(new ZoomPanModifier());
     sciChartSurface.chartModifiers.add(new ZoomExtentsModifier());
-
     sciChartSurface.chartModifiers.add(new MouseWheelZoomModifier());
+
     sciChartSurface.zoomExtents();
 };
 
+/**
+ * Optional: Implement a IPointMarkerPaletteProvider which colors every tenth scatter point
+ * to demonstrate the PaletteProvider feature
+ */
 class ScatterPaletteProvider implements IPointMarkerPaletteProvider {
     readonly strokePaletteMode: EStrokePaletteMode;
+    private overrideStroke: number = parseColorToUIntArgb("Red");
+    private overrideFill: number = parseColorToUIntArgb("Green");
     onAttached(parentSeries: IRenderableSeries): void {
     }
 
@@ -59,14 +73,14 @@ class ScatterPaletteProvider implements IPointMarkerPaletteProvider {
     }
 
     overridePointMarkerArgb(xValue: number, yValue: number, index: number): TPointMarkerArgb {
-        // Every tenth point colour the scatter points Blue fill & red stroke
-        // to demonstrate the PaletteProvider feature
+        // Every 10th point return an object with stroke/fill for the overriden scatter point color
         if (index % 10 === 0) {
             return {
-                stroke: parseColorToUIntArgb("Red"),
-                fill: parseColorToUIntArgb("White")
+                stroke: this.overrideStroke,
+                fill: this.overrideFill
             }
         }
+        // Undefined means use default colors
         return undefined;
     }
 }
