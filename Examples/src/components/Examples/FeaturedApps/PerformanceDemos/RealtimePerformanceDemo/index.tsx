40f82ad14e3d02bd60560af88b5ee4dfca76b278
Andrew Burnett-Thompson <andrew@scichart.com>
2020-09-12 11:33:33 +0100

    Improvements to the performance demo

--- Examples/src/components/Examples/FeaturedApps/PerformanceDemos/RealtimePerformanceDemo/index.tsx
+++ Examples/src/components/Examples/FeaturedApps/PerformanceDemos/RealtimePerformanceDemo/index.tsx
@@ -1,42 +1,69 @@
 import * as React from "react";
 import {SciChartSurface} from "scichart";
 import {NumericAxis} from "scichart/Charting/Visuals/Axis/NumericAxis";
-import {NumberRange} from "scichart/Core/NumberRange";
-import {GlowEffect} from "scichart/Charting/Visuals/RenderableSeries/GlowEffect";
-import {Point} from "scichart/Core/Point";
 import {FastLineRenderableSeries} from "scichart/Charting/Visuals/RenderableSeries/FastLineRenderableSeries";
-import {fillNoisySinewave, getNoisySinewave} from "scichart/utils/math";
 import {XyDataSeries} from "scichart/Charting/Model/XyDataSeries";
 import {EAutoRange} from "scichart/types/AutoRange";
 import {RandomWalkGenerator} from "../../../RandomWalkGenerator";
+import {RubberBandXyZoomModifier} from "scichart/Charting/ChartModifiers/RubberBandXyZoomModifier";
+import {MouseWheelZoomModifier} from "scichart/Charting/ChartModifiers/MouseWheelZoomModifier";
+import {XAxisDragModifier} from "scichart/Charting/ChartModifiers/XAxisDragModifier";
+import {EDragMode} from "scichart/types/DragMode";
+import {YAxisDragModifier} from "scichart/Charting/ChartModifiers/YAxisDragModifier";
+import {ZoomExtentsModifier} from "scichart/Charting/ChartModifiers/ZoomExtentsModifier";
 
 const AMPLITUDE = 200;
 
 const divElementId = "chart";
 
 const drawExample = async () => {
+    // Define some constants
+    const numberOfPointsPerTimerTick = 1000; // 1,000 points every timer tick
+    const timerInterval = 10; // timer tick every 10 milliseconds
+    const maxPoints = 1E6; // max points for a single series before the demo stops
+
     // Create a SciChartSurface
     const { wasmContext, sciChartSurface } = await SciChartSurface.create(divElementId);
 
     // Create an XAxis and YAxis
-    sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { autoRange: EAutoRange.Always}));
-    sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { autoRange: EAutoRange.Always}));
+    const xAxis = new NumericAxis(wasmContext, { autoRange: EAutoRange.Always});
+    sciChartSurface.xAxes.add(xAxis);
+    const yAxis = new NumericAxis(wasmContext, { autoRange: EAutoRange.Always});
+    sciChartSurface.yAxes.add(yAxis);
 
     // Create some DataSeries
-    const dataSeries = new XyDataSeries(wasmContext);
+    const dataSeries: XyDataSeries[] = [
+        new XyDataSeries(wasmContext),
+        new XyDataSeries(wasmContext),
+        new XyDataSeries(wasmContext),
+    ];
 
-    // Create a RenderableSeries
-    const lineSeries = new FastLineRenderableSeries(wasmContext, {
-        dataSeries,
-        stroke: "Red",
-        strokeThickness: 2
+    const seriesColors = [
+        "#4083B7",
+        "#FFA500",
+        "#E13219"
+    ]
+
+    // Create some FastLineRenderableSeries bound to each dataSeries and add to the chart
+    dataSeries.map((ds, index) => {
+        sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+            dataSeries: ds,
+            strokeThickness: 2,
+            stroke: seriesColors[index],
+        }));
     });
-    sciChartSurface.renderableSeries.add(lineSeries);
+
+    // Add some interactivity modifiers. These are only operational when the demo is paused
+    sciChartSurface.chartModifiers.add(new RubberBandXyZoomModifier(),
+        new MouseWheelZoomModifier(),
+        new XAxisDragModifier({ dragMode: EDragMode.Panning}),
+        new YAxisDragModifier({dragMode: EDragMode.Panning}),
+        new ZoomExtentsModifier());
 
     // This class generates some data for our example
-    // It generates a random walk, which is a lnine which increases or decreases by a random value
+    // It generates a random walk, which is a line which increases or decreases by a random value
     // each data-point
-    const randomWalk = new RandomWalkGenerator(0);
+    const randomWalkGenerators = [1,2,3].map(_ => { return new RandomWalkGenerator(0);} );
 
     let timerId: NodeJS.Timeout;
 
@@ -44,7 +71,8 @@ const drawExample = async () => {
     const stopDemo = () => {
         clearTimeout(timerId);
         timerId = undefined;
-        randomWalk.reset();
+        randomWalkGenerators.forEach(rw => rw.reset());
+        xAxis.autoRange = EAutoRange.Once;
     };
     document.getElementById("stopDemo").addEventListener("click", stopDemo);
 
@@ -54,20 +82,25 @@ const drawExample = async () => {
         // debugger;
         if (!timerId) {
             const updateFunc = () => {
-                if (dataSeries.count() >= 1E6) {
+                if (dataSeries[0].count() >= maxPoints) {
                     stopDemo();
                     return;
                 }
-                // Get the next 10 random walk x,y values
-                const {xValues, yValues} = randomWalk.getRandomWalkSeries(1000);
 
-                // Append these to the dataSeries. This will cause the chart to redraw
-                dataSeries.appendRange(xValues, yValues);
+                randomWalkGenerators.forEach((randomWalk, index) => {
+                    // Get the next N random walk x,y values
+                    const {xValues, yValues} = randomWalk.getRandomWalkSeries(numberOfPointsPerTimerTick);
 
-                timerId = setTimeout(updateFunc, 10);
+                    // Append these to the dataSeries. This will cause the chart to redraw
+                    dataSeries[index].appendRange(xValues, yValues);
+                });
+
+                timerId = setTimeout(updateFunc, timerInterval);
             };
 
-            timerId = setTimeout(updateFunc, 10);
+            xAxis.autoRange = EAutoRange.Always;
+
+            timerId = setTimeout(updateFunc, timerInterval);
         }
     };
     document.getElementById("startDemo").addEventListener("click", startDemo);
