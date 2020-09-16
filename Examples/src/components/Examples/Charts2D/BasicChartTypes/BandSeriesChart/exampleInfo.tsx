48af7e5ee56da89a194985c83e3793d8dd4519f5
= <andrew@scichart.com>
2020-09-16 13:20:27 +0100

    Example of some subtitles in JSX

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/BandSeriesChart/exampleInfo.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/BandSeriesChart/exampleInfo.tsx
@@ -4,11 +4,13 @@ import { code } from "./GENERATED_SRC";
 import { githubUrl } from "./GENERATED_GITHUB_URL";
 
 const Description = () => <div>Band Series description</div>;
-const Subtitle = () => <div>Subtitle</div>;
+const Subtitle = () => (<p>Demonstrates how to create a <strong>JavaScript Band Chart</strong> or High-Low Fill{' '}
+    using SciChart.js, High Performance{' '}
+    <a href="https://www.scichart.com/javascript-chart-features" target="_blank">JavaScript Charts</a></p>);
 
 export const bandSeriesChartExampleInfo: TExampleInfo = {
-    title: "Band Series Chart",
-    path: "/chart2D_basicCharts_BandSeriesChart",
+    title: "JavaScript Band Chart",
+    path: "/javascript-band-chart",
     subtitle: Subtitle,
     description: Description,
     code,
--- Examples/src/components/Examples/Charts2D/BasicChartTypes/LineChart/exampleInfo.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/LineChart/exampleInfo.tsx
@@ -4,7 +4,9 @@ import { code } from "./GENERATED_SRC";
 import { githubUrl } from "./GENERATED_GITHUB_URL";
 
 const Description = () => <div>Line chart description</div>;
-const Subtitle = () => <div>Woot woot</div>;
+const Subtitle = () => (<p>Demonstrates how to create a <strong>JavaScript Line Chart</strong>{' '}
+    using SciChart.js, High Performance{' '}
+    <a href="https://www.scichart.com/javascript-chart-features" target="_blank">JavaScript Charts</a></p>);
 
 export const lineChartExampleInfo: TExampleInfo = {
     title: "JavaScript Line Chart",
