61490d162e5d0c89249136b34dda818538fe3148
= <andrew@scichart.com>
2020-10-01 16:00:42 +0100

    Small change to allow compilation of modulize.test.ts

--- Examples/testUnit/modularize.test.ts
+++ Examples/testUnit/modularize.test.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai";
-import { sciChartInit2DTest } from "scichart/sciChartInit2DTest";
+import {TSciChart} from "scichart/types/TSciChart";
+import {sciChartInit2DTest} from "scichart3d/sciChartInit2DTest";
 
 describe("Sample Test", () => {
     it("One", done => {
-        sciChartInit2DTest(webAssemblyContext => {
+        sciChartInit2DTest((webAssemblyContext: TSciChart) => {
             const res = 0.5;
             expect(res).to.equal(0.5);
         }, done);
