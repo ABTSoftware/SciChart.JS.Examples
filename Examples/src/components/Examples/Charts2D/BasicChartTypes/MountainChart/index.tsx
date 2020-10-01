f5bd0c4b65b0436da245ef6d8e5dcc4014c03499
Michael Klishevich <busation@gmail.com>
2020-10-01 17:55:48 +0300

    chore(examples): update mountain chart image on the homepage

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/MountainChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/MountainChart/index.tsx
@@ -53,7 +53,7 @@ const drawExample = async () => {
             { color: "rgba(70,130,180,0.2)", offset: 1 },
         ]),
         // Optional: Allows per-point colouring of mountain fill and stroke
-        paletteProvider: new MountainPaletteProvider(),
+        // paletteProvider: new MountainPaletteProvider(),
     });
     sciChartSurface.renderableSeries.add(mountainSeries);
 
