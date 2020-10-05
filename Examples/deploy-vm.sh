9faec624c6a7ab4467eb692d1157786c10335026
Michael Klishevich <busation@gmail.com>
2020-10-05 23:37:50 +0300

    try deploy 1

--- Examples/deploy-vm.sh
+++ Examples/deploy-vm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Start production deploy"
 server=webjsdemoadmin@40.71.93.205
 appRoot=/home/webjsdemoadmin/apps/SciChart.JS.Examples
 ssh $server ". /home/webjsdemoadmin/.nvm/nvm.sh; pm2 stop examples"
