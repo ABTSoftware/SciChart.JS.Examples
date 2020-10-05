dc70a34c654c627ebc0b47d69ef4d13b1b656534
Michael Klishevich <busation@gmail.com>
2020-10-06 00:20:19 +0300

    try deploy 2

--- Examples/deploy-vm.sh
+++ Examples/deploy-vm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Start production deploy"
 server=webjsdemoadmin@40.71.93.205
-appRoot=/home/webjsdemoadmin/apps/SciChart.JS.Examples
-ssh $server ". /home/webjsdemoadmin/.nvm/nvm.sh; pm2 stop examples"
+appRoot=/home/webjsdemoadmin/apps/SciChart.JS.Examples/Examples
+ssh $server ". ~/.nvm/nvm.sh; pm2 stop examples; pm2 delete examples"
+ssh $server "cd ${appRoot} && git pull"
+ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; node -v; npm run build"
+ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; pm2 start build/server.js --name examples"
