7f9876794a24a364cd8e4167ad0d4d3ae572427a
Michael Klishevich <busation@gmail.com>
2020-11-09 18:54:19 +0300

    fix(examples): sdk button improve styles

--- Examples/src/components/Examples/ExamplesRoot.tsx
+++ Examples/src/components/Examples/ExamplesRoot.tsx
@@ -6,11 +6,11 @@ import sciChartLogoImg from "../../images/scichart-logo-making-impossible-projec
 import GettingStarted from "../GettingStarted/GettingStarted";
 import Description from "../Description/Description";
 import SeoTags from "../SeoTags/SeoTags";
-import {makeStyles} from "@material-ui/core/styles";
-import {TExamplePage} from "../AppRouter/examplePages";
-import {HOME_PAGE_TITLE} from "../PageHome/PageHome";
-import {updateGoogleTagManagerPage} from "../../utils/googleTagManager";
-import {getExampleComponent} from "../AppRouter/examples";
+import { makeStyles } from "@material-ui/core/styles";
+import { TExamplePage } from "../AppRouter/examplePages";
+import { HOME_PAGE_TITLE } from "../PageHome/PageHome";
+import { updateGoogleTagManagerPage } from "../../utils/googleTagManager";
+import { getExampleComponent } from "../AppRouter/examples";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 
@@ -69,15 +69,15 @@ const useStyles = makeStyles(
             marginBottom: 20
         },
         textGreen: {
-            color: "#5CB85C",
+            color: "#5CB85C"
         }
     }),
-    {index: 1}
+    { index: 1 }
 );
 
 const ExamplesRoot: React.FC<TProps> = props => {
     const classes = useStyles();
-    const {examplePage} = props;
+    const { examplePage } = props;
 
     const ExampleComponent = getExampleComponent(examplePage.id);
 
@@ -98,45 +98,51 @@ const ExamplesRoot: React.FC<TProps> = props => {
 
     return (
         <div className={classes.root}>
-            <SeoTags title={titleText} keywords={todoKeywords} description={todoDescription}/>
+            <SeoTags title={titleText} keywords={todoKeywords} description={todoDescription} />
             <div className={classes.body}>
                 <div className={classes.colMain}>
                     <div className={classes.colMainContent}>
                         <p>SciChart.js - High Performance Realtime Javascript Charts Examples Suite</p>
                         <div className={classes.title}>
-                            <Title title={titleText}/>
+                            <Title title={titleText} />
                             <div className={classes.subtitle}>{subtitleText}</div>
                         </div>
-                        <ExampleComponent/>
-                        {examplePage && <SourceCode code={codeStr} githubUrl={githubUrl}/>}
+                        <ExampleComponent />
+                        {examplePage && <SourceCode code={codeStr} githubUrl={githubUrl} />}
                     </div>
                 </div>
                 <div className={classes.colDescription}>
                     <div className={classes.sciChartLogo}>
-                        <img src={sciChartLogoImg} width={209} height={42}/>
+                        <img src={sciChartLogoImg} width={209} height={42} />
                     </div>
                     <div className={classes.description}>
                         <Typography variant="h5" variantMapping={{ h5: "p" }} gutterBottom>
                             <span className={classes.textGreen}>// </span>JavaScript Chart Examples
                         </Typography>
-                        <em>SciChart.js ships with ~40 <a href="https://demo.scichart.com">JavaScript Chart
-                            Examples</a> which you can browse, play with, view the source code and see
-                            related documentation.
-                            All of this is possible with the SciChart.js Examples Suite, which ships as part of
-                            the <a href="https://www.scichart.com/downloads">SciChart.js SDK</a></em>
+                        <p>
+                            <em>
+                                SciChart.js ships with ~40{" "}
+                                <a href="https://demo.scichart.com">JavaScript Chart Examples</a> which you can browse,
+                                play with, view the source code and see related documentation. All of this is possible
+                                with the SciChart.js Examples Suite, which ships as part of the{" "}
+                                <a href="https://www.scichart.com/downloads">SciChart.js SDK</a>
+                            </em>
+                        </p>
                         <ButtonGroup
-                            style={{ marginTop: 30 }}
+                            style={{ marginTop: 0 }}
                             size="large"
                             color="primary"
                             aria-label="small outlined button group"
                         >
-                            <Button href="https://www.scichart.com/downloads/" target="_blank">Download the SDK</Button>
+                            <Button href="https://www.scichart.com/downloads/" target="_blank">
+                                Download the SDK
+                            </Button>
                         </ButtonGroup>
                     </div>
                     {DescComponent && (
                         <div className={classes.description}>
                             <Description>
-                                <DescComponent/>
+                                <DescComponent />
                             </Description>
                         </div>
                     )}
