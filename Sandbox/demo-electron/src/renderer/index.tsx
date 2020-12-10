6d615356436557f813c5a915344788938b69a5c8
= <andrew@scichart.com>
2020-12-10 15:39:55 +0000

    Removed the margin around body in electron app

--- Sandbox/demo-electron/src/renderer/index.tsx
+++ Sandbox/demo-electron/src/renderer/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import BasicChart from './components/basicChart';
 import { SciChartSurface } from 'scichart/Charting/Visuals/SciChartSurface';
+import "./main.css"
 
-SciChartSurface.setRuntimeLicenseKey('trial key');
+SciChartSurface.setRuntimeLicenseKey('YOUR_SCICHART_LICENSE_KEY_HERE');
 
 const mainElement = document.createElement('div');
 document.body.appendChild(mainElement);
