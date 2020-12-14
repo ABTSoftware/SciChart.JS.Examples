e4aff4cb95e7c8fd3f346df812355f2e0e1ac8b0
= <andrew@scichart.com>
2020-12-14 13:50:38 +0000

    Updated sitemap

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/ContoursChart/GENERATED_SRC.ts
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/ContoursChart/GENERATED_SRC.ts
@@ -8,38 +8,56 @@ import { ZoomPanModifier } from "scichart/Charting/ChartModifiers/ZoomPanModifie
 import { ZoomExtentsModifier } from "scichart/Charting/ChartModifiers/ZoomExtentsModifier";
 import { MouseWheelZoomModifier } from "scichart/Charting/ChartModifiers/MouseWheelZoomModifier";
 import { zeroArray2D } from "scichart/utils/zeroArray2D";
+import {UniformContoursRenderableSeries} from "scichart/Charting/Visuals/RenderableSeries/UniformContoursRenderableSeries";
 
 const divElementId = "chart";
-const cachedHeatmapDataForExample: number[][][] = [];
-const MAX_SERIES = 20;
-const WIDTH = 300;
-const HEIGHT = 200;
 
 const drawExample = async () => {
-    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId);
+    // Create a SciChartSurface with X,Y Axis
+    const {sciChartSurface, wasmContext} = await SciChartSurface.create(divElementId);
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
 
-    // Create a Heatmap Data-series. Pass heatValues as a number[][] to the UniformHeatmapDataSeries
-    const initialZValues: number[][] = iterate(WIDTH, HEIGHT, 200, 0, MAX_SERIES);
-    const heatmapDataSeries = new UniformHeatmapDataSeries(wasmContext, 100, 1, 100, 1, initialZValues);
+    const heatmapWidth = 300;
+    const heatmapHeight = 200;
 
-    // Create a Heatmap RenderableSeries with the color map. ColorMap.minimum/maximum defines the values in
-    // HeatmapDataSeries which correspond to gradient stops at 0..1
+    const colorPaletteMin = 0;
+    const colorPaletteMax = 200;
+
+    // Create a Heatmap Data-series. Pass heatValues as a number[][] to the UniformHeatmapDataSeries
+    const initialZValues: number[][] = createSeries( 3, heatmapWidth, heatmapHeight, colorPaletteMax);
+    const heatmapDataSeries = new UniformHeatmapDataSeries(wasmContext, 0, 1, 0, 1, initialZValues);
+
+    // Create a Contours RenderableSeries with the same data
+    const contourSeries = new UniformContoursRenderableSeries(wasmContext, {
+        dataSeries: heatmapDataSeries,
+        zMin: 20,
+        zMax: colorPaletteMax,
+        zStep: 20,
+        strokeThickness: 1,
+        stroke: "#C6E6FF",
+    });
+
+    // Add the contours to the chart
+    sciChartSurface.renderableSeries.add(contourSeries);
+
+    // Create a background heatmap series with the same data and add to the chart
     const heatmapSeries = new UniformHeatmapRenderableSeries(wasmContext, {
         dataSeries: heatmapDataSeries,
+        useLinearTextureFiltering: false,
+        opacity: 0.8,
         colorMap: new HeatmapColorMap({
-            minimum: 0,
-            maximum: 200,
+            minimum: colorPaletteMin,
+            maximum: colorPaletteMax,
             gradientStops: [
-                { offset: 0, color: "#00008B" },
-                { offset: 0.2, color: "#6495ED" },
-                { offset: 0.4, color: "#006400" },
-                { offset: 0.6, color: "#7FFF00" },
-                { offset: 0.8, color: "#FFFF00" },
-                { offset: 1.0, color: "#FF0000" },
-            ],
-        }),
+                {offset: 0, color: "#00008B"},
+                {offset: 0.2, color: "#6495ED"},
+                {offset: 0.4, color: "#006400"},
+                {offset: 0.6, color: "#7FFF00"},
+                {offset: 0.8, color: "#FFFF00"},
+                {offset: 1.0, color: "#FF0000"}
+            ]
+        })
     });
 
     // Add heatmap to the chart
@@ -50,99 +68,49 @@ const drawExample = async () => {
     sciChartSurface.chartModifiers.add(new ZoomExtentsModifier());
     sciChartSurface.chartModifiers.add(new MouseWheelZoomModifier());
 
-    return { sciChartSurface, wasmContext, heatmapDataSeries };
+    return { sciChartSurface };
 };
 
-// This function generates data for the heatmap series example
-// because data-generation is not trivial, we generate once before the example starts
-// so you can see the speed & power of SciChart.js
-function iterate(width: number, height: number, cpMax: number, index: number, maxIndex: number): number[][] {
-    const zValues = zeroArray2D([height, width]);
-    // math.round but to X digits
-    function roundTo(number: number, digits: number) {
-        return parseFloat(number.toFixed(digits));
-    }
-    const angle = roundTo(Math.PI * 2 * index, 3) / maxIndex;
+// This function generates data for the heatmap with contours series example
+function createSeries(index: number, heatmapWidth: number, heatmapHeight: number, colorPaletteMax: number) {
+    const zValues = zeroArray2D([heatmapHeight, heatmapWidth]);
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            const v =
-                (1 + roundTo(Math.sin(x * 0.04 + angle), 3)) * 50 +
-                (1 + roundTo(Math.sin(y * 0.1 + angle), 3)) * 50 * (1 + roundTo(Math.sin(angle * 2), 3));
-            const cx = width / 2;
-            const cy = height / 2;
+    const angle = Math.PI * 2 * index / 30;
+    let smallValue = 0;
+    for (let x = 0; x < heatmapWidth; x++) {
+        for (let y = 0; y < heatmapHeight; y++) {
+            const v = (1 + Math.sin(x * 0.04 + angle)) * 50 +
+                (1 + Math.sin(y * 0.1 + angle)) * 50 * (1 + Math.sin(angle * 2));
+            const cx = heatmapWidth / 2;
+            const cy = heatmapHeight / 2;
             const r = Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
             const exp = Math.max(0, 1 - r * 0.008);
-            const zValue = v * exp + Math.random() * 50;
-            zValues[y][x] = zValue > cpMax ? cpMax : zValue;
+            const zValue = (v * exp);
+            zValues[y][x] = (zValue > colorPaletteMax) ? colorPaletteMax : zValue;
+            zValues[y][x] += smallValue;
         }
+
+        smallValue += 0.001;
     }
+
     return zValues;
 }
 
-let timerId: NodeJS.Timeout;
-let updateIndex: number = 0;
-
 export default function HeatmapChart() {
-    const [heatmapDataSeries, setHeatmapDataSeries] = React.useState<UniformHeatmapDataSeries>();
     const [sciChartSurface, setSciChartSurface] = React.useState<SciChartSurface>();
 
     React.useEffect(() => {
         (async () => {
             const res = await drawExample();
             setSciChartSurface(res.sciChartSurface);
-            setHeatmapDataSeries(res.heatmapDataSeries);
         })();
         // Delete sciChartSurface on unmount component to prevent memory leak
         return () => sciChartSurface?.delete();
     }, []);
 
-    const updateChart = () => {
-        timerId = setTimeout(updateChart, 20);
-        // Cycle through pre-generated data every 20ms
-        const newZValues = cachedHeatmapDataForExample[updateIndex++];
-        // Update the heatmap z-values
-        heatmapDataSeries.setZValues(newZValues);
-        if (updateIndex >= MAX_SERIES) {
-            updateIndex = 0;
-        }
-    };
-
-    const handleStart = () => {
-        // Pre-generate data for the example.
-        // We do this once since data-generation of complex waveforms is quite heavy.
-        if (cachedHeatmapDataForExample.length === 0) {
-            for (let i = 1; i < MAX_SERIES; i++) {
-                cachedHeatmapDataForExample.push(iterate(WIDTH, HEIGHT, 200, i, MAX_SERIES));
-            }
-        }
-
-        if (!timerId) {
-            updateChart();
-        }
-    };
-
-    const handleStop = () => {
-        clearTimeout(timerId);
-        timerId = undefined;
-    };
-
     return (
         <div>
             <div id={divElementId} style={{ maxWidth: 900 }} />
-            <div style={{ marginTop: 20 }}>
-                When click Start first time data is being generated, it could take a while
-            </div>
-            {heatmapDataSeries && (
-                <div>
-                    <div style={{ marginTop: 20 }}>
-                        <button onClick={handleStart}>Start</button>
-                        <button onClick={handleStop} style={{ marginLeft: 10 }}>
-                            Stop
-                        </button>
-                    </div>
-                </div>
-            )}
         </div>
     );
 }
