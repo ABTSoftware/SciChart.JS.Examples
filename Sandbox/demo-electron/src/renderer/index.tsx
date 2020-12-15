6ff4aade06a7400b4932877373423c59e22df380
= <andrew@scichart.com>
2020-12-15 18:08:03 +0000

    Ensure SciChartSurface stretches to full screen. Note only works with v1.11346 or higher

--- Sandbox/demo-electron/src/renderer/index.tsx
+++ Sandbox/demo-electron/src/renderer/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import BasicChart from './components/basicChart';
+import FullScreenChart from './components/fullScreenChart';
 import { SciChartSurface } from 'scichart/Charting/Visuals/SciChartSurface';
 import "./main.css"
 
 SciChartSurface.setRuntimeLicenseKey('YOUR_SCICHART_LICENSE_KEY_HERE');
 
+// <div class="mainDiv"> is placed at the root of the app. See main.css where it is given 100vh size
 const mainElement = document.createElement('div');
+mainElement.className = "mainDiv";
 document.body.appendChild(mainElement);
 
 ReactDom.render(
-    <BasicChart />,
+    <FullScreenChart />,
     mainElement
 );
