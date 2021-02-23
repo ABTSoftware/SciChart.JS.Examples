b6d67263fbb3ba4e7459b17c7c2b0df70f3c2276
danylboiko95 <danylboiko@gmail.com>
2021-02-23 17:03:42 +0200

    feat: sass and css work

--- Examples/webpack.client.config.js
+++ Examples/webpack.client.config.js
@@ -20,8 +20,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    { loader: "style-loader" }, // to inject the result into the DOM as a style block
-                    // { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+                    { loader: "style-loader" },
                     {
                         loader: "css-loader",
                         options: {
@@ -29,9 +28,8 @@ module.exports = {
                                 localIdentName: "[name]__[local]--[hash:base64:5]"
                             }
                         }
-                    }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
-                    { loader: "sass-loader" } // to convert SASS to CSS
-                    // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+                    },
+                    { loader: "sass-loader" }
                 ],
                 exclude: /node_modules/
             },
@@ -71,7 +69,7 @@ module.exports = {
         new webpack.IgnorePlugin(/(fs)/),
         // new BundleAnalyzerPlugin()
         new MiniCssExtractPlugin({
-            filename: filename("css")
+            filename: filename("styles.css")
         })
     ]
 };
