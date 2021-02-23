e518648498bfeac2f26a7449e1ed9aebf59d8aee
danylboiko95 <danylboiko@gmail.com>
2021-02-23 10:55:48 +0200

    feat: adding scss loaders for migrating to css styles

--- Examples/webpack.server.config.js
+++ Examples/webpack.server.config.js
@@ -15,6 +15,29 @@ module.exports = {
     },
     module: {
         rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: ["style-loader", "css-loader"],
+            //     exclude: /node_modules/
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         { loader: "style-loader" }, // to inject the result into the DOM as a style block
+            //         // { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+            //         {
+            //             loader: "css-loader",
+            //             // options: {
+            //             //     modules: {
+            //             //         localIdentName: "[name]__[local]--[hash:base64:5]"
+            //             //     }
+            //             // }
+            //         }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+            //         { loader: "sass-loader" } // to convert SASS to CSS
+            //         // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+            //     ],
+            //     exclude: /node_modules/
+            // },
             {
                 test: /\.tsx?$/,
                 use: "ts-loader",
@@ -24,8 +47,8 @@ module.exports = {
                 test: /\.(png|svg|jpg|gif)$/,
                 loader: "file-loader",
                 options: {
-                    name: 'images/[name].[ext]',
-                },
+                    name: "images/[name].[ext]"
+                }
             }
         ]
     },
