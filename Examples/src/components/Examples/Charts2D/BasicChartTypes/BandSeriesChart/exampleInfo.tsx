72674db7b6429442b9ce9421471824eb013e0d2e
danylboiko95 <danylboiko@gmail.com>
2021-03-17 12:49:56 +0200

    finished digital and band charts

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/BandSeriesChart/exampleInfo.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/BandSeriesChart/exampleInfo.tsx
@@ -1,51 +1,102 @@
 import * as React from "react";
-import {TExampleInfo} from "../../../../AppRouter/examplePages";
-import {code} from "./GENERATED_SRC";
-import {githubUrl} from "./GENERATED_GITHUB_URL";
-import {ExampleStrings} from "../../../ExampleStrings";
+import { TExampleInfo } from "../../../../AppRouter/examplePages";
+import { code } from "./GENERATED_SRC";
+import { githubUrl } from "./GENERATED_GITHUB_URL";
+import { ExampleStrings } from "../../../ExampleStrings";
+import Gallery from "../../../../Gallery/Gallery";
+import { GalleryItem } from "../../../../../helpes/types/types";
+import { TDocumentationLink } from "../../../../../helpes/types/ExampleDescriptionTypes";
+import ExampleDescription from "../../../../ExampleDescription/ExampleDescription";
+import multiPaneStockChart from "../../CreateStockCharts/MultiPaneStockCharts/javascript-multi-pane-stock-charts.jpg";
+import fanChart from "../FanChart/javascript-fan-chart.jpg";
 
-const Description = () => (<div>
-    <p>Demonstrates how to create a JavaScript Spline Band Chart. This is a chart type which draws an area (polygon or fill)
-        between two lines. The FastBandRenderableSeries requires an XyyDataSeries, which contains one X-point and two
-        Y-points</p>
-    <p>Dual lines are drawn by the stroke, strokeY1 properties and shaded bands are drawn by the fill and fillY1
-        properties, depending on whether y1 is greater than y2</p>
-    <h4>Tips!</h4>
-    <p>If you have data where Y1 is greater than Y2 always, you’ll get an envelope effect. Great for rendering
-        confidence intervals, error margins or Bollinger Bands!</p>
-    <h4>Documentation Links</h4>
-    <ul>
-        <li><a href={ExampleStrings.urlDocumentationHome} title={ExampleStrings.titleDocumentationHome} target="_blank">
-            SciChart.js Documentation Home</a></li>
-        <li><a href={ExampleStrings.urlTutorialsHome} title={ExampleStrings.titleTutorialsHome} target="_blank">
-            SciChart.js Tutorials</a></li>
-        <li><a href={ExampleStrings.urlBandChartDocumentation} target="_blank"
-               title={ExampleStrings.urlTitleBandChartDocumentation}>JavaScript Band Chart Documentation</a></li>
-        <li><a href={ExampleStrings.urlRenderSeriesPropertiesDocumentation}
-               title={ExampleStrings.urlTitleRenderSeriesProperties} target="_blank">
-            Common RenderableSeries Properties</a></li>
-    </ul>
-    <h4>See Also</h4>
-    <ul>
-        <li><a href={ExampleStrings.urlMultiPaneStockChart}
-               title={ExampleStrings.urlTitleMultiPaneStockChart}>The Multi-Pane
-            Stock Chart Example</a></li>
-        <li><a href={ExampleStrings.urlFanChart}
-               title={ExampleStrings.urlTitleFanChart}>The JavaScript Fan Chart Example</a></li>
-    </ul>
-</div>);
-const Subtitle = () => (<p>Demonstrates how to create a <strong>JavaScript Band Chart</strong> or High-Low Fill{' '}
-    using SciChart.js, High Performance{' '}
-    <a href={ExampleStrings.urlJavascriptChartFeatures} target="_blank">JavaScript Charts</a></p>);
+const previewDescription = `Demonstrates how to create a JavaScript Spline Band Chart. This is a chart type which draws an area (polygon
+    or fill) between two lines. The FastBandRenderableSeries requires an XyyDataSeries, which contains one
+    X-point and two Y-points`;
+const description = `Dual lines are drawn by the stroke, strokeY1 properties and shaded bands are drawn by the fill and fillY1
+properties, depending on whether y1 is greater than y2`;
+const tips = [
+    `If you have data where Y1 is greater than Y2 always, you’ll get an envelope effect. Great for rendering
+    confidence intervals, error margins or Bollinger Bands!`
+];
+
+const documentationLinks: TDocumentationLink[] = [
+    {
+        href: ExampleStrings.urlDocumentationHome,
+        title: ExampleStrings.titleDocumentationHome,
+        linkTitle: "SciChart.js Documentation Home"
+    },
+    {
+        href: ExampleStrings.urlTutorialsHome,
+        title: ExampleStrings.titleTutorialsHome,
+        linkTitle: "SciChart.js Tutorials"
+    },
+    {
+        href: ExampleStrings.urlBandChartDocumentation,
+        title: ExampleStrings.urlTitleDigitalLineChartDocumentation,
+        linkTitle: "JavaScript Band Chart Documentation"
+    },
+    {
+        href: ExampleStrings.urlRenderSeriesPropertiesDocumentation,
+        title: ExampleStrings.urlTitleRenderSeriesProperties,
+        linkTitle: "Common RenderableSeries Properties"
+    }
+];
+
+const seeAlso: GalleryItem[] = [
+    {
+        chartGroupTitle: "See also",
+        items: [
+            {
+                imgPath: multiPaneStockChart,
+                title: ExampleStrings.titleMultiPaneStockChart,
+                seoTitle: ExampleStrings.urlTitleMultiPaneStockChart,
+                examplePath: ExampleStrings.urlMultiPaneStockChart
+            },
+            {
+                imgPath: fanChart,
+                title: ExampleStrings.titleFanChart,
+                seoTitle: ExampleStrings.urlTitleFanChartDocumentation,
+                examplePath: ExampleStrings.urlFanChart
+            }
+        ]
+    }
+];
+
+const SeeAlsoComponent = () => <Gallery examples={seeAlso} />;
+
+const Subtitle = () => (
+    <p>
+        Demonstrates how to create a <strong>JavaScript Band Chart</strong> or High-Low Fill using SciChart.js, High
+        Performance{" "}
+        <a href={ExampleStrings.urlJavascriptChartFeatures} target="_blank">
+            JavaScript Charts
+        </a>
+    </p>
+);
+
+const Description = () => (
+    <div>
+        <ExampleDescription
+            documentationLinks={documentationLinks}
+            tips={tips}
+            description={description}
+            previewDescription={previewDescription}
+            seeAlso={seeAlso}
+        />
+    </div>
+);
 
 export const bandSeriesChartExampleInfo: TExampleInfo = {
     title: ExampleStrings.titleBandChart,
     path: ExampleStrings.urlBandChart,
     subtitle: Subtitle,
     description: Description,
+    seeAlso: SeeAlsoComponent,
     code,
     githubUrl,
-    seoDescription: "Demonstrates how to create a JavaScript Band Chart. This is a chart type which draws an area (polygon or fill) " +
+    seoDescription:
+        "Demonstrates how to create a JavaScript Band Chart. This is a chart type which draws an area (polygon or fill) " +
         "between two lines. The Band series requires one X-point and two Y-points to draw the polygon",
     seoKeywords: "band, chart, javascript, webgl, canvas",
     thumbnailImage: "javascript-band-chart.jpg"
