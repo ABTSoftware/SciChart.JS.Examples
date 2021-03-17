a197781cebd7ba759b7ba9af91ebecf02c59a5bc
Ivan Gasyuk <ivan@scihart.com>
2021-03-17 10:42:20 +0200

    redesign: update top section of sidebar; trial download button

--- Examples/src/components/DrawerContent/DrawerContent.tsx
+++ Examples/src/components/DrawerContent/DrawerContent.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from '@material-ui/icons/Close';
 import Navigation from "../Navigation/Navigation";
 import { useHistory } from "react-router-dom";
 import classes from "./DrawerContent.module.scss";
@@ -20,11 +23,25 @@ const DrawerContent: React.FC<TProps> = props => {
 
     return (
         <div className={classes.DrawerContent}>
-            <div className={classes.toolbar}>
-                <h6 className={classes.homepageLink} onClick={() => history.push("/")}>
-                    SciChart.js
-                </h6>
-                <span className={classes.versionCaption}>{`v${APP_VERSION}`}</span>
+            <div className={classes.DrawerTopSection}>
+                <div className={classes.toolbar}>
+                    <h6 className={classes.homepageLink} onClick={() => history.push("/")}>
+                        SciChart.js
+                    </h6>
+                    <span className={classes.versionCaption}>{`v${APP_VERSION}`}</span>
+                </div>
+                <IconButton
+                    onClick={toggleDrawer}
+                    className={classes.CloseButton}
+                    aria-label="close-drawer-button"
+                >
+                    <CloseIcon />
+                </IconButton>
+            </div>
+            <div className={classes.DownloadTrialButton}>
+                <Button href="https://www.scichart.com/downloads/" target="_blank">
+                    Download Trial
+                </Button>
             </div>
             <Divider />
             <Navigation testIsOpened={testIsOpened} onExpandClick={toggleOpenedMenuItem} toggleDrawer={toggleDrawer} />
