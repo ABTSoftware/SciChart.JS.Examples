29494336fca24212fa9121f44b4146bd25a5cf2a
David Burleigh <david@scichart.com>
2021-06-16 16:43:57 +0100

    fix(demo): fix effects on EKG example

--- Examples/src/components/Examples/FeaturedApps/MedicalCharts/VitalSignsMonitorDemo/index.tsx
+++ Examples/src/components/Examples/FeaturedApps/MedicalCharts/VitalSignsMonitorDemo/index.tsx
@@ -116,12 +116,7 @@ const drawExample = async (
         dataSeries4.append(i, NaN);
     }
 
-    const effect = new GlowEffect(wasmContext, {
-        range: 0,
-        intensity: 1,
-        color: "#333333",
-        offset: new Point(10, 10)
-    });
+    const effect = new GlowEffect(wasmContext);
 
     sciChartSurface.renderableSeries.add(
         new FastLineRenderableSeries(wasmContext, {
