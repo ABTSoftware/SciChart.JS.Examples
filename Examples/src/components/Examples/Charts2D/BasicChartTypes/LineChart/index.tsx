b6c85569e3ce6f976ad286f4d533e939b8cfcfc1
andyb1979 <aburnettthompson@gmail.com>
2021-07-22 10:41:19 +0100

    chore(examples) cleaning example code

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/LineChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/LineChart/index.tsx
@@ -13,6 +13,7 @@ import { Point } from "scichart/Core/Point";
 
 import classes from "../../../../Examples/Examples.module.scss";
 import { EAnimationType } from "scichart/types/AnimationType";
+import {ShadowEffect} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/RenderableSeries/ShadowEffect";
 
 const divElementId = "chart";
 
@@ -21,45 +22,44 @@ const drawExample = async () => {
     const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId);
 
     // Create the X,Y Axis
-    const xAxis = new NumericAxis(wasmContext);
-    sciChartSurface.xAxes.add(xAxis);
+    sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
+    sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { growBy: new NumberRange(0.05, 0.05) }));
 
-    const yAxis = new NumericAxis(wasmContext, { growBy: new NumberRange(0.05, 0.05) });
-    sciChartSurface.yAxes.add(yAxis);
+    const xValues = Array.from({length: 100}, (x,i) => i);
+    const yValues = xValues.map(x => Math.sin(x * 0.1));
 
     // Create an XyDataSeries as data source
-    const xyDataSeries = new XyDataSeries(wasmContext);
-    for (let i = 0; i < 100; i++) {
-        xyDataSeries.append(i, Math.sin(i * 0.1));
-    }
+    const xyDataSeries = new XyDataSeries(wasmContext, { xValues, yValues });
 
     // Create and add a line series to the chart
-    const lineSeries = new FastLineRenderableSeries(wasmContext, {
+    sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
         stroke: "#ff6600",
         strokeThickness: 5,
         dataSeries: xyDataSeries,
-        animation: { type: EAnimationType.Wave, options: { zeroLine: -1, pointDurationFraction: 0.5, duration: 1000 } }
-    });
-    sciChartSurface.renderableSeries.add(lineSeries);
+        animation: { type: EAnimationType.Wave, options: { zeroLine: -1, pointDurationFraction: 0.5, duration: 1000 } },
+        effect: new ShadowEffect(wasmContext, { brightness: 1, offset: new Point(5,-10), range: 0.7})
+    }));
 
-    // OPTIONAL: PaletteProvider APIs allow you to add per-point colouring based on a rule, or a gradient
-    lineSeries.paletteProvider = PaletteFactory.createGradient(
-        wasmContext,
-        new GradientParams(new Point(0, 0), new Point(1, 1), [
-            { color: "red", offset: 0 },
-            { color: "pink", offset: 0.2 },
-            { color: "yellow", offset: 0.5 },
-            { color: "purple", offset: 0.7 },
-            { color: "green", offset: 1 }
-        ])
-    );
+    // // OPTIONAL: PaletteProvider APIs allow you to add per-point colouring based on a rule, or a gradient
+    // lineSeries.paletteProvider = PaletteFactory.createGradient(
+    //     wasmContext,
+    //     new GradientParams(new Point(0, 0), new Point(1, 1), [
+    //         { color: "red", offset: 0 },
+    //         { color: "pink", offset: 0.2 },
+    //         { color: "yellow", offset: 0.5 },
+    //         { color: "purple", offset: 0.7 },
+    //         { color: "green", offset: 1 }
+    //     ])
+    // );
 
     // OPTIONAL: Add some interactivity modifiers
     sciChartSurface.chartModifiers.add(new ZoomPanModifier());
     sciChartSurface.chartModifiers.add(new ZoomExtentsModifier());
     sciChartSurface.chartModifiers.add(new MouseWheelZoomModifier());
 
+    // Zoom to fit
     sciChartSurface.zoomExtents();
+
     return { sciChartSurface, wasmContext };
 };
 
