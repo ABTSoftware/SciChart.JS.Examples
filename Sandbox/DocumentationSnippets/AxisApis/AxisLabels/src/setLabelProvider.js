118a4fbb4b13781b8145416694489b1fb7ae5df5
andyb1979 <aburnettthompson@gmail.com>
2021-09-09 10:22:28 +0100

    chore(documentation) added LabelProvider examples

--- Sandbox/DocumentationSnippets/AxisApis/AxisLabels/src/setLabelProvider.js
+++ Sandbox/DocumentationSnippets/AxisApis/AxisLabels/src/setLabelProvider.js
@@ -7,10 +7,13 @@ import {ENumericFormat} from "scichart/types/NumericFormat";
 export async function setLabelProvider(divId) {
     const { sciChartSurface, wasmContext } = await SciChartSurface.create(divId);
 
-    // Create an XAxis with 2 Decimal Places
+    // Method 1: Apply a LabelProvider
+
+    // First, create an XAxis with 2 Decimal Places
     const xAxis = new NumericAxis(wasmContext, {
         visibleRange: new NumberRange(0, 1)
     });
+    // Then later set the LabelProvider
     xAxis.labelProvider = new NumericLabelProvider({
         labelFormat: ENumericFormat.Decimal,
         labelPrecision: 2,
@@ -19,7 +22,7 @@ export async function setLabelProvider(divId) {
     });
     sciChartSurface.xAxes.add(xAxis);
 
-    // Create a YAxis with 4 Decimal Places
+    // Method 2: Set Label Formatting properties in the axis constructor
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext, {
         visibleRange: new NumberRange(0, 1),
         labelFormat: ENumericFormat.Decimal,
