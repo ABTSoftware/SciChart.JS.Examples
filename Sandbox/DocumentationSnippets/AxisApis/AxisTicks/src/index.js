dc4e6d9487889008e9b63e32e28b8b8a4759b6b3
andyb1979 <aburnettthompson@gmail.com>
2021-09-17 17:24:48 +0100

    chore(documentation) Added Axis Ticks documentation code samples

---
+++ Sandbox/DocumentationSnippets/AxisApis/AxisTicks/src/index.js
@@ -0,0 +1,7 @@
+import { autoTicks } from "./autoTicks";
+import { manualTicks } from "./manualTicks";
+import {tickProvider} from "./tickProvider";
+
+autoTicks("scichart-div-1");
+manualTicks("scichart-div-2");
+tickProvider("scichart-div-3");
