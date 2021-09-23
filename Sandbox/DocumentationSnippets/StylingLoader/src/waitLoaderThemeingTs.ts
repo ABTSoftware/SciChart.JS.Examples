075b87d95a0cfa8703a250c3c06998d8467e9e44
Michael Klishevich <busation@gmail.com>
2021-09-23 18:07:29 +0300

    feat(sandbox): add documentation snippet for styling the loader

---
+++ Sandbox/DocumentationSnippets/StylingLoader/src/waitLoaderThemeingTs.ts
@@ -0,0 +1,43 @@
+import { SciChartSurface } from 'scichart/Charting/Visuals/SciChartSurface';
+import { NumericAxis } from 'scichart/Charting/Visuals/Axis/NumericAxis';
+import { ISciChartLoader } from 'scichart/Charting/Visuals/loader';
+import { IThemeProvider } from 'scichart/Charting/Themes/IThemeProvider';
+import { SciChartJSDarkv2Theme } from 'scichart/Charting/Themes/SciChartJSDarkv2Theme';
+
+export async function waitLoaderThemeingTs(divId: string) {
+    const loader = new CustomChartLoader();
+    const theme = new SciChartJSDarkv2Theme();
+    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divId, { loader, theme });
+
+    sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
+    sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
+}
+
+export class CustomChartLoader implements ISciChartLoader {
+    public type: 'Custom';
+    public loadingText: string = 'Loading SciChart...';
+
+    constructor(options?: { loadingText?: string }) {
+        this.loadingText = options?.loadingText ?? this.loadingText;
+    }
+
+    public addChartLoader(domChartRoot: HTMLDivElement, theme: IThemeProvider): HTMLElement {
+        const loaderContainerDiv = document.createElement('div');
+        loaderContainerDiv.style.backgroundColor = 'black';
+        loaderContainerDiv.style.height = '100%';
+        loaderContainerDiv.style.width = '100%';
+        loaderContainerDiv.style.display = 'flex';
+        loaderContainerDiv.style.justifyContent = 'center';
+        loaderContainerDiv.style.alignItems = 'center';
+        const loaderImage = document.createElement('object');
+        loaderImage.type = 'image/svg+xml';
+        loaderImage.data = '/svg_animation_ext.svg';
+        loaderContainerDiv.appendChild(loaderImage);
+        domChartRoot.appendChild(loaderContainerDiv);
+        return loaderContainerDiv;
+    }
+    public removeChartLoader(domChartRoot: HTMLDivElement, loaderElement: HTMLElement): void {
+        // Remove loader after 2000ms timeout
+        setTimeout(() => domChartRoot.removeChild(loaderElement), 2000);
+    }
+}
---
+++ Sandbox/DocumentationSnippets/StylingLoader/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "images/[name].[ext]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/index.html", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
+        { from: "src/img/svg_animation_ext.svg", to: "" }
+      ]
+    })
+  ]
+};
