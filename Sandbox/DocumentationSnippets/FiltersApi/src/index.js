201e3366d9a9acedf34f15a439913a4b128fb653
David Burleigh <david@scichart.com>
2021-09-23 12:41:54 +0100

    chore(docs): Filters Api documentation snippets

---
+++ Sandbox/DocumentationSnippets/FiltersApi/src/index.js
@@ -0,0 +1,11 @@
+import { initSciChart} from "./scaleoffset1";
+import { initSciChart2} from "./lineartrend1";
+import { initSciChart3} from "./lineartrend2";
+import { initSciChart4} from "./movingAverage1";
+import { initSciChart5} from "./ratio";
+
+initSciChart();
+initSciChart2();
+initSciChart3();
+initSciChart4();
+initSciChart5();
