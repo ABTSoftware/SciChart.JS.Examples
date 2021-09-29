aca7b29cdeef69fea2523a3b99a1ccd21e8ec110
David Burleigh <david@scichart.com>
2021-09-29 17:24:19 +0100

    fix(Examples): missing comma

--- Examples/webpack.client.config.js
+++ Examples/webpack.client.config.js
@@ -70,7 +70,7 @@ module.exports = {
                 { from: "sitemap.xml", to: "" },
                 { from: "robots.txt", to: "" },
                 { from: "src/components/Examples/BuilderApi/**/*.jpg", to: "images/[name].jpg" },
-                { from: "src/components/Examples/Charts2D/Filters/**/*.jpg", to: "images/[name].jpg" }
+                { from: "src/components/Examples/Charts2D/Filters/**/*.jpg", to: "images/[name].jpg" },
                 { from: "src/components/Examples/Charts2D/AxisLabelCustomization/**/*.jpg", to: "images/[name].jpg" }
             ]
         }),
