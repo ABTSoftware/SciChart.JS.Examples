80b6aad0691ca0763e2672e8c620e3f0f385371a
Michael klishevich <busation@gmail.com>
2021-10-11 18:34:59 +0300

    chore(docs): add LegendModifier snippet

---
+++ Sandbox/DocumentationSnippets/LegendModifier/src/index.js
@@ -0,0 +1,6 @@
+import { legendModifierJs } from './legendModifierJs';
+import { legendModifierTs } from './legendModifierTs';
+
+const divId = 'scichart-div-id';
+// legendModifierJs(divId);
+legendModifierTs(divId);
