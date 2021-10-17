ef417daf74beae04701a1d4c56de7455941c5c5f
andyb1979 <aburnettthompson@gmail.com>
2021-10-17 19:49:04 +0100

    chore(documentation) moved the web demos folder

--- Sandbox/CustomerExamples/ZoomingPanningDemoJustJavascript/src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.use(express.static("src/client"));
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
