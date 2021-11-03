e88b79c8408c63466f089642e55f0fbf6ec382a3
Michael Klishevich <busation@gmail.com>
2021-11-03 18:51:15 +0300

    fix(tutorials): casing in imports for 3D example

--- Tutorials/3D_Chart_Tutorials_JavaScript/Tutorial_1_Setting_up_a_3D_Chart_project_with_SciChart/src/index.js
+++ Tutorials/3D_Chart_Tutorials_JavaScript/Tutorial_1_Setting_up_a_3D_Chart_project_with_SciChart/src/index.js
@@ -1,25 +1,31 @@
-import {SciChart3DSurface} from "scichart/charting3d/Visuals/SciChart3DSurface";
-import {NumericAxis3D} from "scichart/charting3d/Visuals/Axis/NumericAxis3D";
-import { CameraController } from "scichart/charting3d/CameraController";
-import { Vector3 } from "scichart/charting3d/Vector3";
+import { SciChart3DSurface } from "scichart/Charting3D/Visuals/SciChart3DSurface";
+import { NumericAxis3D } from "scichart/Charting3D/Visuals/Axis/NumericAxis3D";
+import { CameraController } from "scichart/Charting3D/CameraController";
+import { Vector3 } from "scichart/Charting3D/Vector3";
+import { OrbitModifier3D } from "scichart/Charting3D/ChartModifiers/OrbitModifier3D";
+import { MouseWheelZoomModifier3D } from "scichart/Charting3D/ChartModifiers/MouseWheelZoomModifier3D";
 
 async function initSciChart() {
-    // Create the SciChart3DSurface in the div 'scichart-root'
-    // The SciChart3DSurface, and webassembly context 'wasmContext' are paired. This wasmContext
-    // instance must be passed to other types that exist on the same surface.
-    const {sciChart3DSurface, wasmContext} = await SciChart3DSurface.create("scichart-root");
+  // Create the SciChart3DSurface in the div 'scichart-root'
+  // The SciChart3DSurface, and webassembly context 'wasmContext' are paired. This wasmContext
+  // instance must be passed to other types that exist on the same surface.
+  const { sciChart3DSurface, wasmContext } = await SciChart3DSurface.create(
+    "scichart-root"
+  );
 
-    sciChart3DSurface.camera = new CameraController(wasmContext, {
-        position: new Vector3(300,200,300),
-        target: new Vector3(0,50,0)
-    });
-    // Create an X,Y,Z Axis and add to the chart    
-    
-    sciChart3DSurface.xAxis = new NumericAxis3D(wasmContext)
-    sciChart3DSurface.yAxis = new NumericAxis3D(wasmContext);
-    sciChart3DSurface.zAxis = new NumericAxis3D(wasmContext);    
+  sciChart3DSurface.camera = new CameraController(wasmContext, {
+    position: new Vector3(300, 200, 300),
+    target: new Vector3(0, 50, 0)
+  });
+  // Create an X,Y,Z Axis and add to the chart
 
-    // That's it! You just created your first SciChart3DSurface!
+  sciChart3DSurface.xAxis = new NumericAxis3D(wasmContext);
+  sciChart3DSurface.yAxis = new NumericAxis3D(wasmContext);
+  sciChart3DSurface.zAxis = new NumericAxis3D(wasmContext);
+
+  // That's it! You just created your first SciChart3DSurface!
+  sciChart3DSurface.chartModifiers.add(new MouseWheelZoomModifier3D());
+  sciChart3DSurface.chartModifiers.add(new OrbitModifier3D());
 }
 
 initSciChart();
