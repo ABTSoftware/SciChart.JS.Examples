1d2d0e411117de378527966ccb4572fc678f4979
andyb1979 <andrew@scichart.com>
2022-04-07 17:30:24 +0100

    chore(documentation) updated styling and theming documentation sample to include axis background

--- Sandbox/DocumentationSnippets/StylingThemeing/src/index.js
+++ Sandbox/DocumentationSnippets/StylingThemeing/src/index.js
@@ -7,6 +7,7 @@ import {stylingAxisInCode} from "./stylingAxis";
 import {createCustomTheme} from "./customTheme";
 import {createCustomThemeTs} from "./customThemeTs";
 import {inheritThemeGradientBackground} from "./gradientBackgrounds";
+import {stylingAxisBorderAndBackground} from "./stylingAxisBorderBackground";
 
 transparentBackground("scichart-div-id-1");
 imageBackground("scichart-div-id-2");
@@ -14,6 +15,8 @@ blurredBackground("scichart-div-id-3");
 waitLoaderThemeing("scichart-div-id-4");
 waitLoaderThemeingTs("scichart-div-id-5");
 stylingAxisInCode("scichart-div-id-6");
-createCustomTheme("scichart-div-id-7");
-createCustomThemeTs("scichart-div-id-8");
-inheritThemeGradientBackground("scichart-div-id-9");
+stylingAxisBorderAndBackground("scichart-div-id-7");
+createCustomTheme("scichart-div-id-8");
+createCustomThemeTs("scichart-div-id-9");
+inheritThemeGradientBackground("scichart-div-id-10");
+
