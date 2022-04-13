232eb5f3c086fb232efeac536cc6e09dda2cc68d
andyb1979 <andrew@scichart.com>
2022-04-13 19:55:12 +0100

    chore(documentation) updated axis docs samples

--- Sandbox/DocumentationSnippets/AxisApis/AxisTypes/src/categoryaxis.js
+++ Sandbox/DocumentationSnippets/AxisApis/AxisTypes/src/categoryaxis.js
@@ -10,11 +10,18 @@ import {XyDataSeries} from "scichart/Charting/Model/XyDataSeries";
 export async function initCategoryAxis() {
     const {sciChartSurface, wasmContext} = await SciChartSurface.create("scichart-div-3");
 
+    // Unix Epoch for March 1st 2022 & March 2nd
+    const March1st2022 = 1646092800;
+    const March2nd2022 = 1646179200;
+    const OneDay = March2nd2022 - March1st2022;
+
     // Creating a CategoryAxis as an XAxis on the bottom
     sciChartSurface.xAxes.add(new CategoryAxis(wasmContext, {
+        defaultXStart: March1st2022,
+        defaultXStep: OneDay,
         drawMajorGridLines: true,
         drawMinorGridLines: true,
-        axisTitle: "X Axis",
+        axisTitle: "Category X Axis",
         axisAlignment: EAxisAlignment.Bottom,
         autoRange: EAutoRange.Once,
         labelFormat: ENumericFormat.Date_DDMMYYYY
@@ -25,16 +32,16 @@ export async function initCategoryAxis() {
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext, {
         drawMajorGridLines: true,
         drawMinorGridLines: true,
-        axisTitle: "Y Axis",
+        axisTitle: "Numeric Y Axis",
         axisAlignment: EAxisAlignment.Left,
-        autoRange: EAutoRange.Once
+        autoRange: EAutoRange.Once,
     }));
 
-    // Note: a category axis will require a series to be able to draw
-    sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
-        dataSeries: new XyDataSeries(wasmContext, {
-            xValues: [1E5,1E7,1E9,1E11],
-            yValues: [2,3,4,5]
-        })
-    }))
+    // Note: a category axis will require a series to be able to draw unless defaultXStart, defaultXStep or defaultXValues are defined
+    // sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+    //     dataSeries: new XyDataSeries(wasmContext, {
+    //         xValues: [1E5,1E7,1E9,1E11],
+    //         yValues: [2,3,4,5]
+    //     })
+    // }))
 }
