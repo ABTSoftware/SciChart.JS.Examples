824ea07425d3586ea4912b65c14858c32a3fb971
andyb1979 <andrew@scichart.com>
2022-08-18 19:54:22 +0100

    chore(documentation) examples of overriding getText in labelProviders

---
+++ Sandbox/DocumentationSnippets/DataLabels/src/example7.js
@@ -0,0 +1,43 @@
+import {LineSeriesDataLabelProvider} from "../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/RenderableSeries/DataLabels/LineSeriesDataLabelProvider";
+import {chartBuilder} from "../../../../../scichart.dev/Web/src/SciChart/lib/Builder/chartBuilder";
+import {ESeriesType} from "../../../../../scichart.dev/Web/src/SciChart/lib/types/SeriesType";
+import {EPointMarkerType} from "../../../../../scichart.dev/Web/src/SciChart/lib/types/PointMarkerType";
+
+export async function dataLabelProviderGetTextBuilderApi(divElementId) {
+
+    const { sciChartSurface, wasmContext } = await chartBuilder.buildChart(divElementId, {
+        series: {
+            type: ESeriesType.LineSeries,
+            xyData: {
+                xValues: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                yValues: [4.3, 5.3, 6, 6.3, 6, 5.2, 4.5, 4.6, 5, 6, 7, 8],
+            },
+            options: {
+                stroke: "SteelBlue",
+                strokeThickness: 3,
+                pointMarker: {
+                    type: EPointMarkerType.Ellipse,
+                    options: {
+                        stroke: "SteelBlue",
+                        fill: "LightSteelBlue",
+                        width: 10,
+                        height: 10,
+                        strokeThickness: 2
+                    }
+                },
+                dataLabels: {
+                    style: {
+                        fontFamily: "Arial",
+                        fontSize: 16,
+                        color: "#EEE"
+                    }
+                }
+            },
+        }
+    });
+
+    // Note you can access dataLabelProvider from a constructed chart as follows
+    sciChartSurface.renderableSeries.get(0).dataLabelProvider.getText = (dataLabelState) => {
+        return `[x: ${dataLabelState.xVal()}, y: ${dataLabelState.yVal()}]`;
+    };
+}
--- Sandbox/DocumentationSnippets/DataLabels/src/index.js
+++ Sandbox/DocumentationSnippets/DataLabels/src/index.js
@@ -3,9 +3,13 @@ import {dataLabelsBuilderAPIExample} from "./example2";
 import {dataLabelsPrecisionExample} from "./example3";
 import {dataLabelsMetadataExample} from "./example4";
 import {dataLabelsMetadataBuilderApiExample} from "./example5";
+import {dataLabelProviderGetText} from "./example6";
+import {dataLabelProviderGetTextBuilderApi} from "./example7";
 
 dataLabelsConstructorOptionsExample();
 dataLabelsBuilderAPIExample("scichart-div-id-2");
 dataLabelsPrecisionExample("scichart-div-id-3");
 dataLabelsMetadataExample("scichart-div-id-4");
 dataLabelsMetadataBuilderApiExample("scichart-div-id-5");
+dataLabelProviderGetText("scichart-div-id-6");
+dataLabelProviderGetTextBuilderApi("scichart-div-id-7");
