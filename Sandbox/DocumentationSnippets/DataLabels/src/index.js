0653b0699cbb3f08fe5aae4e481fd908472d301b
andyb1979 <andrew@scichart.com>
2022-08-18 18:43:24 +0100

    chore(documentation) added DataLabels examples to DocumentationSnippets

---
+++ Sandbox/DocumentationSnippets/DataLabels/src/index.js
@@ -0,0 +1,5 @@
+import {dataLabelsConstructorOptionsExample} from "./example1";
+import {dataLabelsBuilderAPIExample} from "./example2";
+
+dataLabelsConstructorOptionsExample();
+dataLabelsBuilderAPIExample("scichart-div-id-2");
