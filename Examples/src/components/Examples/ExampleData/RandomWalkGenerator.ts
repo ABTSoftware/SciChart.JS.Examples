28822b20d4d95c89c486011faa1a77662b102a0b
andyb1979 <andrew@scichart.com>
2022-10-26 19:42:27 +0100

    chore(examples): styled fan chart example

--- Examples/src/components/Examples/ExampleData/RandomWalkGenerator.ts
+++ Examples/src/components/Examples/ExampleData/RandomWalkGenerator.ts
@@ -4,9 +4,16 @@ export class RandomWalkGenerator {
     private readonly bias: number;
     private last: number;
     private i: number;
+    private _seed: number;
     constructor(bias: number = 0.01) {
         this.bias = bias;
         this.reset();
+    }
+
+    public Seed(seed: number){
+        this._seed = seed % 2147483647;
+        if (this._seed <= 0) this._seed += 2147483646;
+        return this;
     }
 
     public reset(){
@@ -17,13 +24,18 @@ export class RandomWalkGenerator {
     public getRandomWalkSeries(count: number): IXyValues {
         const xValues: number[] = [];
         const yValues: number[] = [];
+        const random = () => this._seed === undefined ? Math.random() : (this.nextSeeded() - 1) / 2147483646;
         for (let i = 0; i < count; i++) {
-            const next: number = this.last + (Math.random() - 0.5 + this.bias);
+            const next: number = this.last + (random() - 0.5 + this.bias);
             xValues.push(this.i++);
             yValues.push(next);
             this.last = next;
         }
 
         return {xValues, yValues};
+    }
+
+    private nextSeeded() {
+        return this._seed = this._seed * 16807 % 2147483647;
     }
 }
