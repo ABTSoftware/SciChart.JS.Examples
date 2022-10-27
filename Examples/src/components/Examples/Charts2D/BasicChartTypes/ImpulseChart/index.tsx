0cc74118b7e4786950d2951b6017624e779841e2
andyb1979 <andrew@scichart.com>
2022-10-27 16:00:21 +0100

    chore(examples): styled impulse chart type

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/ImpulseChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/ImpulseChart/index.tsx
@@ -1,48 +1,54 @@
 import * as React from "react";
-import { NumericAxis } from "scichart/Charting/Visuals/Axis/NumericAxis";
-import { XyDataSeries } from "scichart/Charting/Model/XyDataSeries";
-import { SciChartSurface } from "scichart";
-import { NumberRange } from "scichart/Core/NumberRange";
+import {NumericAxis} from "scichart/Charting/Visuals/Axis/NumericAxis";
+import {XyDataSeries} from "scichart/Charting/Model/XyDataSeries";
+import {SciChartSurface} from "scichart";
 
 import classes from "../../../../Examples/Examples.module.scss";
-import { EAutoRange } from "scichart/types/AutoRange";
-import { FastImpulseRenderableSeries } from "scichart/Charting/Visuals/RenderableSeries/FastImpulseRenderableSeries";
-import { ZoomPanModifier } from "scichart/Charting/ChartModifiers/ZoomPanModifier";
-import { ZoomExtentsModifier } from "scichart/Charting/ChartModifiers/ZoomExtentsModifier";
-import { MouseWheelZoomModifier } from "scichart/Charting/ChartModifiers/MouseWheelZoomModifier";
+import {FastImpulseRenderableSeries} from "scichart/Charting/Visuals/RenderableSeries/FastImpulseRenderableSeries";
+import {ZoomPanModifier} from "scichart/Charting/ChartModifiers/ZoomPanModifier";
+import {ZoomExtentsModifier} from "scichart/Charting/ChartModifiers/ZoomExtentsModifier";
+import {MouseWheelZoomModifier} from "scichart/Charting/ChartModifiers/MouseWheelZoomModifier";
+import {appTheme} from "../../../theme";
+import {EAnimationType} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/types/AnimationType";
+import {
+    EllipsePointMarker
+} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/PointMarkers/EllipsePointMarker";
+import {
+    TrianglePointMarker
+} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/PointMarkers/TrianglePointMarker";
+import {NumberRange} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Core/NumberRange";
+import {
+    SquarePointMarker
+} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/PointMarkers/SquarePointMarker";
 
 const divElementId = "chart";
 
 const drawExample = async () => {
-    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId);
-    sciChartSurface.xAxes.add(
-        new NumericAxis(wasmContext, { autoRange: EAutoRange.Never, visibleRange: new NumberRange(0, 100) })
-    );
-    sciChartSurface.yAxes.add(
-        new NumericAxis(wasmContext, { autoRange: EAutoRange.Never, visibleRange: new NumberRange(-3, 2) })
-    );
+    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId, { theme: appTheme.SciChartJsTheme });
+    sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { axisTitle: "X Axis" }));
+    sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: "Y Axis", growBy: new NumberRange(0.1, 0.1) }));
 
-    const dataSeries = new XyDataSeries(wasmContext);
     const xValues = [];
     const yValues = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 70; i++) {
         xValues.push(i);
-        yValues.push(Math.sin(i * 0.2) * Math.log(i / 100));
+        yValues.push(Math.sin(i * 0.2) * -Math.log(i / 100));
     }
-    dataSeries.appendRange(xValues, yValues);
+
     const impulseSeries = new FastImpulseRenderableSeries(wasmContext, {
-        fill: "#26c6da",
-        dataSeries
+        fill: appTheme.VividPink,
+        strokeThickness: 2,
+        pointMarker: new EllipsePointMarker(wasmContext, { width: 1, height: 1 }),
+        dataSeries: new XyDataSeries(wasmContext, { xValues, yValues }),
+        animation: { type: EAnimationType.Wave, options: { duration: 500, delay: 200, fadeEffect: true }}
     });
     sciChartSurface.renderableSeries.add(impulseSeries);
 
     sciChartSurface.chartModifiers.add(new ZoomPanModifier());
     sciChartSurface.chartModifiers.add(new ZoomExtentsModifier());
-
     sciChartSurface.chartModifiers.add(new MouseWheelZoomModifier());
-    return {
-        sciChartSurface
-    }
+
+    return { sciChartSurface }
 };
 
 // React component needed as our examples app is react.
