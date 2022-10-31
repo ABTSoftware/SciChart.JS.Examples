c1e83a22d8e9465775d54c22b1bc90ec51a5193c
andyb1979 <andrew@scichart.com>
2022-10-31 16:35:46 +0000

    chore(examples): Digital line chart styling

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/DigitalLineChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/DigitalLineChart/index.tsx
@@ -11,29 +11,42 @@ import { ScaleAnimation } from "scichart/Charting/Visuals/RenderableSeries/Anima
 import { ShadowEffect } from "scichart/Charting/Visuals/RenderableSeries/ShadowEffect";
 import { Point } from "scichart/Core/Point";
 import classes from "../../../../Examples/Examples.module.scss";
+import {RandomWalkGenerator} from "../../../ExampleData/RandomWalkGenerator";
+import {appTheme} from "../../../theme";
+import {
+    EllipsePointMarker
+} from "../../../../../../../../scichart.dev/Web/src/SciChart/lib/Charting/Visuals/PointMarkers/EllipsePointMarker";
 
 const divElementId = "chart";
 
 const drawExample = async () => {
     // Create a SciChartSurface
-    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId);
+    const { sciChartSurface, wasmContext } = await SciChartSurface.create(divElementId, {
+        theme: appTheme.SciChartJsTheme
+    });
 
     // Create the X,Y Axis
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { growBy: new NumberRange(0.05, 0.05) }));
 
     // Create some xValues, yValues arrays
-    const xValues = Array.from({length: 100}, (x,i) => i);
-    const yValues = xValues.map(x => Math.sin(x * 0.1));
+    const { xValues, yValues } = new RandomWalkGenerator().Seed(1337).getRandomWalkSeries(25);
 
     // Create and add a line series to the chart
     sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
         dataSeries: new XyDataSeries(wasmContext, { xValues, yValues }),
-        stroke: "#ff6600",
+        stroke: appTheme.VividPink,
         strokeThickness: 3,
+        // Flag isDigitalLine turns a normal line series into a digital line series
         isDigitalLine: true,
-        animation: new ScaleAnimation({ duration: 1000, fadeEffect: true }),
-        effect: new ShadowEffect(wasmContext, { brightness: 1, offset: new Point(5,-10), range: 0.7})
+        pointMarker: new EllipsePointMarker(wasmContext, {
+            width: 11,
+            height: 11,
+            fill: appTheme.VividPink,
+            stroke: appTheme.PaleSkyBlue,
+            strokeThickness: 2,
+        }),
+        animation: new ScaleAnimation({ zeroLine: -1, duration: 500, fadeEffect: true }),
     }));
 
     // OPTIONAL: Add some interactivity modifiers
