2cbcd8dce0e5a7c5fd0393e14bae6ecf3e3ce9f9
andyb1979 <andrew@scichart.com>
2022-11-01 23:12:21 +0000

    chore(examples): pie chart example styling

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/PieChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/PieChart/index.tsx
@@ -8,169 +8,108 @@ import {makeStyles} from "@material-ui/core/styles";
 import {appTheme} from "../../../theme";
 
 export const divElementId1 = "chart1";
-export const divElementId2 = "chart2";
-export const divElementId3 = "chart3";
-export const divElementId4 = "chart4";
 
 export const drawExample = async () => {
 
-    // Helper function to create a gradient for pie segments
-    const createGradient = (color1: string, color2: string) => {
-        return new GradientParams(new Point(0, 0), new Point(0, 1), [
-            { color: color1, offset: 0 },
-            { color: color2, offset: 1 }
-        ]);
-    };
+    // Create the pie chart
+    const sciChartPieSurface = await SciChartPieSurface.create(divElementId1, {
+        theme: appTheme.SciChartJsTheme,
+        pieType: EPieType.Pie,
+        animate: true,
+        showLegend: true,
+        showLegendCheckBoxes: true
+    });
 
-    const justPieChart = async () => {
-        const sciChartPieSurface = await SciChartPieSurface.create(divElementId1, {
-            theme: appTheme.SciChartJsTheme,
-            pieType: EPieType.Pie,
-            animate: true,
-            showLegend: true,
-            showLegendCheckBoxes: false,
-            animateLegend: true,
-        });
+    const pieSegment1 = new PieSegment({
+        value: 40,
+        text: "Green",
+        colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
+            { color: appTheme.VividSkyBlue, offset: 0 },
+            { color: appTheme.VividSkyBlue + "77", offset: 1 }
+        ]),
+        isSelected: true,
+    });
 
-        sciChartPieSurface.pieSegments.add(
-            new PieSegment({ value: 10, text: "Blue", colorLinearGradient: createGradient(appTheme.VividSkyBlue,appTheme.VividTeal)}),
-            new PieSegment({ value: 10, text: "Red", colorLinearGradient: createGradient(appTheme.VividPink,appTheme.VividPurple)}),
-            new  PieSegment({ value: 10, text: "Blue", colorLinearGradient: createGradient(appTheme.VividOrange,appTheme.MutedOrange)}),
-            new PieSegment({ value: 10, text: "Orange", colorLinearGradient: createGradient(appTheme.VividGreen,appTheme.VividSkyBlue)})
-            // newSegment(10, appTheme.VividSkyBlue,appTheme.VividTeal, "Blue"),
-            // newSegment(10, "#DD5E89","#F7BB97", "Red"),
-            // newSegment(10, "#1FA2FF","#A6FFCB", "Blue"),
-            // newSegment(10, "#F09819","#EDDE5D", "Orange"),
-        );
-        return sciChartPieSurface;
-    };
+    const pieSegment2 = new PieSegment({
+        value: 10,
+        text: "Red",
+        colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
+            { color: appTheme.VividPink, offset: 0 },
+            { color: appTheme.VividPink + "77", offset: 1 }
+        ])
+    });
 
-    const variableRadiusPie = async () => {
-        const sciChartPieSurface = await SciChartPieSurface.create(divElementId2, {
-            theme: appTheme.SciChartJsTheme,
-            pieType: EPieType.Pie,
-            animate: false,
-            showLegend: false,
-        });
+    const pieSegment3 = new PieSegment({
+        value: 20,
+        text: "Blue",
+        colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
+            { color: appTheme.VividOrange, offset: 0 },
+            { color: appTheme.VividOrange + "77", offset: 1 }
+        ])
+    });
 
-        const newSegmentWithRadius = (value: number, color1: string, color2: string, segmentName: string, radiusAdjustment: number) => {
-            return new PieSegment({
-                value,
-                text: segmentName,
-                radiusAdjustment,
-                colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
-                    { color: color1, offset: 0 },
-                    { color: color2, offset: 1 }
-                ])
-            });
-        }
+    const pieSegment4 = new PieSegment({
+        value: 15,
+        text: "Orange",
+        colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
+            { color: appTheme.VividGreen, offset: 0 },
+            { color: appTheme.VividGreen + "77", offset: 1 }
+        ])
+    });
 
-        sciChartPieSurface.pieSegments.add(
-            newSegmentWithRadius(40, "#1D976C","#93F9B9", "Green", 1),
-            newSegmentWithRadius(10, "#DD5E89","#F7BB97", "Red", 1.1),
-            newSegmentWithRadius(20, "#1FA2FF","#A6FFCB", "Blue", 1.2),
-            newSegmentWithRadius(15, "#F09819","#EDDE5D", "Orange", 1.3),
-        );
-        return sciChartPieSurface;
-    };
+    sciChartPieSurface.pieSegments.add(
+        pieSegment1, pieSegment2, pieSegment3, pieSegment4
+    );
 
-    const dynamicPie = async () => {
-        const sciChartPieSurface = await SciChartPieSurface.create(divElementId3, {
-            theme: appTheme.SciChartJsTheme,
-            pieType: EPieType.Pie,
-            animate: false,
-            showLegend: false,
-        });
+    // const selectSegments = () => {
+    //     console.log("Selecting segments state");
+    //     setTimeout(() => pieSegment1.isSelected = true, 0);
+    //     setTimeout(() => pieSegment2.isSelected = true, 1000);
+    //     setTimeout(() => pieSegment3.isSelected = true, 2000);
+    //     setTimeout(() => pieSegment4.isSelected = true, 3000);
+    //     setTimeout(() => {
+    //         pieSegment1.isSelected = false;
+    //         pieSegment2.isSelected = false;
+    //         pieSegment3.isSelected = false;
+    //         pieSegment4.isSelected = false;
+    //     }, 4000);
+    // };
+    //
+    // const adjustRadius = () => {
+    //     console.log("adjustRadius state");
+    // };
 
-        const newSegment = (value: number, color1: string, color2: string, segmentName: string) => {
-            return new PieSegment({
-                value,
-                text: segmentName,
-                colorLinearGradient: new GradientParams(new Point(0, 0), new Point(0, 1), [
-                    { color: color1, offset: 0 },
-                    { color: color2, offset: 1 }
-                ])
-            });
-        }
+    // let currentState = 0;
+    // const allStates = [selectSegments, adjustRadius];
+    //
+    // const updateState = () => {
+    //     allStates[currentState++]();
+    //     if (currentState >= allStates.length) {
+    //         currentState = 0;
+    //     }
+    //     setTimeout(updateState, 5000);
+    // }
 
-        sciChartPieSurface.pieSegments.add(
-            newSegment(50, "#1D976C","#93F9B9", "Green"),
-            newSegment(50, "#DD5E89","#F7BB97", "Red"),
-        );
+    // updateState();
 
-        // const updateChart = () => {
-        //     pieSegment1.value = value;
-        //     pieSegment2.value = 100 - value;
-        //     setTimeout(updateChart, 1000);
-        // };
-        // updateChart();
-
-        return sciChartPieSurface;
-    };
-
-    const legendPie = async () => {
-
-    };
-
-
-    const charts = await Promise.all([justPieChart(), variableRadiusPie(), dynamicPie()
-        // legendPie(), dynamicPie(),
-    ]);
-    return { charts };
+    return sciChartPieSurface;
 };
 
-// Styles for the 2x2 grid
-const useStyles = makeStyles(theme => ({
-    flexOuterContainer: {
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        background: appTheme.DarkIndigo,
-    },
-    flexContainerRow: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignContent: "stretch",
-        margin: 10,
-        height: "50%",
-        width: "calc(100% - 10)",
-        // background: "#00ff0033",
-    },
-    item: {
-        height: "100%",
-        width: "49%",
-        // background: "#FF000033",
-    }
-}));
-
 export default function PieChart() {
-    const [charts, setCharts] = React.useState<SciChartPieSurface[]>();
+    const [chart, setChart] = React.useState<SciChartPieSurface>();
 
     React.useEffect(() => {
         (async () => {
             const res = await drawExample();
-            setCharts(res.charts);
+            setChart(res);
         })();
         // Deleting sciChartSurface to prevent memory leak
-        return () => charts?.forEach(chart => chart?.delete());
+        return () => chart?.delete();
     }, []);
-
-    const localClasses = useStyles();
 
     return (
         <div className={classes.ChartWrapper}>
-            <div className={localClasses.flexOuterContainer}>
-                <div className={localClasses.flexContainerRow}>
-                    <div id={divElementId1} className={localClasses.item}/>
-                    <div id={divElementId2} className={localClasses.item}/>
-                </div>
-                <div className={localClasses.flexContainerRow}>
-                    <div id={divElementId3} className={localClasses.item}/>
-                    <div id={divElementId4} className={localClasses.item}/>
-                </div>
-            </div>
+            <div id={divElementId1} style={{ width: "100%", height: "100%" }}/>
         </div>
     );
 }
