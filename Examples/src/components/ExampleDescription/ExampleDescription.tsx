8e55e8557bb0173f093ece175a1d3d1b528824f3
andyb1979 <andrew@scichart.com>
2022-11-24 15:05:50 +0000

    chore(examples) Removed documentation, description, tips from example root. Put subtitle under title.

--- Examples/src/components/ExampleDescription/ExampleDescription.tsx
+++ Examples/src/components/ExampleDescription/ExampleDescription.tsx
@@ -12,32 +12,32 @@ type TProps = {
 export default function ExampleDescription(props: TProps) {
     return (
         <div>
-            <div className={classes.ExampleInfoText}>
-                {props.previewDescription && <p>{props.previewDescription}</p>}
-                <p>{props.description}</p>
-            </div>
-            {props.tips && (
-                <div className={classes.UsefulLinksWrapper}>
-                    <h4>Tips!</h4>
-                    {props.tips.map((item, index) => (
-                        <p key={index + item}>{item}</p>
-                    ))}
-                </div>
-            )}
-            <div className={classes.UsefulLinksWrapper}>
-                <h4>Documentation Links</h4>
-                <ul>
-                    {props.documentationLinks.map((item, index) => {
-                        return (
-                            <li key={index + item.href}>
-                                <a href={item.href} title={item.title}>
-                                    {item.linkTitle}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
+            {/*<div className={classes.ExampleInfoText}>*/}
+            {/*    {props.previewDescription && <p>{props.previewDescription}</p>}*/}
+            {/*    <p>{props.description}</p>*/}
+            {/*</div>*/}
+            {/*{props.tips && (*/}
+            {/*    <div className={classes.UsefulLinksWrapper}>*/}
+            {/*        <h4>Tips!</h4>*/}
+            {/*        {props.tips.map((item, index) => (*/}
+            {/*            <p key={index + item}>{item}</p>*/}
+            {/*        ))}*/}
+            {/*    </div>*/}
+            {/*)}*/}
+            {/*<div className={classes.UsefulLinksWrapper}>*/}
+            {/*    <h4>Documentation Links</h4>*/}
+            {/*    <ul>*/}
+            {/*        {props.documentationLinks.map((item, index) => {*/}
+            {/*            return (*/}
+            {/*                <li key={index + item.href}>*/}
+            {/*                    <a href={item.href} title={item.title}>*/}
+            {/*                        {item.linkTitle}*/}
+            {/*                    </a>*/}
+            {/*                </li>*/}
+            {/*            );*/}
+            {/*        })}*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
         </div>
     );
 }
