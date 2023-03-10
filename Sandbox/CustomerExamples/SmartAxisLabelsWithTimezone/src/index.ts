3353c4fa84c001754b096addc1297ce13c533282
klishevich <busation@gmail.com>
2023-03-10 14:51:28 +0300

    feat(sandbox): add timezone and locale selects to Smart Axis Labels example

--- Sandbox/CustomerExamples/SmartAxisLabelsWithTimezone/src/index.ts
+++ Sandbox/CustomerExamples/SmartAxisLabelsWithTimezone/src/index.ts
@@ -8,7 +8,8 @@ import {
     EAutoRange,
     EColor,
     FastLineRenderableSeries,
-    CursorModifier
+    CursorModifier,
+    ZoomExtentsModifier
 } from 'scichart';
 import { axisLabelDateFormatter } from './axisLabelDateFormatter';
 import { DateTickProvider } from './DateTickProvider';
@@ -16,7 +17,9 @@ import { SmartDateTZLabelProvider } from './SmartDateTZLabelProvider';
 import { createDateTzConverter } from './timezones';
 import { createDataSeries } from './createDataSeries';
 
-async function initSciChart(timeZone: string, locale: string) {
+async function initSciChart() {
+    const settings = { timezone: 'utc', locale: 'en' };
+
     const { sciChartSurface, wasmContext } = await SciChartSurface.create('scichart-root');
     const xAxis = new NumericAxis(wasmContext, {
         growBy: new NumberRange(0.05, 0.05),
@@ -31,43 +34,67 @@ async function initSciChart(timeZone: string, locale: string) {
     // Overrides getDeltaFromRange to get nice major and minor deltas for the ticks
     xAxis.deltaCalculator.getDeltaFromRange = axisLabelDateFormatter.getDeltaFromRange;
 
-    // Calculate ticks which respect the time zone
-    xAxis.tickProvider = new DateTickProvider(wasmContext, timeZone);
+    const updateSettings = (xAxis$: NumericAxis) => {
+        const { timezone, locale } = settings;
+        // Calculate ticks which respect the time zone
+        xAxis$.tickProvider = new DateTickProvider(wasmContext, timezone);
+        // Draws nice axis labels timezone and locale specific
+        xAxis$.labelProvider = new SmartDateTZLabelProvider(timezone, locale);
+        const dateTzConverter = createDateTzConverter(timezone);
+        // This is needed for Cursor and Rollover chart modifiers
+        xAxis$.labelProvider.formatCursorLabel = timestamp =>
+            axisLabelDateFormatter.toFullDateTime(timestamp, dateTzConverter, locale);
+    };
+    updateSettings(xAxis);
 
-    // Draws nice axis labels timezone and locale specific
-    xAxis.labelProvider = new SmartDateTZLabelProvider(timeZone, locale);
-
-    const dateTzConverter = createDateTzConverter(timeZone);
-
-    // This is needed for Cursor and Rollover chart modifiers
-    xAxis.labelProvider.formatCursorLabel = timestamp =>
-        axisLabelDateFormatter.toFullDateTime(timestamp, dateTzConverter, locale);
     sciChartSurface.xAxes.add(xAxis);
 
-    const yAxis1 = new NumericAxis(wasmContext, {
-        growBy: new NumberRange(0.1, 0.1),
-        axisAlignment: EAxisAlignment.Left,
-        labelStyle: { color: EColor.Orange },
-        cursorLabelPrecision: 4,
-        visibleRange: new NumberRange(-2.2, 2.2),
-        autoRange: EAutoRange.Never,
-        visibleRangeSizeLimit: new NumberRange(0.1, 1000),
-        useSharedCache: false,
-        useNativeText: true
-    });
-    yAxis1.labelProvider.precision = 2;
-    sciChartSurface.yAxes.add(yAxis1);
+    sciChartSurface.yAxes.add(
+        new NumericAxis(wasmContext, {
+            growBy: new NumberRange(0.1, 0.1),
+            axisAlignment: EAxisAlignment.Left,
+            labelStyle: { color: EColor.Orange },
+            cursorLabelPrecision: 4,
+            visibleRange: new NumberRange(-2.2, 2.2),
+            autoRange: EAutoRange.Never,
+            visibleRangeSizeLimit: new NumberRange(0.1, 1000),
+            useSharedCache: false,
+            useNativeText: true,
+            labelPrecision: 2
+        })
+    );
 
-    const firstSeriesData = createDataSeries(wasmContext, 0, { dataSeriesName: 'Sinewave Green' });
-    const renderableSeries1 = new FastLineRenderableSeries(wasmContext, {
+    const dataSeries = createDataSeries(wasmContext, 0, { dataSeriesName: 'Sinewave Green' });
+    const renderableSeries = new FastLineRenderableSeries(wasmContext, {
         stroke: EColor.Orange,
         strokeThickness: 3,
-        dataSeries: firstSeriesData,
+        dataSeries,
         isVisible: true
     });
-    sciChartSurface.renderableSeries.add(renderableSeries1);
+    sciChartSurface.renderableSeries.add(renderableSeries);
 
-    sciChartSurface.chartModifiers.add(new ZoomPanModifier(), new MouseWheelZoomModifier(), new CursorModifier());
+    sciChartSurface.chartModifiers.add(
+        new ZoomPanModifier(),
+        new MouseWheelZoomModifier(),
+        new CursorModifier(),
+        new ZoomExtentsModifier()
+    );
+
+    // Subscribe to timezone changes
+    const timezoneSelect = document.getElementById('timezones') as HTMLSelectElement;
+    timezoneSelect.addEventListener('input', () => {
+        settings.timezone = timezoneSelect.options[timezoneSelect.selectedIndex].value;
+        console.log(settings);
+        updateSettings(xAxis);
+    });
+
+    // Subscribe to locale changes
+    const localeSelect = document.getElementById('locales') as HTMLSelectElement;
+    localeSelect.addEventListener('input', () => {
+        settings.locale = localeSelect.options[localeSelect.selectedIndex].value;
+        console.log(settings);
+        updateSettings(xAxis);
+    });
 }
 
-initSciChart('Asia/Istanbul', 'de-DE');
+initSciChart();
