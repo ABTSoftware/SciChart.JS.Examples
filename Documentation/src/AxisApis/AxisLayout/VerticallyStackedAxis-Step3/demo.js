44e67a1cd284b2237d650b882a72b98a4e8f4d94
andyb1979 <aburnettthompson@gmail.com>
2023-03-23 14:52:02 +0000

    Improved the vertically stacked axis docs

--- Documentation/src/AxisApis/AxisLayout/VerticallyStackedAxis-Step3/demo.js
+++ Documentation/src/AxisApis/AxisLayout/VerticallyStackedAxis-Step3/demo.js
@@ -15,11 +15,6 @@ async function verticallyStackedAxis(divElementId) {
     theme: new SciChartJsNavyTheme()
   });
 
-  // #region ExampleA
-  sciChartSurface.layoutManager.leftOuterAxesLayoutStrategy
-      = new LeftAlignedOuterVerticallyStackedAxisLayoutStrategy();
-  // #endregion
-
   // Create an XAxis on the bottom
   sciChartSurface.xAxes.add(new NumericAxis(wasmContext, {
     axisTitle: "X Axis",
@@ -28,23 +23,31 @@ async function verticallyStackedAxis(divElementId) {
     axisBorder: { color: "#50C7E0", borderTop: 1 }
   }));
 
-  // Create several YAxis on the left
-  // Creating a NumericAxis as a YAxis on the left
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis0", axisTitle: "Y Axis 0", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis1", axisTitle: "Y Axis 1", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis2", axisTitle: "Y Axis 2", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis3", axisTitle: "Y Axis 3", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis4", axisTitle: "Y Axis 4", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis5", axisTitle: "Y Axis 5", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis6", axisTitle: "Y Axis 6", axisAlignment: EAxisAlignment.Left }));
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis7", axisTitle: "Y Axis 7", axisAlignment: EAxisAlignment.Left }));
+  // #region ExampleA
+  sciChartSurface.layoutManager.leftOuterAxesLayoutStrategy
+      = new LeftAlignedOuterVerticallyStackedAxisLayoutStrategy();
+
+  // ...
+
+  // Create several YAxis on the left using stackedAxisLength
+  const axisAlignment = EAxisAlignment.Left;
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis0", axisAlignment, axisTitle: "25% Size", stackedAxisLength: "25%" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis1", axisAlignment, axisTitle: "Default" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis2", axisAlignment, axisTitle: "Default" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis3", axisAlignment, axisTitle: "Default" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis4", axisAlignment, axisTitle: "100 pixels", stackedAxisLength: 100 }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis5", axisAlignment, axisTitle: "Default" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis6", axisAlignment, axisTitle: "Default" }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { id: "YAxis7", axisAlignment, axisTitle: "Default" }));
+  // #endregion
 
   // To make it clearer what's happening, colour the axis backgrounds & borders
   const axisColors = ["#50C7E0", "#EC0F6C", "#30BC9A", "#F48420", "#364BA0", "#882B91", "#67BDAF", "#C52E60"];
   sciChartSurface.yAxes.asArray().forEach((yAxis, index) => {
     yAxis.backgroundColor = axisColors[index] + "22";
     yAxis.axisBorder = { color: axisColors[index], borderRight: 1 };
-    yAxis.axisTitleStyle.fontSize = 13;
+    yAxis.axisTitleStyle.fontSize = yAxis.stackedAxisLength === undefined ? 13 : 15;
+    yAxis.axisTitleStyle.color = yAxis.stackedAxisLength === undefined ? "#FFFFFF77" : "#FFFFFF";
   });
 
   // Let's add some series to the chart to show how they also behave with axis
