a3c529e19acd8def04bc04576c43e5a8b820cf6e
andyb1979 <aburnettthompson@gmail.com>
2023-07-17 23:06:05 +0100

    chore(demo): Updated the Vue.js boilerplate and readme

--- Sandbox/demo-vue-scichart/README.md
+++ Sandbox/demo-vue-scichart/README.md
@@ -1,16 +1,25 @@
-# vue-scichart-demo
+# SciChart.js Vue Sample
 
-Vue 3.0 project with TypeScript
+## Licensing
 
-## Trial licensing
+SciChart.js is commercial software with a [free community license](https://scichart.com/community-licensing).
 
-Ensure you have followed steps from our [getting-started](https://www.scichart.com/getting-started-scichart-js) guide to get a trial!
+- From SciChart.js v3.2 and onwards, trial licenses are not required. Instead the chart initialises with a [Community License](https://scichart.com/community-licensing)
+- For commercial licensing, follow steps from [scichart.com/licensing-scichart-js](https://scichart.com/licensing-scichart-js).
 
-## Wasm file deployment
 
-SciChart.js has a Wasm (webAssembly) and Data file which must be deployed to output folders for correct operation of our Js chart library.
+## Step 1: Adding SciChart to your React Application
 
-Vue.js requires *.data file to be in the /public output folder, and *.wasm files to be in the /public/js folder. This is done using the copy-files-to-from npm package:
+If you haven't already done so, add SciChart.js to your react application
+```javascript
+npm install scichart 
+```
+
+## Step 2: Wasm file deployment
+
+SciChart.js uses WebAssembly files which must be served. The easiest way to do this is to copy the wasm files from the node_modules/scichart/_wasm folder to your output folder.
+
+Vue.js requires *.data file to be in the /public output folder, and *.wasm files to be in the /public/js folder. This is done using the `copy-files-to-from` npm package:
 
 ```
  // copy-files-to-from.json
@@ -53,7 +62,89 @@ Vue.js requires *.data file to be in the /public output folder, and *.wasm files
   }
 ```
 
-## Project setup
+## Step 3: Creating the chart
+
+Next, create a function to initialize a SciChartSurface like this:
+
+```javascript
+import {
+  SciChartSurface,
+  NumericAxis,
+  FastLineRenderableSeries,
+  XyDataSeries,
+  EllipsePointMarker,
+  SweepAnimation,
+  SciChartJsNavyTheme,
+  NumberRange
+} from "scichart";
+
+async function initSciChart() {
+  // LICENSING
+  // Commercial licenses set your license code here
+  // Purchased license keys can be viewed at https://www.scichart.com/profile
+  // How-to steps at https://www.scichart.com/licensing-scichart-js/
+  // SciChartSurface.setRuntimeLicenseKey("YOUR_RUNTIME_KEY");
+
+  // Initialize SciChartSurface. Don't forget to await!
+  const { sciChartSurface, wasmContext } = await SciChartSurface.create("scichart-root", {
+    theme: new SciChartJsNavyTheme(),
+    title: "SciChart.js First Chart",
+    titleStyle: { fontSize: 22 }
+  });
+
+  // Create an XAxis and YAxis with growBy padding
+  const growBy = new NumberRange(0.1, 0.1);
+  sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { axisTitle: "X Axis", growBy }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: "Y Axis", growBy }));
+
+  // Create a line series with some initial data
+  sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+    stroke: "steelblue",
+    strokeThickness: 3,
+    dataSeries: new XyDataSeries(wasmContext, {
+      xValues: [0,1,2,3,4,5,6,7,8,9],
+      yValues: [0, 0.0998, 0.1986, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7173, 0.7833]
+    }),
+    pointMarker: new EllipsePointMarker(wasmContext, { width: 11, height: 11, fill: "#fff" }),
+    animation: new SweepAnimation({ duration: 300, fadeEffect: true })
+  }));
+
+  return sciChartSurface;
+}
+```
+
+## Step 4: Create a Vue Component
+
+Charts can be initialized in a Vue Component and placed in a `<div>` element. Make sure to delete the chart on component unmount.
+
+```javascript
+export default defineComponent({
+  // Best practise in Vue.js is to ensure that sciChartSurface is deleted on component unmount.
+  // Here's one way to do this
+  data() {
+    return {
+      chartInitializationPromise: undefined,
+    };
+  },
+  mounted() {
+    console.log("SciChart2d.vue onMounted");
+    this.chartInitializationPromise = initSciChart();
+  },
+  beforeUnmount() {
+    console.log("SciChart2d.vue beforeUnmount");
+    this.chartInitializationPromise.then((sciChartSurface) => {
+      console.log("..deleting sciChartSurface");
+      sciChartSurface.delete();
+    });
+  },
+  name: "scichart2d",
+  props: {
+    msg: String
+  }
+});
+```
+
+## Using this Project 
 ```
 npm install
 ```
@@ -83,8 +174,7 @@ We have a wealth of information on our site showing how to get started with SciC
 
 Take a look at:
 
-* [Getting-Started with SciChart.js](https://www.scichart.com/getting-started-scichart-js): includes trial licensing, first steps and more
+* [Getting-Started with SciChart.js](https://www.scichart.com/getting-started-scichart-js): includes community licensing details, first steps and more
 * [Javascript / npm tutorials](https://www.scichart.com/documentation/js/current/Tutorial%2002%20-%20Adding%20Series%20and%20Data.html): using npm, webpack, and scichart.js, create static and dynamic charts with zooming, panning tooltips and more
 * [Vanilla Javascript tutorials](https://www.scichart.com/documentation/js/current/Tutorial%2001%20-%20Including%20SciChart.js%20in%20an%20HTML%20Page.html): using only vanilla javascript and HTML,
 * [Official scichart.js demos](https://demo.scichart.com): view our demos online! Full github source code also available at [github.com/abtsoftware/scichart.js.examples](https://github.com/abtsoftware/scichart.js.examples)
-
--- Sandbox/demo-vue-scichart/src/components/Scichart3d.vue
+++ Sandbox/demo-vue-scichart/src/components/Scichart3d.vue
@@ -6,69 +6,121 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
-import { CameraController } from "scichart/Charting3D/CameraController";
-import { MouseWheelZoomModifier3D } from "scichart/Charting3D/ChartModifiers/MouseWheelZoomModifier3D";
-import { OrbitModifier3D } from "scichart/Charting3D/ChartModifiers/OrbitModifier3D";
-import { XyzDataSeries3D } from "scichart/Charting3D/Model/DataSeries/XyzDataSeries3D";
-import { Vector3 } from "scichart/Charting3D/Vector3";
-import { NumericAxis3D } from "scichart/Charting3D/Visuals/Axis/NumericAxis3D";
+import { defineComponent } from "vue";
 import {
-    QuadPointMarker,
-    SpherePointMarker3D
-} from "scichart/Charting3D/Visuals/PointMarkers/DefaultPointMarkers";
-import { ScatterRenderableSeries3D } from "scichart/Charting3D/Visuals/RenderableSeries/ScatterRenderableSeries3D";
-import { SciChart3DSurface } from "scichart/Charting3D/Visuals/SciChart3DSurface";
+  CameraController,
+  MouseWheelZoomModifier3D,
+  OrbitModifier3D,
+  Vector3,
+  NumericAxis3D,
+  SciChart3DSurface,
+  SciChartJsNavyTheme,
+  ResetCamera3DModifier,
+  NumberRange,
+  EMeshPaletteMode,
+  EDrawMeshAs,
+  SurfaceMeshRenderableSeries3D,
+  GradientColorPalette,
+  UniformGridDataSeries3D
+} from "scichart";
 
-async function initSciChart() {
-    const { sciChart3DSurface, wasmContext } = await SciChart3DSurface.create("chart3d");
-
-    sciChart3DSurface.camera = new CameraController(wasmContext, {
-        position: new Vector3(300, 300, 300),
-        target: new Vector3(0, 50, 0)
-    });
-
-    sciChart3DSurface.chartModifiers.add(new MouseWheelZoomModifier3D());
-    sciChart3DSurface.chartModifiers.add(new OrbitModifier3D());
-
-    sciChart3DSurface.xAxis = new NumericAxis3D(wasmContext, { axisTitle: "X Axis" });
-    sciChart3DSurface.yAxis = new NumericAxis3D(wasmContext, { axisTitle: "Y Axis" });
-    sciChart3DSurface.zAxis = new NumericAxis3D(wasmContext, { axisTitle: "Z Axis" });
-
-    const defaultPointMarker = new SpherePointMarker3D(wasmContext, { size: 10, fill: "#00FF00" });
-    const series = new ScatterRenderableSeries3D(wasmContext, { pointMarker: defaultPointMarker });;
-    const xyzDataSeries = new XyzDataSeries3D(wasmContext);
-
-    const count = 1000;
-    for (let i = 0; i < count; i++) {
-         const x = getGaussianRandom(200, 40);
-         const y = getGaussianRandom(200, 40);
-         const z = getGaussianRandom(200, 40);
-
-        const scale = (Math.random() + 0.5) * 0.5;
-        const randomColor = Math.floor(Math.random() * 0xffffff) + 0xff000000;
-         // To declare scale and colour, add an optional PointMetadata3D type as the w (fourth) parameter.
-         // The PointMetadata3D type also has other properties defining the behaviour of the XYZ point
-         xyzDataSeries.append(x, y, z, { vertexColor: 0xffa88d32, pointScale: scale });
-     }
-     series.dataSeries = xyzDataSeries;
-     sciChart3DSurface.renderableSeries.add(series)
+// Generates some example data for this demo, returning a 2D array of numbers [zIndex][xIndex]
+const generateData = (xSize, zSize) => {
+  const heightmapArray = [];
+  const wc = xSize*0.5, hc = zSize*0.5;
+  const freq = Math.sin(0.5)*0.1 + 0.1;
+  for (let z = 0; z < zSize; z++) {
+    heightmapArray[z] = [];
+    for (let x = 0; x < xSize; x++) {
+      const radius = Math.sqrt((wc - z)*(wc - z) + (hc - x)*(hc - x));
+      const d = Math.PI*radius*freq;
+      const value = Math.sin(d)/d;
+      heightmapArray[z][x] = isNaN(value) ? 1.0 : value;
+    }
+  }
+  return heightmapArray;
 }
 
-function getGaussianRandom(mean: number, stdDev: number): number {
-    const u1 = Math.random(); // these are uniform(0,1) random doubles
-    const u2 = Math.random();
-    // random normal(0,1)
-    const randStdNormal = Math.sqrt(-2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
-    // random normal(mean, stddev^2)
-    return mean + stdDev * randStdNormal;
+
+async function initSciChart() {
+  // Create a SciChart3DSurface in the host <div id=".." />
+  const { wasmContext, sciChart3DSurface } = await SciChart3DSurface.create("chart3d", {
+    theme: new SciChartJsNavyTheme(),
+    // worldDimensions: new Vector3(300, 200, 300),
+    // cameraOptions: {
+    //   position: new Vector3(-300, 300, -300),
+    //   target: new Vector3(0, 50, 0),
+    // }
+  });
+  sciChart3DSurface.camera = new CameraController(wasmContext, {
+    position: new Vector3(-300, 300, -300),
+    target: new Vector3(0, 50, 0),
+  });
+
+  // Add mouse interactivity
+  sciChart3DSurface.chartModifiers.add(new OrbitModifier3D(), new MouseWheelZoomModifier3D(), new ResetCamera3DModifier());
+
+  // Declare your X,Y,Z axis
+  sciChart3DSurface.xAxis = new NumericAxis3D(wasmContext, { axisTitle: "X Axis" });
+  sciChart3DSurface.yAxis = new NumericAxis3D(wasmContext, { axisTitle: "Y Axis", visibleRange: new NumberRange(0, 1) });
+  sciChart3DSurface.zAxis = new NumericAxis3D(wasmContext, { axisTitle: "Z Axis" });
+
+  // Create a 2D array and fill this with data. returns 2D array [zIndex][xIndex]
+  const heightmapArray = generateData(40, 40);
+
+  const dataSeries = new UniformGridDataSeries3D(wasmContext, {
+    yValues: heightmapArray,
+    xStep: 1, // Defines each cell in X occupies 1 data point on the X axis
+    zStep: 1, // Defines each cell in Z occupies 1 data point on the Z axis
+    dataSeriesName: "Uniform Surface Mesh"
+  });
+
+// Create the color map. GradientColorPalette maps heightMap values to a color range
+  const colorMap = new GradientColorPalette(wasmContext, {
+    gradientStops: [
+      {offset: 1, color: "#EC0F6C"},
+      {offset: 0.55, color: "#F48420"},
+      {offset: 0.3, color: "#67BDAF"},
+      {offset: 0.2, color: "#50C7E0"},
+      {offset: 0.1, color: "#264B93"},
+      {offset: 0, color: "#14233C"}
+    ],
+  });
+
+  // Finally, create a SurfaceMeshRenderableSeries3D and add to the chart
+  const series = new SurfaceMeshRenderableSeries3D(wasmContext, {
+    // Apply the Data to the series. Data can be updated dynamically
+    dataSeries,
+    minimum: 0,   // minimum value corresponds to colorMap offset=0
+    maximum: 1.0, // maximum value corresponds to colorMap offset=1
+    stroke: "White", // Wireframe stroke
+    strokeThickness: 1.5,
+    drawSkirt: false, // Draws solid wall to zero
+    drawMeshAs: EDrawMeshAs.SOLID_WIREFRAME, // Draw mesh as solid, wireframe or solid wireframe
+    meshPaletteMode: EMeshPaletteMode.HEIGHT_MAP_SOLID_CELLS, // Interpolation mode for cell colors
+    meshColorPalette: colorMap,
+  });
+
+  sciChart3DSurface.renderableSeries.add(series);
+
+  return sciChart3DSurface;
 }
 
 export default defineComponent({
-  setup() {
-    onMounted(() => {
-      console.log("execute onMounted");
-      initSciChart();
+  data() {
+    return {
+      chartInitializationPromise: undefined,
+    };
+  },
+  mounted() {
+    console.log("SciChart3d.vue onMounted");
+    this.chartInitializationPromise = initSciChart();
+  },
+  beforeUnmount() {
+    console.log("SciChart3d.vue beforeUnmount");
+    this.chartInitializationPromise.then((sciChart3DSurface) => {
+      console.log("..deleting SciChart3DSurface");
+      sciChart3DSurface.delete()
     });
   },
   name: "Scichart3d",
