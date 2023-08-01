f7f14c9b3106e802e741d551c2a1eb727d9154e0
Jim Risen <jim.dev@scichart.com>
2023-08-01 10:14:14 +0300

    fix(sandbox): upgrade dependencies of the electron example

--- Sandbox/scichart-electron-demo/forge.config.ts
+++ Sandbox/scichart-electron-demo/forge.config.ts
@@ -3,13 +3,16 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
 import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
 
 const config: ForgeConfig = {
-    packagerConfig: {},
+    packagerConfig: {
+        asar: true,
+    },
     rebuildConfig: {},
     makers: [
         new MakerSquirrel({}),
@@ -18,6 +21,7 @@ const config: ForgeConfig = {
         new MakerDeb({}),
     ],
     plugins: [
+        new AutoUnpackNativesPlugin({}),
         new WebpackPlugin({
             // this is needed to allow connection with SciChart Licensing Wizard during development
             devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
