3efd18998eacfb1ab08267a03bc6838debfe0ab0
Jim Risen <jim.dev@scichart.com>
2023-09-28 15:31:15 +0300

    fix(sandbox): SCJS-1567 fix ThresholdSlider position calculation

--- Sandbox/dashboard-demo/src/ThresholdSlider.tsx
+++ Sandbox/dashboard-demo/src/ThresholdSlider.tsx
@@ -7,22 +7,29 @@ import { Rect } from 'scichart';
 const ThresholdSlider = () => {
     // get reference to chart init result
     const context = useContext(SurfaceContext) as Awaited<ReturnType<TMainChartConfigFunc>>;
-    const [seriesViewRect, setSeriesViewRect] = useState(context.sciChartSurface.seriesViewRect)
+    const [seriesViewRect, setSeriesViewRect] = useState(context.sciChartSurface.seriesViewRect);
     const viewport = context.sciChartSurface.renderSurface.viewportSize;
 
     // subscribe to seriesViewRectChange
     useEffect(() => {
-        if (context) {
-            let previousViewRect = seriesViewRect;
-            const checkViewRectChange = () => {
-                const currentSeriesViewRect = context.sciChartSurface.seriesViewRect
-                if (!Rect.isEqual(currentSeriesViewRect, previousViewRect)) {
-                    previousViewRect = currentSeriesViewRect
-                    setSeriesViewRect(currentSeriesViewRect)
-                }
-            };
-            context.sciChartSurface.rendered.subscribe(checkViewRectChange);
+        if (!context) {
+            return null;
         }
+
+        let previousViewRect = seriesViewRect;
+        const checkViewRectChange = () => {
+            const currentSeriesViewRect = context.sciChartSurface.seriesViewRect;
+
+            if (!previousViewRect || !Rect.isEqual(currentSeriesViewRect, previousViewRect)) {
+                previousViewRect = currentSeriesViewRect;
+                setSeriesViewRect(currentSeriesViewRect);
+            }
+        };
+        context.sciChartSurface.rendered.subscribe(checkViewRectChange);
+
+        return () => {
+            context.sciChartSurface.rendered.unsubscribe(checkViewRectChange);
+        };
     }, [context]);
 
     const [width, setWidth] = useState('1600');
@@ -31,6 +38,9 @@ const ThresholdSlider = () => {
         context.updateThreshold(parseInt(event.target.value));
     };
 
+    if (!seriesViewRect) {
+        return null;
+    }
 
     return (
         <div
@@ -39,8 +49,6 @@ const ThresholdSlider = () => {
                 right: viewport.width - seriesViewRect.right,
                 position: 'absolute',
                 color: appTheme.ForegroundColor,
-                // gridArea: "1 / 4 / 2 / -1"
-                // transform: 'rotate(270deg)'
             }}
         >
             Duration Threshold
