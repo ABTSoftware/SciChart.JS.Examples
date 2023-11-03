f55312bf7495806cb90d7dcfed844d86d940000f
antichaosdb <dave@antichaos.co.uk>
2023-11-03 15:55:13 +0000

    starting multi chart examples

---
+++ Examples/src/components/Examples/Charts2D/MultiChart/SyncMultiChart/exampleInfo.tsx
@@ -0,0 +1,49 @@
+import { TExampleInfo } from "../../../../AppRouter/examplePages";
+import { githubUrl } from "./GENERATED_GITHUB_URL";
+import * as React from "react";
+import { ExampleStrings } from "../../../ExampleStrings";
+import { TDocumentationLink } from "../../../../../helpers/types/ExampleDescriptionTypes";
+import exampleImage from "./javascript-sync-multi-chart.jpg";
+
+const previewDescription = `This examples shows how to control the synchronisation of x axes for multiple vertically stacked charts`;
+const description = `Drag the box to pan the main chart, and drag the handles of the box to resize it and zoom the main chart.
+Right click and drag on the main chart to zoom.`;
+const tips = [
+    `The overview is a normal sciChartSurface so you can add annotations to it, or customise it any way you want.`,
+    `You can easily customise which series appear in the overview, and how they are rendered, with the transformRenderableSeries option.`,
+    `You can also apply the overview to vertical charts.`
+];
+
+const documentationLinks: TDocumentationLink[] = [
+    {
+        href: ExampleStrings.urlOverviewDocumentation,
+        title: ExampleStrings.urlTitleOverviewDocumentation,
+        linkTitle: "SciChart.js Overview Documentation"
+    }
+];
+
+const Subtitle = () => (
+    <p>
+        Demonstrates how to zoom and pan with an <strong>Overview Chart</strong> using SciChart.js, High Performance{" "}
+        <a href={ExampleStrings.urlJavascriptChartFeatures} target="_blank">
+            JavaScript Charts
+        </a>
+    </p>
+);
+
+export const overviewExampleInfo: TExampleInfo = {
+    onWebsite: true,
+    title: ExampleStrings.titleOverview,
+    pageTitle: ExampleStrings.titleOverview + ExampleStrings.exampleGenericTitleSuffix,
+    path: ExampleStrings.urlOverview,
+    filepath: "Charts2D/ZoomingAndPanning/OverviewModifier",
+    subtitle: Subtitle,
+    documentationLinks,
+    tips,
+    description,
+    previewDescription,
+    githubUrl,
+    metaDescription: "Demonstrates how to zoom and pan with an Overview Chart",
+    metaKeywords: "drag, axis, scale, overview, javascript, webgl, canvas",
+    thumbnailImage: exampleImage
+};
