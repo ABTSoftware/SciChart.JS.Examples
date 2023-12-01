87dd8760b11628bfe16d0dfd7b2ebb0fa0433166
antichaosdb <dave@antichaos.co.uk>
2023-12-01 16:14:53 +0000

    feat(Examples): New Draggable Event Markers demo

---
+++ Examples/src/components/Examples/FeaturedApps/ShowCases/EventMarkers/exampleInfo.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { TExampleInfo } from "../../../../AppRouter/examplePages";
+import { githubUrl } from "./GENERATED_GITHUB_URL";
+import { ExampleStrings } from "../../../ExampleStrings";
+import { TDocumentationLink } from "../../../../../helpers/types/ExampleDescriptionTypes";
+import exampleImage from "./javascript-draggable-event-markers.jpg";
+
+const Subtitle = () => (
+    <p>
+        Demonstrates how to repurpose a Candlestick Series into dragabble, labled, event markers, using SciChart.js High Performance{" "}
+        <a href={ExampleStrings.urlJavascriptChartFeatures} target="_blank">
+            JavaScript Charts
+        </a>
+    </p>
+);
+
+const previewDescription = ``;
+const description = `Demonstrates how to repurpose a Candlestick Series into dragabble, labled, event markers`;
+const tips = [
+    `Take control of the candle width by overriding series.getDataPointWidth`,
+    `FastCandleStickRenderableSeries needs to be given a properly configured DataLabelProvider in order to show labels`,
+    `DataPointSelectionPaletteProvider takes care of coloring the selected point, even when you are setting isSelected on the metaData manually`
+];
+const documentationLinks: TDocumentationLink[] = [{
+    href: ExampleStrings.urlDocumentationHome,
+    title: ExampleStrings.titleDocumentationHome,
+    linkTitle: "SciChart.js Documentation Home"
+}];
+
+export const eventMarkersExampleInfo: TExampleInfo = {
+    onWebsite: false,
+    title: ExampleStrings.titleEventMarkers,
+    pageTitle: ExampleStrings.titleEventMarkers + ExampleStrings.exampleGenericTitleSuffix,
+    path: ExampleStrings.urlEventMarkers,
+    filepath: "FeaturedApps/ShowCases/EventMarkers",
+    subtitle: Subtitle,
+    documentationLinks,
+    tips,
+    description,
+    previewDescription,
+    githubUrl,
+    metaDescription:
+        "Demonstrates how to repurpose a Candlestick Series into dragabble, labled, event markers",
+    metaKeywords: "events, drag, edit, datalabels, , layout, demo, chart, javascript, webgl, canvas",
+    thumbnailImage: exampleImage
+};
