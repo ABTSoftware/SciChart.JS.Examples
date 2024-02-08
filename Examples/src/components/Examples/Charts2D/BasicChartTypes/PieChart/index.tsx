901d57ca3152611f7b4845ae2bde6478efdd700f
Jim Risen <jim.dev@scichart.com>
2024-02-08 21:19:21 +0200

    refactor(examples): splitting src of PieChart example; split

---
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/PieChart/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import classes from "../../../styles/Examples.module.scss";
+import { appTheme } from "scichart-example-dependencies";
+import { SciChartReact } from "scichart-react";
+
+export default function ChartComponent() {
+    return (
+        <div className={classes.ChartWrapper}>
+            <SciChartReact style={{ width: "100%", height: "100%", float: "left" }} initChart={drawExample} />
+            {/*Placeholder until we have a proper chart title (soon!)*/}
+            <span
+                style={{
+                    color: appTheme.ForegroundColor,
+                    fontSize: 20,
+                    position: "absolute",
+                    left: "50%",
+                    top: "20px",
+                    transform: "translate(-50%)",
+                }}
+            >
+                Market share of Mobile Phone Manufacturers (2022)
+            </span>
+        </div>
+    );
+}
