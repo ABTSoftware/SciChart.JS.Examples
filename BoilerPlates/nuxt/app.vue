437390c4ab3ac886afec70855b293518a2f77ad1
Jim Risen <jim.dev@scichart.com>
2024-04-09 17:31:44 +0300

    chore(boilerplates): SCJS-1714 add web-components, vue and nuxt boiler-plates

---
+++ BoilerPlates/nuxt/app.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <button @click="initSciChart()">Show SciChart</button>
+
+    <div id="scichart-root" style="width: 600px; height: 400px; margin: auto" />
+  </div>
+</template>
+
+<script>
+import * as SciChart from "scichart";
+
+export default {
+  name: "index",
+  mounted() {
+    console.log("onMounted");
+  },
+  methods: {
+    async initSciChart() {
+      // Create the SciChartSurface in the div 'scichart-root'
+      // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
+      // instance must be passed to other types that exist on the same surface.
+      //
+      // Make sure to call `sciChartSurface.delete()` to do a cleanup when necessary.
+      // Refer to the Vue boilerplate for an example.
+      const { sciChartSurface, wasmContext } =
+        await SciChart.SciChartSurface.create("scichart-root");
+
+      const {
+        NumericAxis,
+        XyDataSeries,
+        ZoomExtentsModifier,
+        ZoomPanModifier,
+        MouseWheelZoomModifier,
+        RolloverModifier,
+        FastLineRenderableSeries,
+      } = SciChart;
+
+      // Create an X,Y Axis and add to the chart
+      const xAxis = new NumericAxis(wasmContext);
+      const yAxis = new NumericAxis(wasmContext);
+      sciChartSurface.xAxes.add(xAxis);
+      sciChartSurface.yAxes.add(yAxis);
+
+      // Declare a DataSeries
+      const xyDataSeries = new XyDataSeries(wasmContext);
+
+      function getRandonInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      const xValues = [];
+      const yValues = [];
+
+      for (let index = 0; index < 100; index++) {
+        xValues.push(index);
+        yValues.push(getRandonInt(50));
+      }
+
+      xyDataSeries.appendRange(xValues, yValues);
+
+      // Add Chart Modifiers
+      sciChartSurface.chartModifiers.add(
+        new ZoomExtentsModifier(),
+        new ZoomPanModifier(),
+        new MouseWheelZoomModifier(),
+        new RolloverModifier({ rolloverLineStroke: "rgba(255,0,0,1)" })
+      );
+
+      // Add a line series to the SciChartSurface
+      const lineSeries = new FastLineRenderableSeries(wasmContext);
+      lineSeries.strokeThickness = 3;
+      lineSeries.stroke = "rgba(255,0,0,1)";
+      lineSeries.dataSeries = xyDataSeries;
+
+      sciChartSurface.renderableSeries.add(lineSeries);
+    },
+  },
+};
+</script>
