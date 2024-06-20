2b210d2da1da5d62305b5efbb6de2245f3ac5789
Dilip Tripathi <dilipt@capitalnumbers.com>
2024-06-20 10:28:26 +0530

    replace direct core lib usage with SciChartReact in RealtimeSurfaceMesh3DChart component

--- Examples/src/components/Examples/Charts3D/Basic3DChartTypes/RealtimeSurfaceMesh3DChart/index.tsx
+++ Examples/src/components/Examples/Charts3D/Basic3DChartTypes/RealtimeSurfaceMesh3DChart/index.tsx
@@ -1,43 +1,28 @@
 import * as React from "react";
 import classes from "../../../styles/Examples.module.scss";
-
+import { SciChartReact, TResolvedReturnType } from "scichart-react";
+// import { SciChart3DSurface } from "scichart";
+import { drawExample } from "./drawExample";
 import { SciChart3DSurface } from "scichart";
-import { drawExample, divElementId } from "./drawExample";
 
 // REACT COMPONENT
 export default function RealtimeSurfaceMesh3DChart() {
     const sciChartSurfaceRef = React.useRef<SciChart3DSurface>();
-    const controlsRef = React.useRef<{ startAnimation: () => void; stopAnimation: () => void }>();
-
-    React.useEffect(() => {
-        const chartPromise = drawExample().then((res) => {
-            sciChartSurfaceRef.current = res.sciChart3DSurface;
-            controlsRef.current = res.controls;
-            res.controls.startAnimation();
-        });
-
-        // Delete sciChartSurface on unmount component to prevent memory leak
-        return () => {
-            if (sciChartSurfaceRef.current) {
-                sciChartSurfaceRef.current.delete();
-                sciChartSurfaceRef.current = undefined;
-                controlsRef.current.stopAnimation();
-                controlsRef.current = undefined;
-            } else {
-                chartPromise.then((res) => {
-                    sciChartSurfaceRef.current.delete();
-                    sciChartSurfaceRef.current = undefined;
-                    controlsRef.current.stopAnimation();
-                    controlsRef.current = undefined;
-                });
-            }
-        };
-    }, []);
+    const animationRef = React.useRef<any>();
 
     return (
         <div className={classes.ChartWrapper}>
             <div style={{ position: "relative", height: "100%", width: "100%" }}>
-                <div id={divElementId} style={{ position: "absolute", height: "100%", width: "100%" }}></div>
+                <SciChartReact
+                    initChart={drawExample}
+                    onInit={(initResult: TResolvedReturnType<typeof drawExample>) => {
+                        const { sciChartSurface, controls } = initResult;
+                        sciChartSurfaceRef.current = sciChartSurface;
+                        animationRef.current = controls;
+                        controls.startAnimation();
+                    }}
+                    style={{ position: "absolute", height: "100%", width: "100%" }}
+                />
             </div>
         </div>
     );
