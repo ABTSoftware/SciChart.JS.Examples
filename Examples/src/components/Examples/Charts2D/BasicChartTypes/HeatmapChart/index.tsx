7ad591cf51901be7be6215340add310d6b017582
Dilip Tripathi <dilipt@capitalnumbers.com>
2024-06-21 14:11:03 +0530

    replace direct core lib usage with SciChartReact in HeatMapChart component

--- Examples/src/components/Examples/Charts2D/BasicChartTypes/HeatmapChart/index.tsx
+++ Examples/src/components/Examples/Charts2D/BasicChartTypes/HeatmapChart/index.tsx
@@ -4,7 +4,8 @@ import { Button } from "@material-ui/core";
 import { appTheme } from "../../../theme";
 import { makeStyles } from "@material-ui/core/styles";
 import { SciChartSurface, HeatmapLegend } from "scichart";
-import { drawExample, divElementId, drawHeatmapLegend, divHeatmapLegend } from "./drawExample";
+import { SciChartReact, TResolvedReturnType } from "scichart-react";
+import { drawExample, drawHeatmapLegend } from "./drawExample";
 
 // Styles for layout of the toolbar / chart area
 const useStyles = makeStyles((theme) => ({
@@ -17,7 +18,6 @@ const useStyles = makeStyles((theme) => ({
     },
     toolbarRow: {
         display: "flex",
-        // flex: "auto",
         flexBasis: "70px",
         padding: 10,
         width: "100%",
@@ -28,8 +28,6 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-// React component needed as our examples app is react.
-// SciChart can be used in Angular, Vue, Blazor and vanilla JS! See our Github repo for more info
 export default function HeatmapChart() {
     const controlsRef = React.useRef({
         startDemo: () => {},
@@ -38,47 +36,6 @@ export default function HeatmapChart() {
     const sciChartSurfaceRef = React.useRef<SciChartSurface>();
     const heatmapLegendRef = React.useRef<HeatmapLegend>();
     const [stats, setStats] = React.useState({ xSize: 0, ySize: 0, fps: 0 });
-
-    React.useEffect(() => {
-        const chartInitializationPromise = Promise.all([drawExample(), drawHeatmapLegend()]).then(([res, legend]) => {
-            sciChartSurfaceRef.current = res.sciChartSurface;
-            heatmapLegendRef.current = legend;
-            controlsRef.current = res.controls;
-
-            // Handle drawing/updating FPS
-            let lastRendered = Date.now();
-            res.sciChartSurface.rendered.subscribe(() => {
-                const currentTime = Date.now();
-                const timeDiffSeconds = new Date(currentTime - lastRendered).getTime() / 1000;
-                lastRendered = currentTime;
-                const fps = 1 / timeDiffSeconds;
-                setStats({
-                    xSize: res.heatmapDataSeries.arrayWidth,
-                    ySize: res.heatmapDataSeries.arrayHeight,
-                    fps,
-                });
-            });
-            res.controls.startDemo();
-        });
-
-        // Delete sciChartSurface on unmount component to prevent memory leak
-        return () => {
-            // check if chart is already initialized
-            if (sciChartSurfaceRef.current) {
-                controlsRef.current.stopDemo();
-                sciChartSurfaceRef.current.delete();
-                heatmapLegendRef.current.delete();
-                return;
-            }
-
-            // else postpone deletion
-            chartInitializationPromise.then(() => {
-                controlsRef.current.stopDemo();
-                sciChartSurfaceRef.current.delete();
-                heatmapLegendRef.current.delete();
-            });
-        };
-    }, []);
 
     const localClasses = useStyles();
 
@@ -105,18 +62,57 @@ export default function HeatmapChart() {
                         <span style={{ margin: 12 }}>FPS: {stats.fps.toFixed(0)}</span>
                     </div>
                     <div className={localClasses.chartArea} style={{ position: "relative" }}>
-                        <div id={divElementId} style={{ width: "100%", height: "100%" }}></div>
-                        <div
-                            id={divHeatmapLegend}
+                        <SciChartReact
+                            initChart={drawExample}
+                            style={{ width: "100%", height: "100%" }}
+                            onInit={(initResult: TResolvedReturnType<typeof drawExample>) => {
+                                const { sciChartSurface, heatmapDataSeries, controls } = initResult;
+                                sciChartSurfaceRef.current = sciChartSurface;
+                                controlsRef.current = controls;
+
+                                // Initialize heatmap legend not required for now
+                                // drawHeatmapLegend().then((legend) => {
+                                //     heatmapLegendRef.current = legend;
+                                // });
+
+                                // Handle drawing/updating FPS
+                                let lastRendered = Date.now();
+                                sciChartSurface.rendered.subscribe(() => {
+                                    const currentTime = Date.now();
+                                    const timeDiffSeconds = (currentTime - lastRendered) / 1000;
+                                    lastRendered = currentTime;
+                                    const fps = 1 / timeDiffSeconds;
+                                    setStats({
+                                        xSize: heatmapDataSeries.arrayWidth,
+                                        ySize: heatmapDataSeries.arrayHeight,
+                                        fps,
+                                    });
+                                });
+
+                                // Start the demo
+                                controls.startDemo();
+
+                                // Cleanup function
+                                return () => {
+                                    controls.stopDemo();
+                                    sciChartSurface.delete();
+                                    if (heatmapLegendRef.current) {
+                                        heatmapLegendRef.current.delete();
+                                    }
+                                };
+                            }}
+                        />
+                        <SciChartReact
+                            initChart={drawHeatmapLegend}
                             style={{
                                 position: "absolute",
                                 height: "90%",
                                 width: "100px",
                                 top: 0,
                                 right: "75px",
-                                margin: "20",
+                                margin: "20px",
                             }}
-                        ></div>
+                        />
                     </div>
                 </div>
             </div>
