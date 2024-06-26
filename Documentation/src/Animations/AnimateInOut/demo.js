43b7c2971c30001c9c47ccd7856b8e5b7a984944
antichaosdb <dave@antichaos.co.uk>
2024-06-26 14:46:50 +0100

    feat(docs): Show how to use revertAnimationVectors to allow reuse of a startupAnimation

--- Documentation/src/Animations/AnimateInOut/demo.js
+++ Documentation/src/Animations/AnimateInOut/demo.js
@@ -1,6 +1,4 @@
 const animateInOut = async (divElementId) => {
-  // #region ExampleA
-  // Demonstrates how to create a text chart with SciChart.js
   const {
     SciChartSurface,
     NumericAxis,
@@ -11,104 +9,92 @@ const animateInOut = async (divElementId) => {
     ZoomExtentsModifier,
     ZoomPanModifier,
     MouseWheelZoomModifier,
-    SplineLineRenderableSeries
+    SplineLineRenderableSeries,
   } = SciChart;
 
   // Create a SciChartSurface
-  const { wasmContext, sciChartSurface } = await SciChartSurface.create(divElementId);
+  const { wasmContext, sciChartSurface } = await SciChartSurface.create(
+    divElementId
+  );
 
   // Create an xAxis, yAxis
   sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
   sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
 
+  //#region LoopExample
+  const DURATION = 700;
+  const inOutAnimation = (series, delay) =>
+    new ScaleAnimation({
+      duration: DURATION,
+      delay,
+      onCompleted: () => {
+        series.runAnimation(
+          new ScaleAnimation({
+            duration: DURATION,
+            reverse: true,
+            onCompleted: () => {
+              // Hide the series
+              series.isVisible = false;
+              // Restore the data to the original values (before the animate out) so it can be animated in again.
+              // This only works in an onComplete
+              series.dataSeries.revertAnimationVectors();
+            },
+          })
+        );
+        // Restart this animation after the others have run
+        setTimeout(() => {
+          series.isVisible = true;
+          series.runAnimation(inOutAnimation(series, 0));
+        }, 4 * DURATION);
+      },
+    });
+  // #endregion
+
   // Create a Line Series
   const xValues = makeIncArray(15);
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 5; i++) {
     const yValues = xValues.map(() => Math.random() * 10);
     const lineSeries = new SplineLineRenderableSeries(wasmContext, {
       dataSeries: new XyDataSeries(wasmContext, { xValues, yValues }),
       stroke: AUTO_COLOR,
-      strokeThickness: 4
+      strokeThickness: 4,
     });
 
     sciChartSurface.renderableSeries.add(lineSeries);
-
-    lineSeries.animation = new ScaleAnimation({ 
-      duration: 700,
-      delay: i * 800,
-      
-      onCompleted: () => {
-        lineSeries.runAnimation(
-          new ScaleAnimation({
-            duration: 700,
-            reverse: true,
-          })
-        )
-      }
-    });
+    if (i === 0) {
+      //#region InOutExample
+      lineSeries.animation = new ScaleAnimation({
+        duration: 700,
+        delay: i * 700,
+        fadeEffect: true,
+        onCompleted: () => {
+          lineSeries.runAnimation(
+            new ScaleAnimation({
+              duration: 700,
+              reverse: true,
+              fadeEffect: true,
+            })
+          );
+        },
+      });
+      // #endregion
+    } else {
+      lineSeries.animation = inOutAnimation(lineSeries, i * DURATION);
+    }
   }
 
   // Add some interactivity modifiers
   sciChartSurface.chartModifiers.add(
-    new ZoomExtentsModifier(), 
-    new ZoomPanModifier(), 
+    new ZoomExtentsModifier(),
+    new ZoomPanModifier(),
     new MouseWheelZoomModifier()
   );
 
   sciChartSurface.zoomExtents();
-  // #endregion
 };
 
 animateInOut("scichart-root");
 
+async function builderExample(divElementId) {}
 
-
-
-async function builderExample(divElementId) {
-  // #region ExampleB
-  // Demonstrates how to create a band chart with SciChart.js using the Builder API
-  const {
-    chartBuilder,
-    ESeriesType,
-    EThemeProviderType
-  } = SciChart;
-
-  // or, for npm, import { chartBuilder, ... } from "scichart"
-
-  const xValues = [];
-  const yValues = [];
-  const y1Values = [];
-  const POINTS = 1000;
-  const STEP = (3 * Math.PI) / POINTS;
-  for (let i = 0; i <= 1000; i++) {
-    const k = 1 - i / 2000;
-    xValues.push(i);
-    yValues.push(Math.sin(i * STEP) * k * 0.7);
-    y1Values.push(Math.cos(i * STEP) * k);
-  }
-
-  const { wasmContext, sciChartSurface } = await chartBuilder.build2DChart(divElementId, {
-    surface: { theme: { type: EThemeProviderType.Dark } },
-    series: [
-      {
-        type: ESeriesType.BandSeries,
-        xyyData: {
-          xValues,
-          yValues,
-          y1Values
-        },
-        options: {
-          stroke: "#FF1919FF",
-          strokeY1: "#279B27FF",
-          fill: "#279B2733",
-          fillY1: "#FF191933",
-          strokeThickness: 2,
-        }
-      }
-    ]
-  });
-  // #endregion
-};
-
-if (location.search.includes("builder=1"))
-builderExample("scichart-root");
+if (location.search.includes("builder=1")) builderExample("scichart-root");
