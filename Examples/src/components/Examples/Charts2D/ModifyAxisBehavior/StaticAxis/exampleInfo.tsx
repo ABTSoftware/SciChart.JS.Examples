73b71cc382e4ebca0e9257db28c7561ed1d044ff
antichaosdb <dave@antichaos.co.uk>
2024-07-03 13:03:45 +0100

    chore(Examples): update subtitle

--- Examples/src/components/Examples/Charts2D/ModifyAxisBehavior/StaticAxis/exampleInfo.tsx
+++ Examples/src/components/Examples/Charts2D/ModifyAxisBehavior/StaticAxis/exampleInfo.tsx
@@ -4,11 +4,11 @@ import { ExampleStrings } from "../../../ExampleStrings";
 import { TDocumentationLink } from "../../../../../helpers/types/ExampleDescriptionTypes";
 import exampleImage from "./javascript-central-axes.jpg";
 
-const description = `Demonstrates a chart with a static X Axis
-SciChart supports customization of Axis static behavior by setting the isStaticAxis property to true.`
+const description = `Demonstrates a realtime chart where the ticks and gridlines are fixed, but the labels change.  
+This is achieved by setting the isStaticAxis property to true on the X axis.`;
 
 const documentationLinks: TDocumentationLink[] = [
-    {   
+    {
         href: ExampleStrings.urlStaticAxisDocumentation,
         title: ExampleStrings.urlTitleStaticAxisDocumentation,
         linkTitle: "Static Axis",
@@ -17,10 +17,14 @@ const documentationLinks: TDocumentationLink[] = [
 
 const Subtitle = (frameworkName: string) => (
     <p>
-        Demonstrates how to create a <strong>{frameworkName} Chart with static X Axis</strong>.
+        Demonstrates a realtime <strong>{frameworkName} static axis chart</strong> - where the ticks and gridlines are
+        fixed, but the labels change.
+        <br />
+        With SciChart.js High Performance{" "}
         <a href={ExampleStrings.urlJavascriptChartFeatures} target="_blank">
             JavaScript Charts
-        </a>
+        </a>{" "}
+        you can achieve this simply by setting isStaticAxis property to true on the X axis.
     </p>
 );
 
