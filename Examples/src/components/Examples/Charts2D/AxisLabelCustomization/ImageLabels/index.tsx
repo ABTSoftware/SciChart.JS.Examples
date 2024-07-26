143811f884c101e072d30c4f237387cad947d1d2
Jim Risen <jim.dev@scichart.com>
2024-07-26 11:02:01 +0300

    refactor(examples): extract image references from chart config logic in ImageLabels Example

--- Examples/src/components/Examples/Charts2D/AxisLabelCustomization/ImageLabels/index.tsx
+++ Examples/src/components/Examples/Charts2D/AxisLabelCustomization/ImageLabels/index.tsx
@@ -2,8 +2,42 @@ import { SciChartReact } from "scichart-react";
 import classes from "../../../styles/Examples.module.scss";
 import { drawExample } from "./drawExample";
 
+import appleLogo from "./images/apple.png";
+import samsungLogo from "./images/samsung.png";
+import xiaomiLogo from "./images/xiaomi.png";
+import huaweiLogo from "./images/Huawei.png";
+import oppoLogo from "./images/oppo.png";
+import vivoLogo from "./images/vivo.png";
+import realmeLogo from "./images/realme.png";
+import motorolaLogo from "./images/motorola.png";
+import unknownLogo from "./images/question.png";
+import lgLogo from "./images/Lg.png";
+import oneplusLogo from "./images/oneplus.png";
+import tecnoLogo from "./images/tecno.png";
+import infinixLogo from "./images/infinix.png";
+import googleLogo from "./images/google.png";
+import nokiaLogo from "./images/nokia.png";
+
+const emojiUrls = [
+    appleLogo,
+    samsungLogo,
+    xiaomiLogo,
+    huaweiLogo,
+    oppoLogo,
+    vivoLogo,
+    realmeLogo,
+    motorolaLogo,
+    unknownLogo,
+    lgLogo,
+    oneplusLogo,
+    tecnoLogo,
+    infinixLogo,
+    googleLogo,
+    nokiaLogo,
+];
+
 // React component needed as our examples app is react.
 // SciChart can be used in Angular, Vue, Blazor and vanilla JS! See our Github repo for more info
 export default function ChartComponent() {
-    return <SciChartReact initChart={drawExample} className={classes.ChartWrapper} />;
+    return <SciChartReact initChart={drawExample(emojiUrls)} className={classes.ChartWrapper} />;
 }
