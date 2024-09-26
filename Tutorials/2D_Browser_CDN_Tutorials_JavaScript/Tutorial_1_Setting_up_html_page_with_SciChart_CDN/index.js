2cbb8f4e13d77a571672d7f6b577a30db792c382
andyb1979 <aburnettthompson@gmail.com>
2024-09-26 15:55:05 +0200

    chore(docs): Tutorial 2 to load wasm locally now in docs

--- Tutorials/2D_Browser_CDN_Tutorials_JavaScript/Tutorial_1_Setting_up_html_page_with_SciChart_CDN/index.js
+++ Tutorials/2D_Browser_CDN_Tutorials_JavaScript/Tutorial_1_Setting_up_html_page_with_SciChart_CDN/index.js
@@ -1,25 +1,57 @@
+// #region ExampleA
+// Equivalent of imports when using index.min.js is to declare global variables like this
+const { SciChartSurface, NumericAxis, FastLineRenderableSeries, XyDataSeries } =
+  SciChart;
+
+// This code is syntactically similar to using imports which would look like this:
+// import { SciChartSurface, NumericAxis, FastLineRenderableSeries, XyDataSeries } from "scichart";
+
+// Once you have done that, the API is **EXACTLY** the same as when using npm/webpack/react/vue/angular etc.
 
 async function initSciChart() {
-    // LICENSING //
-    // Set your license code here. See https://www.scichart.com/licensing-scichart-js/
-	// for instructions  
-    
-	// Load SciChart.js from CDN. Ensure version numbers match the script in index.html
-	SciChart.SciChartSurface.useWasmFromCDN();
+  // Create the SciChartSurface in the div 'scichart-root'
+  // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
+  // instance must be passed to other types that exist on the same surface.
+  const { sciChartSurface, wasmContext } = await SciChartSurface.create(
+    "scichart-root"
+  );
 
-    // Create the SciChartSurface in the div 'scichart-root'
-    // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
-    // instance must be passed to other types that exist on the same surface.
-    const {sciChartSurface, wasmContext} = await SciChart.SciChartSurface.create("scichart-root");
+  // Create an X,Y Axis and add to the chart
+  const xAxis = new NumericAxis(wasmContext);
+  const yAxis = new NumericAxis(wasmContext);
 
-    // Create an X,Y Axis and add to the chart
-    const xAxis = new SciChart.NumericAxis(wasmContext);
-    const yAxis = new SciChart.NumericAxis(wasmContext);
-    
-    sciChartSurface.xAxes.add(xAxis);
-    sciChartSurface.yAxes.add(yAxis);
+  sciChartSurface.xAxes.add(xAxis);
+  sciChartSurface.yAxes.add(yAxis);
 
-    // That's it! You just created your first SciChartSurface!
+  // Add a series
+  sciChartSurface.renderableSeries.add(
+    new FastLineRenderableSeries(wasmContext, {
+      dataSeries: new XyDataSeries(wasmContext, {
+        xValues: [0, 1, 2, 3, 4],
+        yValues: [2, 1, 4, 3, 2],
+      }),
+    })
+  );
+
+  // That's it! You just created your first SciChartSurface!
 }
 
 initSciChart();
+
+// #endregion
+
+// #region ExampleB
+// Implicitly the following line of code is called when loading SciChart.js via index.min.js
+SciChartSurface.useWasmFromCDN();
+
+// This is equivalent to calling SciChartSurface.configure() with the URLs from the CDN for wasm/data files
+const libraryVersion = "3.4.662";
+SciChartSurface.configure({
+  dataUrl: `https://cdn.jsdelivr.net/npm/scichart@${libraryVersion}/_wasm/scichart2d.data`,
+  wasmUrl: `https://cdn.jsdelivr.net/npm/scichart@${libraryVersion}/_wasm/scichart2d.wasm`,
+});
+
+// This code doesn't actually need to be called when using index.min.js, however
+// its good to be aware that it is happening in the background.
+
+// #endregion
---
+++ Tutorials/2D_Browser_CDN_Tutorials_JavaScript/Tutorial_2_Adding_Series_and_data/Readme.md
@@ -0,0 +1,3 @@
+# Readme
+
+This tutorial has been deleted as it's a duplicate of ../2D_Chart_Tutorials_JavaScript/Tutorial_2_Adding_Series_and_Data
