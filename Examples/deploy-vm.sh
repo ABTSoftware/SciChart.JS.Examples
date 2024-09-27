a60d5305c8bca3d61a7bd6a855980b4da97400b6
klishevich <busation@gmail.com>
2024-09-27 10:59:58 +0900

    chore(examples): add parameters output to deploy-vm script

--- Examples/deploy-vm.sh
+++ Examples/deploy-vm.sh
@@ -4,6 +4,10 @@ server=$1
 appRoot=$2
 appPort=$3
 appName=$4
+echo "server ${server}"
+echo "appRoot ${appRoot}"
+echo "appPort ${appPort}"
+echo "appName ${appName}"
 ssh $server ". ~/.nvm/nvm.sh; pm2 stop ${appName}; pm2 delete ${appName}"
 ssh $server "cd ${appRoot} && git checkout . && git pull"
 ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; rm package-lock.json; node -v; npm i --loglevel verbose --no-audit; NOINDEX=1 npm run build"
