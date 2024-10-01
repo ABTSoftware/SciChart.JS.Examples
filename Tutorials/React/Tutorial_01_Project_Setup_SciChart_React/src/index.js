237550df990e4555303bea59b9fc6a049b644441
andyb1979 <aburnettthompson@gmail.com>
2024-10-01 18:20:35 +0200

    chore(docs): Thought this was committed - tutorial for CDN and wasm setup

---
+++ Tutorials/React/Tutorial_01_Project_Setup_SciChart_React/src/index.js
@@ -0,0 +1,34 @@
+import { SciChartSurface, NumericAxis } from "scichart";
+
+async function initSciChart() {
+  // LICENSING //
+  // For community or trial usage, SciChart.js works out of the box
+
+  // For commercial use of SciChart, you need a license.
+  // Purchased license keys can be viewed at https://www.scichart.com/profile
+  //
+  // e.g.
+  // Set your license code here
+  // SciChartSurface.setRuntimeLicenseKey("YOUR_RUNTIME_KEY");
+  //
+  // Also, once activated (trial or paid license) having the licensing wizard open on your machine
+  // will mean any or all applications you run locally will be fully licensed.
+
+  // Create the SciChartSurface in the div 'scichart-root'
+  // The SciChartSurface, and webassembly context 'wasmContext' are paired. This wasmContext
+  // instance must be passed to other types that exist on the same surface.
+  const { sciChartSurface, wasmContext } = await SciChartSurface.create(
+    "scichart-root"
+  );
+
+  // Create an X,Y Axis and add to the chart
+  const xAxis = new NumericAxis(wasmContext);
+  const yAxis = new NumericAxis(wasmContext);
+
+  sciChartSurface.xAxes.add(xAxis);
+  sciChartSurface.yAxes.add(yAxis);
+
+  // That's it! You just created your first SciChartSurface!
+}
+
+initSciChart();
