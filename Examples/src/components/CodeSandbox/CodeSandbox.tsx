1d8bf7d6d6919212259c5f5a97073ae96250e269
William Pearson <williammpearson@gmail.com>
2024-11-26 19:37:57 +0000

    extract loader

--- Examples/src/components/CodeSandbox/CodeSandbox.tsx
+++ Examples/src/components/CodeSandbox/CodeSandbox.tsx
@@ -1,44 +1,5 @@
 import { FC, useState } from "react";
-
-export const Loader: FC = () => (
-    <div
-        style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.8)",
-            zIndex: 1,
-        }}
-    >
-        <div
-            style={{
-                width: "50px",
-                height: "50px",
-                border: "6px solid #ccc",
-                borderTop: "6px solid #007BFF",
-                borderRadius: "50%",
-                animation: "spin 1s linear infinite",
-            }}
-        ></div>
-        <style>
-            {`
-          @keyframes spin {
-            0% {
-              transform: rotate(0deg);
-            }
-            100% {
-              transform: rotate(360deg);
-            }
-          }
-        `}
-        </style>
-    </div>
-);
+import { Loader } from "./Loader";
 
 type TCodeSandbox = {
     id: string;
--- Examples/src/components/CodeSandbox/index.ts
+++ Examples/src/components/CodeSandbox/index.ts
@@ -1,1 +1,2 @@
 export * from "./CodeSandbox";
+export * from "./Loader";
