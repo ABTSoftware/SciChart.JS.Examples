ff6096c329036c2915a9d1f37116760ba2e99f84
William Pearson <williammpearson@gmail.com>
2024-11-28 18:59:43 +0000

    inline svg icons instead of using files

---
+++ Examples/src/components/buttons/svgIcons.ts
@@ -0,0 +1,26 @@
+// FIX cannot get webpack to serve these!!!
+
+export const back = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+</svg>`;
+
+export const close = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+</svg>`;
+
+export const browserFill = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M21 3H3C1.9 3 1 3.9 1 5V19C1 20.1 1.9 21 3 21H21C22.1 21 23 20.1 23 19V5C23 3.9 22.1 3 21 3ZM21 19H3V8H21V19ZM21 6H3V5H21V6ZM4 10H20V17H4V10ZM6.5 5.5C6.5 5.78 6.28 6 6 6C5.72 6 5.5 5.78 5.5 5.5C5.5 5.22 5.72 5 6 5C6.28 5 6.5 5.22 6.5 5.5ZM8.5 5.5C8.5 5.78 8.28 6 8 6C7.72 6 7.5 5.78 7.5 5.5C7.5 5.22 7.72 5 8 5C8.28 5 8.5 5.22 8.5 5.5ZM10.5 5.5C10.5 5.78 10.28 6 10 6C9.72 6 9.5 5.78 9.5 5.5C9.5 5.22 9.72 5 10 5C10.28 5 10.5 5.22 10.5 5.5Z"/>
+</svg>`;
+
+export const embedded = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M19 3H5C3.89 3 3 3.89 3 5V19C3 20.11 3.89 21 5 21H19C20.11 21 21 20.11 21 19V5C21 3.89 20.11 3 19 3ZM19 19H5V5H19V19ZM12 17L8 13H16L12 17ZM12 7L16 11H8L12 7Z"/>
+</svg>`;
+
+export const fullScreen = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+</svg>`;
+
+export const DEFAULT_ICON = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="black" style={{ verticalAlign: "middle" }}>
+    <rect width="16" height="16" />
+</svg>`;
