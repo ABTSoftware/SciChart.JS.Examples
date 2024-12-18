09ffe126dd4ffcec1b8e3967daaad30cc29ad702
antichaosdb <dave@antichaos.co.uk>
2024-12-18 11:33:10 +0000

    fix(Examples): add branch to deploy script

--- Examples/deploy-vm.sh
+++ Examples/deploy-vm.sh
@@ -4,11 +4,13 @@ server=$1
 appRoot=$2
 appPort=$3
 appName=$4
+branch=$5
 echo "server ${server}"
 echo "appRoot ${appRoot}"
 echo "appPort ${appPort}"
 echo "appName ${appName}"
+echo "branch ${branch}"
 ssh $server ". ~/.nvm/nvm.sh; pm2 stop ${appName}; pm2 delete ${appName}"
-ssh $server "cd ${appRoot} && git checkout . && git pull"
+ssh $server "cd ${appRoot} && git checkout ${branch} && git pull"
 ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; rm package-lock.json; node -v; npm i --loglevel verbose --no-audit; NOINDEX=1 npm run build"
 ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; PORT=${appPort} pm2 start build/server.js --name ${appName}"
