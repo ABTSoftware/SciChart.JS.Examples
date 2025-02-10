03017c8c5d626a7efc15a4c3c35c6b9a19f69247
andyb1979 <aburnettthompson@gmail.com>
2025-02-10 19:16:59 +0100

    chore(docs): Integrated datamanager, added axis and chart synchronization

--- Tutorials/React/Tutorial_05_Synchronizing_React_Charts/src/App.jsx
+++ Tutorials/React/Tutorial_05_Synchronizing_React_Charts/src/App.jsx
@@ -3,9 +3,15 @@ import "./styles.css";
 import { SciChartGroup, SciChartReact } from "scichart-react";
 import { initChart } from "./initChart";
 import { DataManager } from "./DataManager";
+import { AxisSynchroniser } from "./AxisSynchronizer";
 
 function App() {
   const [charts, setCharts] = useState([0, 1]); // Initialize with 2 charts
+  const [axisSynchronizer, setAxisSynchronizer] = useState(
+    new AxisSynchroniser()
+  );
+
+  // Create a DataManager class to proxy fetching data
   const [dataManager] = useState(() => new DataManager());
 
   const addChart = () => {
@@ -17,6 +23,7 @@ function App() {
       setCharts(charts.slice(0, -1));
     }
   };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -40,15 +47,31 @@ function App() {
       <div style={{ height: "600px" }}>
         <SciChartGroup>
           {charts.map((chartId) => (
+            // #region SciChartReact Component
             <SciChartReact
               key={chartId}
-              initChart={(div) => initChart(div, chartId)}
-              onInit={async (initResult) => {
-                const data = await dataManager.fetchData(chartId);
-                initResult.setData(data.xValues, data.yValues);
+              initChart={(div) => initChart(div, chartId, "chartGroupId")}
+              // After initialization, fetch data and call setData on the chart (see initChart.js)
+              // onInit cannot be an async function, so use dataManager.fetchData().then() to update the chart
+              onInit={(initResult) => {
+                dataManager.fetchData(chartId).then((data) => {
+                  initResult.setData(data.xValues, data.yValues);
+                });
+
+                // After init, add the chart to axis synchronizer
+                axisSynchronizer.addAxis(
+                  initResult.sciChartSurface.xAxes.get(0)
+                );
+              }}
+              onDelete={(initResult) => {
+                // After delete, remove the chart from axis synchronizer
+                axisSynchronizer.removeAxis(
+                  initResult.sciChartSurface.xAxes.get(0)
+                );
               }}
               style={{ height: `${100 / charts.length}%` }}
             />
+            // #endregion
           ))}
         </SciChartGroup>
       </div>
--- Tutorials/React/Tutorial_05_Synchronizing_React_Charts/src/initChart.js
+++ Tutorials/React/Tutorial_05_Synchronizing_React_Charts/src/initChart.js
@@ -9,9 +9,12 @@ import {
   XyDataSeries,
   EllipsePointMarker,
   SciChartJsNavyTheme,
+  EAutoRange,
+  NumberRange,
+  WaveAnimation,
 } from "scichart";
 
-export const initChart = async (divElement, chartId) => {
+export const initChart = async (divElement, chartId, chartGroupId) => {
   const { sciChartSurface, wasmContext } = await SciChartSurface.create(
     divElement,
     {
@@ -31,20 +34,30 @@ export const initChart = async (divElement, chartId) => {
     new NumericAxis(wasmContext, {
       axisTitle: "Y Axis",
       axisTitleStyle: { fontSize: 12 },
+      autoRange: EAutoRange.Always,
+      growBy: new NumberRange(0, 0.1),
     })
   );
 
-  // Add modifiers
+  // #region AddModifiers
+  // Add modifiers for zoom, pan and tooltip behaviour
   sciChartSurface.chartModifiers.add(
     new ZoomPanModifier(),
     new MouseWheelZoomModifier(),
     new ZoomExtentsModifier(),
-    new RolloverModifier()
+    new RolloverModifier({
+      modifierGroup: chartGroupId,
+      rolloverLineStroke: "LightSteelBlue",
+      snapToDataPoint: true,
+    })
   );
+  // #endregion
 
+  // Add a setData function. This will accept xValues and yValues and update the chart
+  // creating a new series each time.
   const setData = (xValues, yValues) => {
-    // Clear existing series
-    sciChartSurface.renderableSeries.clear();
+    // Clear existing series, deleting memory by passing callDeleteOnChildren: true
+    sciChartSurface.renderableSeries.clear(true);
 
     // Create new series
     const mountainSeries = new SplineMountainRenderableSeries(wasmContext, {
@@ -56,10 +69,19 @@ export const initChart = async (divElement, chartId) => {
         fill: "SteelBlue",
         stroke: "White",
       }),
+      animation: new WaveAnimation({ duration: 1000, fadeEffect: true }),
     });
 
+    // Setup series rollovermodifier properties
+    mountainSeries.rolloverModifierProps.tooltipTextColor = "#fff";
+    mountainSeries.rolloverModifierProps.tooltipColor = "SteelBlue";
+    mountainSeries.tooltipLabelX = "X";
+    mountainSeries.tooltipLabelY = "Y";
+
     sciChartSurface.renderableSeries.add(mountainSeries);
+    sciChartSurface.zoomExtents();
   };
 
+  // return values from initChart() go into initResult in <SciChartReact onInit={initResult => {}} />
   return { sciChartSurface, setData };
 };
