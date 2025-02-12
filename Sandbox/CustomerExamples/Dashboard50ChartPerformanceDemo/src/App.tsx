fb9bb463784d39dddd98ffefebfdd2448d3804f0
andyb1979 <aburnettthompson@gmail.com>
2025-02-12 09:26:40 +0100

    chore(docs): Refactor LabelControl. Updated Chart Count to 100

--- Sandbox/CustomerExamples/Dashboard50ChartPerformanceDemo/src/App.tsx
+++ Sandbox/CustomerExamples/Dashboard50ChartPerformanceDemo/src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { LabelControl } from "./Controls/LabelControl";
 import ChartPanel from "./ChartPanel/ChartPanel";
 import { ChartSpec, ChartType } from "./ChartPanel/ChartSpec";
 import { SciChartGroup } from "scichart-react";
@@ -7,14 +8,29 @@ import { DraggableProvider } from "./DraggablePanel/DraggableContext";
 import { FpsControl } from "./FpsControl";
 
 function App() {
-  const [reduceAxisElements, setReduceAxisElements] = useState(true);
-  const [drawLabels, setDrawLabels] = useState(false);
-  const [useNativeText, setUseNativeText] = useState(true);
-  const [cacheLabels, setCacheLabels] = useState(true);
+  const [chartState, setChartState] = useState({
+    reduceAxisElements: true,
+    drawLabels: false,
+    useNativeText: true,
+    cacheLabels: true,
+    hideOutOfView: true,
+  });
   const [pointCount, setPointCount] = useState(200);
-  const [chartCount, setChartCount] = useState(50);
+  const [chartCount, setChartCount] = useState(100);
   const [dataUpdateRate, setDataUpdateRate] = useState(1);
-  const [hideOutOfView, setHideOutOfView] = useState(true);
+
+  const handlePropertyChange = useCallback(
+    (propertyName: string, value: boolean) => {
+      setChartState((prev) => ({ ...prev, [propertyName]: value }));
+      setCharts((charts) =>
+        charts.map((chart) => ({
+          ...chart,
+          [propertyName]: value,
+        }))
+      );
+    },
+    []
+  );
 
   // Initialize chart specs. 50 charts of varying types
   const [charts, setCharts] = useState<ChartSpec[]>(() => {
@@ -29,11 +45,11 @@ function App() {
         left: `${(index % cols) * 25}%`,
         top: Math.floor(index / cols) * 200,
       },
-      drawLabels: drawLabels,
-      useNativeText: useNativeText,
-      reduceAxisElements: reduceAxisElements,
-      cacheLabels: cacheLabels,
-      hideOutOfView: hideOutOfView,
+      drawLabels: chartState.drawLabels,
+      useNativeText: chartState.useNativeText,
+      reduceAxisElements: chartState.reduceAxisElements,
+      cacheLabels: chartState.cacheLabels,
+      hideOutOfView: chartState.hideOutOfView,
     }));
   });
 
@@ -55,86 +71,36 @@ function App() {
           zIndex: 1000,
         }}
       >
-        <label style={{ color: "#333" }}>
-          <input
-            type="checkbox"
-            defaultChecked={true}
-            onChange={(e) => {
-              setReduceAxisElements(e.target.checked);
-              setCharts((charts) =>
-                charts.map((chart) => ({
-                  ...chart,
-                  reduceAxisElements: e.target.checked,
-                }))
-              );
-            }}
-          />{" "}
-          Reduce Axis Elements
-        </label>
-        <label style={{ color: "#333" }}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={(e) => {
-              setDrawLabels(e.target.checked);
-              setCharts((charts) =>
-                charts.map((chart) => ({
-                  ...chart,
-                  drawLabels: e.target.checked,
-                }))
-              );
-            }}
-          />{" "}
-          Draw Labels
-        </label>
-        <label style={{ color: "#333" }}>
-          <input
-            type="checkbox"
-            defaultChecked={true}
-            onChange={(e) => {
-              setUseNativeText(e.target.checked);
-              setCharts((charts) =>
-                charts.map((chart) => ({
-                  ...chart,
-                  useNativeText: e.target.checked,
-                }))
-              );
-            }}
-          />{" "}
-          Native Text
-        </label>
-        <label style={{ color: "#333" }}>
-          <input
-            type="checkbox"
-            defaultChecked={true}
-            onChange={(e) => {
-              setCacheLabels(e.target.checked);
-              setCharts((charts) =>
-                charts.map((chart) => ({
-                  ...chart,
-                  cacheLabels: e.target.checked,
-                }))
-              );
-            }}
-          />{" "}
-          Cache Labels
-        </label>
-        <label style={{ color: "#333" }}>
-          <input
-            type="checkbox"
-            defaultChecked={true}
-            onChange={(e) => {
-              setHideOutOfView(e.target.checked);
-              setCharts((charts) =>
-                charts.map((chart) => ({
-                  ...chart,
-                  hideOutOfView: e.target.checked,
-                }))
-              );
-            }}
-          />{" "}
-          Freeze Charts out of view
-        </label>
+        <LabelControl
+          label="Reduce Axis Elements"
+          checked={chartState.reduceAxisElements}
+          propertyName="reduceAxisElements"
+          onChange={handlePropertyChange}
+        />
+        <LabelControl
+          label="Draw Labels"
+          checked={chartState.drawLabels}
+          propertyName="drawLabels"
+          onChange={handlePropertyChange}
+        />
+        <LabelControl
+          label="Native Text"
+          checked={chartState.useNativeText}
+          propertyName="useNativeText"
+          onChange={handlePropertyChange}
+        />
+        <LabelControl
+          label="Cache Labels"
+          checked={chartState.cacheLabels}
+          propertyName="cacheLabels"
+          onChange={handlePropertyChange}
+        />
+        <LabelControl
+          label="Freeze Charts out of view"
+          checked={chartState.hideOutOfView}
+          propertyName="hideOutOfView"
+          onChange={handlePropertyChange}
+        />
         <div
           style={{
             marginLeft: "auto",
---
+++ Sandbox/CustomerExamples/Dashboard50ChartPerformanceDemo/src/Controls/LabelControl.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface LabelControlProps {
+  label: string;
+  checked: boolean;
+  propertyName: keyof ChartSpec;
+  onChange: (propertyName: string, value: boolean) => void;
+}
+
+export const LabelControl: React.FC<LabelControlProps> = ({
+  label,
+  checked,
+  propertyName,
+  onChange,
+}) => {
+  return (
+    <label style={{ color: "#333" }}>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onChange(propertyName, e.target.checked)}
+      />{" "}
+      {label}
+    </label>
+  );
+};
