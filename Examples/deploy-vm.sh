c8e15f0248e26d1d6a7296ebaeac24e97d638f9d
klishevich <busation@gmail.com>
2025-02-19 19:13:29 +0900

    chore(examples): update deploy script to build bundle locally and then copy

--- Examples/deploy-vm.sh
+++ Examples/deploy-vm.sh
@@ -12,5 +12,15 @@ echo "appName ${appName}"
 echo "branch ${branch}"
 ssh $server ". ~/.nvm/nvm.sh; pm2 stop ${appName}; pm2 delete ${appName}"
 ssh $server "cd ${appRoot} && git checkout ${branch} && git reset --hard && git pull"
-ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; node -v; npm i --loglevel verbose --no-audit; NOINDEX=1 npm run build"
+ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; node -v; npm i --loglevel verbose --no-audit"
+ssh $server "cd ${appRoot}; rm -rf build"
+echo "Creating archive..."
+tar -czvf build.tar.gz ./build
+echo "Archive has been created."
+scp -r build.tar.gz $server:$appRoot
+ssh $server "tar -xzvf ${appRoot}/build.tar.gz -C ${appRoot}"
+echo "Build files have been copied to the server."
+ssh $server "rm ${appRoot}/build.tar.gz"
+rm build.tar.gz
+
 ssh $server ". ~/.nvm/nvm.sh; cd ${appRoot}; PORT=${appPort} pm2 start build/server.js --name ${appName}"
