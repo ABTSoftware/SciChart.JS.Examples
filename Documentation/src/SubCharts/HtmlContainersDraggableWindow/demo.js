731a9d5eb288f42388ef9961860c333050d2cc48
andyb1979 <aburnettthompson@gmail.com>
2025-02-19 20:03:00 +0100

    chore(docs): Making progress with subcharts HTML containers

--- Documentation/src/SubCharts/HtmlContainersDraggableWindow/demo.js
+++ Documentation/src/SubCharts/HtmlContainersDraggableWindow/demo.js
@@ -5,25 +5,55 @@ for (let i = 0; i < 100; i++) {
   yValues.push(0.2 * Math.sin(i * 0.1) - Math.cos(i * 0.01));
 }
 
+const {
+  SciChartSurface,
+  NumericAxis,
+  FastLineRenderableSeries,
+  XyDataSeries,
+  SciChartJsNavyTheme,
+  Rect,
+  ECoordinateMode,
+  ZoomPanModifier,
+  ZoomExtentsModifier,
+  MouseWheelZoomModifier,
+  BoxAnnotation,
+  NumberRange,
+  generateGuid,
+} = SciChart;
+
+// or, for npm, import { SciChartSurface, ... } from "scichart"
+
+function createSubChartContainer(sciChartSurface, subChartOptions) {
+  // Create the subChartContainer HTML and add to the DOM
+  const container = document.createElement("div");
+  container.id = generateGuid(); // generateGuid imported from SciChart
+  container.style.position = "absolute";
+  container.style.border = "1px solid SteelBlue";
+  document.body.appendChild(container);
+
+  // Create a top bar header HTML element for the subchart
+  const div = document.createElement("div");
+  div.style.pointerEvents = "all";
+  div.style.position = "absolute";
+  div.style.top = "0";
+  div.style.width = "100%";
+  div.style.height = "30px";
+  div.style.backgroundColor = "#4682b477";
+  // className is required to specify that this is a top-section bar, to be positioned outside the chart
+  // even if this class isn't used or specified in the DOM.
+  // Default available options are "top-section", "bottom-section", "left-section", "right-section"
+  div.className = "top-section";
+  container.appendChild(div);
+
+  // Add a Sub-Charts to the main surface. This will display a rectangle showing the current zoomed in area on the parent chart
+  return sciChartSurface.addSubChart({
+    ...subChartOptions,
+    // Specify the subChartContainer
+    subChartContainerId: container.id,
+  });
+}
+
 async function simpleSubChart(divElementId) {
-  // Demonstrates how to use the Sub-Charts API to create child charts in a parent chart
-  const {
-    SciChartSurface,
-    NumericAxis,
-    FastLineRenderableSeries,
-    XyDataSeries,
-    SciChartJsNavyTheme,
-    Rect,
-    ECoordinateMode,
-    ZoomPanModifier,
-    ZoomExtentsModifier,
-    MouseWheelZoomModifier,
-    BoxAnnotation,
-    NumberRange,
-  } = SciChart;
-
-  // or, for npm, import { SciChartSurface, ... } from "scichart"
-
   // Create a parent (regular) SciChartSurface which will contain the sub-chart
   const { wasmContext, sciChartSurface } = await SciChartSurface.create(
     divElementId,
@@ -36,27 +66,14 @@ async function simpleSubChart(divElementId) {
   sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
   sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
 
-  // #region ExampleA
-  // Add a Sub-Charts to the main surface. This will display a rectangle showing the current zoomed in area on the parent chart
-  const subChart1 = sciChartSurface.addSubChart({
+  const subChart1 = createSubChartContainer(sciChartSurface, {
     position: new Rect(0.1, 0.1, 0.6, 0.4),
     isTransparent: false,
     isVisible: true,
     coordinateMode: ECoordinateMode.Relative,
     title: "SubChart with HTML Elements",
     titleStyle: { fontSize: 16, color: "#eeeeee77" },
-    // Specify the subChartContainer for extra HTML elements
-    // These will be positioned by SciChartSubSurface
-    // This property accepts a string or an HTMLDivElement
-    subChartContainerId: "sub-chart-container-id-1",
   });
-
-  // specify class names of section elements within the sub-chart container
-  subChart1.topSectionClass = "top-section";
-  subChart1.leftSectionClass = "left-section";
-  subChart1.bottomSectionClass = "bottom-section";
-  subChart1.rightSectionClass = "right-section";
-  // #endregion
 
   // Add x,y axis to the subchart
   subChart1.xAxes.add(new NumericAxis(wasmContext));
