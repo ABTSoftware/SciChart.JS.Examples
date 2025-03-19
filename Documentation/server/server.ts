8a687e8559b2860c1de496f6cca78d889ea9a719
SciChart DevTeam <devteam@scichart.com>
2025-03-19 10:55:27 +0000

    chore(examples): update scichart version to 3.5.744

--- Documentation/server/server.ts
+++ Documentation/server/server.ts
@@ -253,7 +253,7 @@ SciChartSurface.useWasmFromCDN();`
           },
           dependencies: {
             "parcel-bundler": "^1.6.1",
-            scichart: "3.5.742",
+            scichart: "3.5.744",
           },
           devDependencies: {
             "@babel/core": "7.2.0",
