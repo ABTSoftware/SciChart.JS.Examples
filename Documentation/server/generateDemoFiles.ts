ecdfcfeb0c5df86e325177974d7fc21718967663
Jim Risen <jim.dev@scichart.com>
2025-03-28 10:52:12 +0200

    chore(docs): update of project configurations; added separate ts configs for server, TS and JS doc snippets; compile TS snippets to ESM and serve lib bundle as ESM; updated dependencies; added prettier config;

--- Documentation/server/generateDemoFiles.ts
+++ Documentation/server/generateDemoFiles.ts
@@ -4,100 +4,100 @@ import * as fs from "fs";
 const baseDir = path.join(__dirname, "../src");
 
 class Entry {
-  name: string;
-  url: string;
-  entries: Entry[] = [];
-  isDemo: boolean = false;
-  hasDetails: boolean = false;
-  hasHtml: boolean = false;
-  hasCss: boolean = false;
-  isTS: boolean = false;
-  constructor(path: string) {
-    this.url = path.substring(baseDir.length).replaceAll("\\", "/");
-    this.name = path.substring(path.lastIndexOf("\\") + 1);
-  }
+    name: string;
+    url: string;
+    entries: Entry[] = [];
+    isDemo: boolean = false;
+    hasDetails: boolean = false;
+    hasHtml: boolean = false;
+    hasCss: boolean = false;
+    isTS: boolean = false;
+    constructor(path: string) {
+        this.url = path.substring(baseDir.length).replaceAll("\\", "/");
+        this.name = path.substring(path.lastIndexOf("\\") + 1);
+    }
 }
 
 var walk = function (dir: string, done: (err: any, entry?: Entry) => void) {
-  fs.readdir(dir, function (err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    const entry: Entry = new Entry(dir);
-    if (!pending) return done(null, entry);
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var pending = list.length;
+        const entry: Entry = new Entry(dir);
+        if (!pending) return done(null, entry);
 
-    list.forEach((file) => {
-      file = path.resolve(dir, file);
-      fs.stat(file, (err, stat) => {
-        if (stat && stat.isDirectory()) {
-          walk(file, (err, res) => {
-            if (res) {
-              entry.entries.push(res);
-            }
-            if (!--pending) done(null, entry);
-          });
-        } else {
-          const fileName = path.basename(file);
-          if (fileName === "demo.js") {
-            entry.isDemo = true;
-          } else if (fileName === "demo.html") {
-            entry.hasHtml = true;
-          } else if (fileName === "demo.css") {
-            entry.hasCss = true;
-          } else if (fileName === "demo.details") {
-            entry.hasDetails = true;
-          } else if (fileName === "demo.ts") {
-            entry.isTS = true;
-          }
-          if (!--pending) done(null, entry);
-        }
-      });
+        list.forEach(file => {
+            file = path.resolve(dir, file);
+            fs.stat(file, (err, stat) => {
+                if (stat && stat.isDirectory()) {
+                    walk(file, (err, res) => {
+                        if (res) {
+                            entry.entries.push(res);
+                        }
+                        if (!--pending) done(null, entry);
+                    });
+                } else {
+                    const fileName = path.basename(file);
+                    if (fileName === "demo.js") {
+                        entry.isDemo = true;
+                    } else if (fileName === "demo.html") {
+                        entry.hasHtml = true;
+                    } else if (fileName === "demo.css") {
+                        entry.hasCss = true;
+                    } else if (fileName === "demo.details") {
+                        entry.hasDetails = true;
+                    } else if (fileName === "demo.ts") {
+                        entry.isTS = true;
+                    }
+                    if (!--pending) done(null, entry);
+                }
+            });
+        });
     });
-  });
 };
 
 const makeDemoFiles = (entry: Entry) => {
-  if (!entry.isDemo) return;
-  if (entry.isTS) {
-    const jsPath = path.join(baseDir, entry.url, "demo.js");
-    console.log("Fixing js for ", entry.url);
-    fs.promises.readFile(jsPath, "utf8").then((js) => {
-      js = js.replace(
-        'Object.defineProperty(exports, "__esModule", { value: true });',
-        ""
-      );
-      js = js.replace(
-        'const scichart_1 = require("scichart");',
-        "const scichart_1 = SciChart;"
-      );
-      fs.promises.writeFile(jsPath, js);
-    });
-  }
-  if (!entry.hasHtml) {
-    console.log("Writing demo.html for ", entry.url);
-    const htmlPath = path.join(baseDir, entry.url, "demo.html");
-    fs.promises.writeFile(htmlPath, `<div id="scichart-root" ></div>`);
-  }
-  if (!entry.hasCss) {
-    console.log("Writing demo.css for ", entry.url);
-    const cssPath = path.join(baseDir, entry.url, "demo.css");
-    fs.promises.writeFile(
-      cssPath,
-      `body { margin: 0; }
+    if (!entry.isDemo) return;
+    if (entry.isTS) {
+        const jsPath = path.join(baseDir, entry.url, "demo.js");
+        console.log("Fixing js for ", entry.url);
+        // fs.promises.readFile(jsPath, "utf8").then((js) => {
+        //   js = js.replace(
+        //     'Object.defineProperty(exports, "__esModule", { value: true });',
+        //     ""
+        //   );
+        //   js = js.replace(
+        //     'const scichart_1 = require("scichart");',
+        //     "const scichart_1 = SciChart;"
+        //   );
+        //   fs.promises.writeFile(jsPath, js);
+        // });
+    }
+    if (!entry.hasHtml) {
+        console.log("Writing demo.html for ", entry.url);
+        const htmlPath = path.join(baseDir, entry.url, "demo.html");
+        fs.promises.writeFile(htmlPath, `<div id="scichart-root" ></div>`);
+    }
+    if (!entry.hasCss) {
+        console.log("Writing demo.css for ", entry.url);
+        const cssPath = path.join(baseDir, entry.url, "demo.css");
+        fs.promises.writeFile(
+            cssPath,
+            `body { margin: 0; }
 #scichart-root { width: 100%; height: 100vh; }`
-    );
-  }
-  if (!entry.hasDetails) {
-    console.log("Writing demo.details for ", entry.url);
-    const detailsPath = path.join(baseDir, entry.url, "demo.details");
-    const title =
-      "SciChart.js documentation snippet for " +
-      entry.url
-        .split("/")
-        .filter((v) => v.length > 0)
-        .join(" - ");
-    fs.promises.writeFile(
-      detailsPath,
-      `---
+        );
+    }
+    if (!entry.hasDetails) {
+        console.log("Writing demo.details for ", entry.url);
+        const detailsPath = path.join(baseDir, entry.url, "demo.details");
+        const title =
+            "SciChart.js documentation snippet for " +
+            entry.url
+                .split("/")
+                .filter(v => v.length > 0)
+                .join(" - ");
+        fs.promises.writeFile(
+            detailsPath,
+            `---
 name: ${title}
 description: A documentation snippet for SciChart.JS from scichart.com/javascript-chart-documentation.  Find out more about SciChart at scichart.com/javascript-chart-features
 authors:
@@ -108,39 +108,36 @@ normalize_css: no
 panel_js: 0
 panel_html: 0
 panel_css: 0`
-    );
-  }
+        );
+    }
 };
 
 const makeNav = (entry: Entry, categoryName: string) => {
-  if (entry.isDemo) {
-    makeDemoFiles(entry);
-  }
-  const category = categoryName ? `data-category=${categoryName}` : "";
-  let html = `<li ${category}><a href="${entry.url}">${entry.name}</a>
-`;
-  if (!entry.isDemo && entry.entries.length > 0) {
-    html += `<ul class="list">
-`;
-    for (const folder of entry.entries) {
-      console.log(`Folder ${folder.name} in ${entry.name}`);
-      html += makeNav(
-        folder,
-        (categoryName ? categoryName + "_" : "") + entry.name
-      );
+    if (entry.isDemo) {
+        makeDemoFiles(entry);
     }
-    html += `</ul>
- `;
-  }
-  html += `</li>
+    const category = categoryName ? `data-category=${categoryName}` : "";
+    let html = `<li ${category}><a href="${entry.url}">${entry.name}</a>
 `;
-  return html;
+    if (!entry.isDemo && entry.entries.length > 0) {
+        html += `<ul class="list">
+`;
+        for (const folder of entry.entries) {
+            console.log(`Folder ${folder.name} in ${entry.name}`);
+            html += makeNav(folder, (categoryName ? categoryName + "_" : "") + entry.name);
+        }
+        html += `</ul>
+ `;
+    }
+    html += `</li>
+`;
+    return html;
 };
 
 walk(baseDir, (err, entry) => {
-  if (!entry) return;
-  //console.log(JSON.stringify(entry, undefined, 2));
-  let html = `<style>
+    if (!entry) return;
+    //console.log(JSON.stringify(entry, undefined, 2));
+    let html = `<style>
         body {
             font-family: Arial, sans-serif;
         }
@@ -236,13 +233,13 @@ walk(baseDir, (err, entry) => {
         }
     </script>
 `;
-  html += `<ul id="list">
+    html += `<ul id="list">
 `;
-  for (const folder of entry.entries) {
-    html += makeNav(folder, undefined);
-  }
-  html += `</ul>
+    for (const folder of entry.entries) {
+        html += makeNav(folder, undefined);
+    }
+    html += `</ul>
 `;
-  console.log("Updating nav html");
-  fs.promises.writeFile("server/nav.html", html);
+    console.log("Updating nav html");
+    fs.promises.writeFile("server/nav.html", html);
 });
--- Documentation/static/common.js
+++ Documentation/static/common.js
@@ -1,14 +1,16 @@
+import * as SciChart from "scichart";
+
 SciChart.SciChartSurface.configure({
-  dataUrl: "/scichart2d.data",
-  wasmUrl: "/scichart2d.wasm",
+    dataUrl: "/scichart2d.data",
+    wasmUrl: "/scichart2d.wasm"
 });
 SciChart.SciChart3DSurface.configure({
-  dataUrl: "/scichart3d.data",
-  wasmUrl: "/scichart3d.wasm",
+    dataUrl: "/scichart3d.data",
+    wasmUrl: "/scichart3d.wasm"
 });
 SciChart.SciChartDefaults.performanceWarnings = false;
 if (window.location.hostname.includes("scichart")) {
-  SciChart.SciChartSurface.setRuntimeLicenseKey(
-    "J44gYXzq0TUKahynkWEVf4irTgPc4aP7OsLttSzJ1H6evYTUWwq+N7JLRAjXOhNKycb2maYK1TSuD6iXMK1z8Yr/E/WcJyGhFigwPRuo13DmB7Yyn8zQKa0DmLKLljQFD78uTLFq+XxmZlxMgTl+NsPGCT5ZD62GKAuvTtet8e6r4Psdw37X4GtbpYekhxc8s1aA4bMFanKOzUqnwqRy/CQ2BVbUyj0Q4Ndlvy/tggetQMLcXwbWphBl9dle4QdXxOXLbEdeiO7CO/XOouxEpl+664x6AULNv/Ug1Hdy9bkHLQ8GkN4gNs0q50maboHPLDp6984FtOjsmYB35UUqX5CoaFLoCep9YZ7tL0tZ1RGRci7YPN7mIrfNThWTnBHrRrlGYF9IE/o4kMrgXsoRKdpxmaLSbBxpngRXHJ81roFtxCpND7x4D+dYsaHd/93O1gOc7IFE/zLhqJGKA1sn2C9965E+NV2Mn4nB2dJjTmTci87mN284/8Mvs83dMRiQbI5R69AmDRTVRJb6T6vZWd2k6Z6URTAvymp0UXcYip9gYBwXtlYI3o8WQtapl3Q0AGbTKq7RYS7riylodcVTr3g9oHoLNApto2r4BOIVscXXZSGDWtJpaYZGojc0h3tcAwDby/R3UG5o94sMGC1vZT2WwHeGMkWPxx62Xpg8g+gpB4er91c0NhcGdeIbq0z8sLKIxsSeJ6OH02X/Dc2BXI580xfHsCatMVISJnNVbGvwHmrOXsWO3ZQwv49IU8xE5t7UnxdB3yuKH7VuHtV2khE="
-  );
+    SciChart.SciChartSurface.setRuntimeLicenseKey(
+        "J44gYXzq0TUKahynkWEVf4irTgPc4aP7OsLttSzJ1H6evYTUWwq+N7JLRAjXOhNKycb2maYK1TSuD6iXMK1z8Yr/E/WcJyGhFigwPRuo13DmB7Yyn8zQKa0DmLKLljQFD78uTLFq+XxmZlxMgTl+NsPGCT5ZD62GKAuvTtet8e6r4Psdw37X4GtbpYekhxc8s1aA4bMFanKOzUqnwqRy/CQ2BVbUyj0Q4Ndlvy/tggetQMLcXwbWphBl9dle4QdXxOXLbEdeiO7CO/XOouxEpl+664x6AULNv/Ug1Hdy9bkHLQ8GkN4gNs0q50maboHPLDp6984FtOjsmYB35UUqX5CoaFLoCep9YZ7tL0tZ1RGRci7YPN7mIrfNThWTnBHrRrlGYF9IE/o4kMrgXsoRKdpxmaLSbBxpngRXHJ81roFtxCpND7x4D+dYsaHd/93O1gOc7IFE/zLhqJGKA1sn2C9965E+NV2Mn4nB2dJjTmTci87mN284/8Mvs83dMRiQbI5R69AmDRTVRJb6T6vZWd2k6Z6URTAvymp0UXcYip9gYBwXtlYI3o8WQtapl3Q0AGbTKq7RYS7riylodcVTr3g9oHoLNApto2r4BOIVscXXZSGDWtJpaYZGojc0h3tcAwDby/R3UG5o94sMGC1vZT2WwHeGMkWPxx62Xpg8g+gpB4er91c0NhcGdeIbq0z8sLKIxsSeJ6OH02X/Dc2BXI580xfHsCatMVISJnNVbGvwHmrOXsWO3ZQwv49IU8xE5t7UnxdB3yuKH7VuHtV2khE="
+    );
 }
