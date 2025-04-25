a43058ebd931bd826f9a0d49bb07e42d57b83572
klishevich <busation@gmail.com>
2025-04-25 14:53:08 +0900

    chore(documantation): fix server version

--- Documentation/server/server.ts
+++ Documentation/server/server.ts
@@ -227,7 +227,7 @@ SciChartSurface.useWasmFromCDN();`
                     },
                     dependencies: {
                         "parcel-bundler": "^1.6.1",
-                        scichart: "${npmVersion}"
+                        scichart: "4.0.0-beta.717"
                     },
                     devDependencies: {
                         "@babel/core": "7.2.0",
