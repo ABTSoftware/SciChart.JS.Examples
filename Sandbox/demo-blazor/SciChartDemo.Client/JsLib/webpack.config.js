18441757ead61ae03e260d40da3ac9a45a99b3f2
antichaosdb <dave@antichaos.co.uk>
2025-04-30 17:06:36 +0100

    chore: Update various version references such that they no longer need to by updated on build

--- Sandbox/demo-blazor/SciChartDemo.Client/JsLib/webpack.config.js
+++ Sandbox/demo-blazor/SciChartDemo.Client/JsLib/webpack.config.js
@@ -2,28 +2,27 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 module.exports = {
-    mode: "development",
-    entry: {
-        demo: './src/demo.js',
-        sciChart: './src/wrapper.js'
-    },
-    module: {
-        rules: []
-    },
-    resolve: {
-        extensions: [".js"]
-    },
-    output: {
-        path: path.resolve(__dirname, '../wwwroot'),
-        filename: "[name].js",
-        library: "[name]"
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [ 
-                { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
-                { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" }
-            ]
-        })
-    ]
+  mode: "development",
+  entry: {
+    demo: "./src/demo.js",
+    sciChart: "./src/wrapper.js",
+  },
+  module: {
+    rules: [],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "../wwwroot"),
+    filename: "[name].js",
+    library: "[name]",
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
+      ],
+    }),
+  ],
 };
