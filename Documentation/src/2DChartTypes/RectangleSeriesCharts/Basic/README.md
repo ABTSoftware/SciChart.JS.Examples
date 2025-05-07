286fb125477b0485dbb8aac35761eb800eaa5317
Igor Cuckovic <cuckovic@gmail.com>
2025-05-07 20:07:39 +0200

    readme update

--- Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/README.md
+++ Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/README.md
@@ -1,1 +1,13 @@
-SciChart.js documentation snippet for 2DChartTypes - RectangleSeriesCharts - Basic
+# The Rectangle Series Type
+
+Rectangle Series can be created using the FastRectangleRenderableSeries type.
+
+Rectangle Series could be used for displaying
+
+- Gannt chart
+- Histogram
+- Range bars
+- Waterfall chart
+- Tree map
+- Linear gauges
+- Bar chart race
--- Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/demo.js
+++ Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/demo.js
@@ -18,16 +18,10 @@ async function basicRectangleSeriesChart(divElementId) {
     sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
     sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
 
-    // const xValues = [0, 6, 10, 17];
-    // const yValues = [0, 6, 2, 5];
-    // const x1Values = [5, 9, 15, 25];
-    // const y1Values = [5, 9, 8, 10];
-
-    const xValues = [0, 1.3, 2.8, 4.8, 7.4, 10.9, 15.1, 19.9, 24.9, 29.6, 33.8, 37.3, 39.9, 41.9, 43.5];
-    const yValues = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
-    const x1Values = [1.3, 2.8, 4.8, 7.4, 10.9, 15.1, 19.9, 24.9, 29.6, 33.8, 37.3, 39.9, 41.9, 43.5, 44.7];
-    const y1Values = [2.3, 2.5, 3.0, 3.6, 4.4, 5.2, 5.8, 6.0, 5.8, 5.2, 4.4, 3.6, 3.0, 2.5, 2.3];
-
+    const xValues = [0, 6, 10, 17];
+    const yValues = [0, 6, 2, 5];
+    const x1Values = [5, 9, 15, 25];
+    const y1Values = [5, 9, 8, 10];
 
     const rectangleSeries = new FastRectangleRenderableSeries(wasmContext, {
         dataSeries: new XyxyDataSeries(wasmContext, {
@@ -36,8 +30,8 @@ async function basicRectangleSeriesChart(divElementId) {
             x1Values,
             y1Values
         }),
-        columnXMode: EColumnMode.StartEnd,
-        columnYMode: EColumnYMode.TopBottom,
+        columnXMode: EColumnMode.StartEnd, // x, x1
+        columnYMode: EColumnYMode.TopBottom, // y, y1
         fill: "steelblue",
         stroke: "white",
         strokeThickness: 1,
