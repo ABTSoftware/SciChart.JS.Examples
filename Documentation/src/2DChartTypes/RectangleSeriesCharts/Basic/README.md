85faee26ed7981cf2cad8b9176ca7e0bb289a1c6
Igor Cuckovic <cuckovic@gmail.com>
2025-05-08 07:46:45 +0200

    readme update

--- Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/README.md
+++ Documentation/src/2DChartTypes/RectangleSeriesCharts/Basic/README.md
@@ -11,3 +11,44 @@ Rectangle Series could be used for displaying
 - Tree map
 - Linear gauges
 - Bar chart race
+
+## Properties
+
+- **columnXMode** - This determines how the x values and optional x1 values are interpreted
+
+    - EColumnMode.Mid
+    - EColumnMode.Start
+    - EColumnMode.MidWidth
+    - EColumnMode.StartWidth
+    - EColumnMode.StartEnd
+
+- **columnYMode** - This determines how the y values and optional y1 values are interpreted
+
+    - EColumnYMode.TopBottom
+    - EColumnYMode.TopHeight
+    - EColumnYMode.CenterHeight
+
+- **dataPointWidth** - Sets a value used to calculate the width of rectangles in X direction. By default the value is treated as data range, since rectangle series do not tend to be evenly spaced. To specify if the value should be treated as relative, absolute, or based on range use dataPointWidthMode Note that Absolute mode does not work well with autoRange due to circularity between the range calculation and the axis layout.
+
+- **dataPointWidthMode** - Sets the mode which determines how dataPointWidth in X direction is interpreted. Available values are EDataPointWidthMode. Default Relative.
+
+    - EDataPointWidthMode.Range - Interprets Data Point Width as the x data range per column. This is useful if you are plotting sparse columns on a NumericAxis
+    - EDataPointWidthMode.Absolute - Interprets Data Point Width as an absolute pixel value
+    - EDataPointWidthMode.Relative - Interprets Data Point Width as a relative to the full width which is axis length / number of columns. This assumes that there are no gaps in the data. If you are plotting sparse columns on a NumericAxis, consider Range mode
+
+- **stroke** - A Stroke for lines, outlines and edges of this RenderableSeries. Acceptable values include RGB format e.g. #FF0000, RGBA format e.g. #FF000077`` and RGBA format e.g. rgba(255,0,0,0.5)
+- **strokeThickness** - The Stroke Thickness for lines, outlines and edges of this RenderableSeries
+- **fill** - The column fill as an HTML color code
+- **opacity** - An Opacity factor of the Series that controls its semi-transparency level, where value 1 means the Series is opaque; 0 - transparent.
+- **defaultY1** - Sets a common y1 value for all rectangles if y1Values are not provided. Default 0
+- **customTextureOptions** - Options that creates a custom texture brush
+- **dataLabels** - Options to pass to the DataLabelProvider. Set a style with font and size to enable per-point text for this series.
+
+    ```javascript
+        dataLabels: {
+            style: {
+                fontSize: 16
+            },
+            color: "black"
+        }
+    ```
---
+++ Documentation/src/2DChartTypes/RectangleSeriesCharts/CustomTextureSupport/demo.ts
@@ -0,0 +1,119 @@
+import {
+    EColumnMode,
+    EColumnYMode,
+    SciChartSurface,
+    NumericAxis,
+    SciChartJsNavyTheme,
+    FastRectangleRenderableSeries,
+    XyxyDataSeries,
+    ICustomTextureOptions,
+    applyOpacityToHtmlColor
+} from "scichart";
+
+class StickFigureTextureOptions implements ICustomTextureOptions {
+    isPerPrimitive?: boolean = false;
+    options: { stroke: string };
+    textureHeight: number = 32;
+    textureWidth: number = 16;
+    repeat = true;
+
+    public constructor(options: { stroke: string }) {
+        this.options = options;
+    }
+
+    public createTexture(
+        context: CanvasRenderingContext2D,
+        options: { fill: string; opacity: number } & { stroke: string }
+    ) {
+        console.log("!!!", options);
+        context.fillStyle = applyOpacityToHtmlColor(options.fill, options.opacity);
+        context.fillRect(0, 0, this.textureWidth, this.textureHeight);
+        context.strokeStyle = applyOpacityToHtmlColor(options.stroke, options.opacity);
+
+        // Set up transformation: move to center, rotate, move back
+        context.translate(this.textureWidth / 2, this.textureHeight / 2);
+        //   context.rotate(Math.PI); // 180 degrees
+        context.translate(-this.textureWidth / 2, -this.textureHeight / 2);
+
+        // Proportional values
+        const centerX = this.textureWidth / 2;
+        const headRadius = Math.min(this.textureWidth, this.textureHeight) * 0.16; // 16% of smaller dimension
+        const headY = this.textureHeight * 0.25;
+        const bodyTopY = headY + headRadius;
+        const bodyBottomY = this.textureHeight * 0.63;
+        const armY = bodyTopY + this.textureHeight * 0.06;
+        const armSpan = this.textureWidth * 0.38; // arms reach out 19% each side
+        const legY = bodyBottomY;
+        const legSpan = this.textureWidth * 0.25; // legs out 12.5% each side
+        const legBottomY = this.textureHeight * 0.97;
+
+        // Head
+        context.beginPath();
+        context.arc(centerX, headY, headRadius, 0, Math.PI * 2);
+        context.stroke();
+
+        // Body
+        context.beginPath();
+        context.moveTo(centerX, bodyTopY);
+        context.lineTo(centerX, bodyBottomY);
+        context.stroke();
+
+        // Left Arm
+        context.beginPath();
+        context.moveTo(centerX, armY);
+        context.lineTo(centerX - armSpan, armY + this.textureHeight * 0.09);
+        context.stroke();
+
+        // Right Arm
+        context.beginPath();
+        context.moveTo(centerX, armY);
+        context.lineTo(centerX + armSpan, armY + this.textureHeight * 0.09);
+        context.stroke();
+
+        // Left Leg
+        context.beginPath();
+        context.moveTo(centerX, legY);
+        context.lineTo(centerX - legSpan, legBottomY);
+        context.stroke();
+
+        // Right Leg
+        context.beginPath();
+        context.moveTo(centerX, legY);
+        context.lineTo(centerX + legSpan, legBottomY);
+        context.stroke();
+    }
+}
+
+async function rectangleSeriesTexture(divElementId) {
+    const { wasmContext, sciChartSurface } = await SciChartSurface.create(divElementId, {
+        theme: new SciChartJsNavyTheme()
+    });
+
+    sciChartSurface.xAxes.add(new NumericAxis(wasmContext));
+    sciChartSurface.yAxes.add(new NumericAxis(wasmContext));
+
+    const xValues = [0, 6, 10, 17];
+    const yValues = [0, 6, 2, 5];
+    const x1Values = [5, 9, 15, 25];
+    const y1Values = [5, 9, 8, 10];
+
+    const rectangleSeries = new FastRectangleRenderableSeries(wasmContext, {
+        dataSeries: new XyxyDataSeries(wasmContext, {
+            xValues,
+            yValues,
+            x1Values,
+            y1Values
+        }),
+        columnXMode: EColumnMode.StartEnd, // x, x1
+        columnYMode: EColumnYMode.TopBottom, // y, y1
+        customTextureOptions: new StickFigureTextureOptions({ stroke: "black" }),
+        fill: "white",
+        stroke: "steelblue",
+        strokeThickness: 1,
+        opacity: 1
+    });
+
+    sciChartSurface.renderableSeries.add(rectangleSeries);
+}
+
+rectangleSeriesTexture("scichart-root");
