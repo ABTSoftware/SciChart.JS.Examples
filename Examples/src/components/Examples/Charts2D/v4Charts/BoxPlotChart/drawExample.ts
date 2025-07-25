227de7cd201d6e66b4eaeb8913587020447f2e04
antichaosdb <dave@antichaos.co.uk>
2025-07-25 17:15:58 +0100

    feat(Examples): Simplified and updated styles for box plot

--- Examples/src/components/Examples/Charts2D/v4Charts/BoxPlotChart/drawExample.ts
+++ Examples/src/components/Examples/Charts2D/v4Charts/BoxPlotChart/drawExample.ts
@@ -12,16 +12,10 @@ import {
     Rect,
     SciChartSubSurface,
     SciChartSurface,
+    ZoomExtentsModifier,
+    ZoomPanModifier,
 } from "scichart";
 import { appTheme } from "../../../theme";
-
-const CATEGORY_AXIS_OPTIONS: ICategoryAxisOptions = {
-    labelFormat: ENumericFormat.Decimal,
-    labelPrecision: 0,
-    cursorLabelFormat: ENumericFormat.Decimal,
-    cursorLabelPrecision: 0,
-    growBy: new NumberRange(0.05, 0.05),
-};
 
 const NUMERIC_AXIS_OPTIONS = {
     axisAlignment: EAxisAlignment.Left,
@@ -37,32 +31,36 @@ export const drawExample = async (rootElement: string | HTMLDivElement) => {
 
     // 1st sub-surface
     const sub1 = SciChartSubSurface.createSubSurface(sciChartSurface, {
-        position: new Rect(0, 0, 0.5, 0.5),
+        position: new Rect(0, 0, 0.5, 1),
         theme: appTheme.SciChartJsTheme,
     });
 
     sub1.xAxes.add(
         new CategoryAxis(wasmContext, {
-            ...CATEGORY_AXIS_OPTIONS,
-            labels: ["P1", "P2", "P3"],
+            growBy: new NumberRange(0.05, 0.05),
+            labels: ["P1", "P2", "P3", "P4", "P5", "P6"],
             axisAlignment: EAxisAlignment.Left,
         })
     );
     sub1.yAxes.add(
         new NumericAxis(wasmContext, {
-            ...NUMERIC_AXIS_OPTIONS,
             axisAlignment: EAxisAlignment.Bottom,
+            growBy: new NumberRange(0.05, 0.05),
+            autoRange: EAutoRange.Once,
+            flippedCoordinates: true,
+            labelFormat: ENumericFormat.Decimal,
+            labelPrecision: 0,
         })
     );
 
     const boxSeries1 = new FastBoxPlotRenderableSeries(wasmContext, {
         dataSeries: new BoxPlotDataSeries(wasmContext, {
-            xValues: [1, 2, 3],
-            minimumValues: [0, 1, 0.5],
-            maximumValues: [10, 9, 9.5],
-            medianValues: [4.5, 5.5, 5],
-            lowerQuartileValues: [3, 4, 3.5],
-            upperQuartileValues: [7, 6, 6.5],
+            xValues: [1, 2, 3, 4, 5, 6],
+            minimumValues: [0, 1, 0.5, 0, 1, 0.5],
+            maximumValues: [10, 9, 9.5, 10, 9, 9.5],
+            medianValues: [4.5, 5.5, 5, 4.5, 5.5, 5],
+            lowerQuartileValues: [3, 4, 3.5, 3, 4, 3.5],
+            upperQuartileValues: [7, 6, 6.5, 7, 6, 6.5],
         }),
         stroke: appTheme.MutedSkyBlue,
         fill: appTheme.MutedSkyBlue + "66",
@@ -76,170 +74,73 @@ export const drawExample = async (rootElement: string | HTMLDivElement) => {
             strokeDashArray: [5, 5],
         },
         cap: {
-            stroke: appTheme.MutedSkyBlue,
+            stroke: appTheme.MutedOrange,
             strokeThickness: 2,
             dataPointWidth: 0.5,
         },
         medianLine: {
-            stroke: appTheme.MutedSkyBlue,
-            strokeThickness: 4,
+            stroke: appTheme.MutedOrange,
+            strokeThickness: 2,
         },
     });
     sub1.renderableSeries.add(boxSeries1);
 
     // 2nd sub-surface
     const sub2 = SciChartSubSurface.createSubSurface(sciChartSurface, {
-        position: new Rect(0.5, 0, 0.5, 0.5),
+        position: new Rect(0.5, 0, 0.5, 1),
         theme: appTheme.SciChartJsTheme,
     });
 
     sub2.xAxes.add(
-        new CategoryAxis(wasmContext, {
-            labels: ["Box1", "Box2", "Box3", "Box4", "Box5"],
-            ...CATEGORY_AXIS_OPTIONS,
+        new NumericAxis(wasmContext, {
+            growBy: new NumberRange(0.1, 0.1),
+            autoRange: EAutoRange.Once,
+            labelFormat: ENumericFormat.Decimal,
+            labelPrecision: 0,
         })
     );
     sub2.yAxes.add(
         new NumericAxis(wasmContext, {
-            ...NUMERIC_AXIS_OPTIONS,
+            axisAlignment: EAxisAlignment.Left,
+            growBy: new NumberRange(0.05, 0.05),
+            autoRange: EAutoRange.Once,
+            flippedCoordinates: false,
+            labelFormat: ENumericFormat.Decimal,
+            labelPrecision: 0,
         })
     );
 
     const boxSeries2 = new FastBoxPlotRenderableSeries(wasmContext, {
         dataSeries: new BoxPlotDataSeries(wasmContext, {
-            xValues: [1, 2, 3, 4, 5],
-            minimumValues: [0, 1, 0.5, 0, 1],
-            maximumValues: [10, 9, 9.5, 10, 9],
-            medianValues: [4.5, 5.5, 5, 4, 5],
-            lowerQuartileValues: [3, 4, 3.5, 3, 4],
-            upperQuartileValues: [7, 6, 6.5, 7, 6],
+            xValues: [1, 2, 5, 7.5, 12],
+            minimumValues: [0, 1, 2, 5, 7],
+            lowerQuartileValues: [1, 2, 3.5, 6, 9],
+            medianValues: [3, 2.8, 6, 7, 10],
+            upperQuartileValues: [4, 3, 8, 9, 11],
+            maximumValues: [5, 4, 9.5, 10, 13],
         }),
-        stroke: appTheme.VividOrange,
-        fill: appTheme.VividOrange + "66",
+        stroke: appTheme.MutedTeal,
+        fill: appTheme.MutedPurple + "66",
         strokeThickness: 1,
-        dataPointWidthMode: EDataPointWidthMode.Relative,
+        dataPointWidthMode: EDataPointWidthMode.Range,
         dataPointWidth: 0.6,
         opacity: 0.8,
         whiskers: {
-            stroke: appTheme.VividOrange,
-            strokeThickness: 2,
-            strokeDashArray: [5, 5],
+            stroke: appTheme.MutedTeal,
+            strokeThickness: 1,
         },
         cap: {
-            stroke: appTheme.VividOrange,
+            stroke: appTheme.MutedPurple,
             strokeThickness: 2,
             dataPointWidth: 0.5,
         },
         medianLine: {
-            stroke: appTheme.VividOrange,
-            strokeThickness: 4,
+            stroke: appTheme.MutedPurple,
+            strokeThickness: 2,
         },
     });
+    sub2.chartModifiers.add(new ZoomPanModifier(), new ZoomExtentsModifier());
     sub2.renderableSeries.add(boxSeries2);
-
-    // 3rd sub-surface
-    const sub3 = SciChartSubSurface.createSubSurface(sciChartSurface, {
-        position: new Rect(0, 0.5, 0.5, 0.5),
-        theme: appTheme.SciChartJsTheme,
-    });
-
-    sub3.xAxes.add(
-        new CategoryAxis(wasmContext, {
-            labels: ["A", "B", "C"],
-            ...CATEGORY_AXIS_OPTIONS,
-        })
-    );
-    sub3.yAxes.add(
-        new NumericAxis(wasmContext, {
-            ...NUMERIC_AXIS_OPTIONS,
-        })
-    );
-
-    const boxSeries3 = new FastBoxPlotRenderableSeries(wasmContext, {
-        dataSeries: new BoxPlotDataSeries(wasmContext, {
-            xValues: [4, 5, 6],
-            minimumValues: [0, 1, 0.5],
-            maximumValues: [10, 9, 9.5],
-            medianValues: [4.5, 5.5, 5],
-            lowerQuartileValues: [3, 4, 3.5],
-            upperQuartileValues: [7, 6, 6.5],
-        }),
-        stroke: appTheme.VividPink,
-        fill: appTheme.VividPink + "66",
-        strokeThickness: 2,
-        dataPointWidthMode: EDataPointWidthMode.Relative,
-        dataPointWidth: 0.5,
-        opacity: 0.8,
-        whiskers: {
-            stroke: appTheme.VividPink,
-            strokeThickness: 2,
-            strokeDashArray: [5, 5],
-        },
-        cap: {
-            stroke: appTheme.VividPink,
-            strokeThickness: 2,
-            dataPointWidth: 0.3,
-        },
-        medianLine: {
-            stroke: appTheme.VividPink,
-            strokeThickness: 4,
-        },
-    });
-    sub3.renderableSeries.add(boxSeries3);
-
-    // 4th sub-surface
-    const sub4 = SciChartSubSurface.createSubSurface(sciChartSurface, {
-        position: new Rect(0.5, 0.5, 0.5, 0.5),
-        theme: appTheme.SciChartJsTheme,
-    });
-
-    sub4.xAxes.add(
-        new CategoryAxis(wasmContext, {
-            ...CATEGORY_AXIS_OPTIONS,
-            labels: ["1st", "2nd", "3rd", "4th"],
-            axisAlignment: EAxisAlignment.Left,
-            flippedCoordinates: true,
-        })
-    );
-    sub4.yAxes.add(
-        new NumericAxis(wasmContext, {
-            ...NUMERIC_AXIS_OPTIONS,
-            axisAlignment: EAxisAlignment.Bottom,
-            flippedCoordinates: true,
-        })
-    );
-
-    const boxSeries4 = new FastBoxPlotRenderableSeries(wasmContext, {
-        dataSeries: new BoxPlotDataSeries(wasmContext, {
-            xValues: [1, 2, 3, 4],
-            minimumValues: [0, 1, 0.5, 0],
-            maximumValues: [10, 9, 9.5, 10],
-            medianValues: [4.5, 5.5, 5, 4],
-            lowerQuartileValues: [3, 4, 3.5, 3],
-            upperQuartileValues: [7, 6, 6.5, 7],
-        }),
-        stroke: appTheme.VividTeal,
-        fill: appTheme.VividTeal + "66",
-        strokeThickness: 2,
-        dataPointWidthMode: EDataPointWidthMode.Relative,
-        dataPointWidth: 0.5,
-        opacity: 0.8,
-        whiskers: {
-            stroke: appTheme.VividTeal,
-            strokeThickness: 2,
-            strokeDashArray: [5, 5],
-        },
-        cap: {
-            stroke: appTheme.VividTeal,
-            strokeThickness: 2,
-            dataPointWidth: 0.2,
-        },
-        medianLine: {
-            stroke: appTheme.VividTeal,
-            strokeThickness: 4,
-        },
-    });
-    sub4.renderableSeries.add(boxSeries4);
 
     return { sciChartSurface, wasmContext };
 };
