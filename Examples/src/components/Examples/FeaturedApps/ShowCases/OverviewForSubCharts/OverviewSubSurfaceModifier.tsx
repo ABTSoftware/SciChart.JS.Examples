91a21c19e10d43aee8922e1cfd8327255280fc1c
Jim Risen <jim.dev@scichart.com>
2025-12-01 15:17:22 +0200

    chore(examples): SCJS-2251 OverviewModifier suggestions draft

--- Examples/src/components/Examples/FeaturedApps/ShowCases/OverviewForSubCharts/OverviewSubSurfaceModifier.tsx
+++ Examples/src/components/Examples/FeaturedApps/ShowCases/OverviewForSubCharts/OverviewSubSurfaceModifier.tsx
@@ -15,18 +15,52 @@ import {
     SciChartSubSurface,
     IOverviewOptions,
     ISciChartSubSurface,
+    ChartModifierBase2D,
+    EModifierType,
+    TSciChart,
+    AxisBase2D,
 } from "scichart";
-import { CustomChartModifier2D } from "scichart";
 
 export interface IOverviewSubSurfaceModifierOptions
     extends IChartModifierBaseOptions,
         IOverviewOptions,
         I2DSubSurfaceOptions {}
 
-/** A Modifier that adds an overview as subsurface.
- * The series to display are aggregated from other subsurfaces on the chart
- */
-export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
+const defaultTransform = (
+    rendSeries: IRenderableSeries,
+    // TODO create a separate type for contextParams
+    contextParams: {
+        wasmContext: TSciChart;
+        originalXAxis: AxisBase2D;
+        originalYAxis: AxisBase2D;
+        overviewXAxis: AxisBase2D;
+        overviewYAxis: AxisBase2D;
+        overviewSurface: SciChartSubSurface;
+    }
+) => {
+    const { wasmContext, originalXAxis, originalYAxis, overviewXAxis, overviewYAxis } = contextParams;
+    // TODO this check might have sense only when mapping series from the main surface, but probably unnecessary when dealing with subcharts on OverviewSSSsubSurfaceModifier
+
+    // return undefined to skip
+    if (rendSeries.xAxis?.id !== originalXAxis.id || rendSeries.yAxis?.id !== originalYAxis.id) {
+        return undefined;
+    }
+    // clone the series using builder api
+    const [overviewSeries] = buildSeries(wasmContext, rendSeries.toJSON(true));
+
+    // TODO (for later) check if deleting data series here is still necessary
+    overviewSeries.dataSeries.delete();
+    overviewSeries.dataSeries = rendSeries.dataSeries;
+    overviewSeries.xAxisId = overviewXAxis.id;
+    overviewSeries.yAxisId = overviewYAxis.id;
+    return overviewSeries;
+};
+
+// TODO move OverviewModifier into a separate file
+/** Basic Overview that maps series from main surface only */
+class OverviewModifier extends ChartModifierBase2D {
+    // TODO add new value to the EModifierType enum
+    readonly type: EModifierType = "OverviewModifier" as EModifierType;
     protected options: IOverviewSubSurfaceModifierOptions;
     protected overviewSurface: SciChartSubSurface;
 
@@ -41,23 +75,36 @@ export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
         this.overviewSurface = this.createOverview();
     }
 
-    public override onAttachSubSurface(subChart: ISciChartSubSurface): void {
-        console.log("onAttachSubSurface", subChart.id);
-        if (subChart.id === "overviewSubSurface") {
-            return;
-        }
+    onDetach(): void {
+        this.overviewSurface.delete();
+    }
 
-        subChart.renderableSeries.collectionChanged.subscribe((args) => {
-            if (this.overviewSurface) {
-                const newMappedSeries = args
-                    .getNewItems()
-                    // @ts-ignore
-                    .map(this.options.transformRenderableSeries)
-                    .filter((rendSeries: IRenderableSeries) => rendSeries);
+    onAttachSeries(rs: IRenderableSeries): void {
+        // TODO run transform function on the series and attach the resulting series to the overview
+        // const mappedSeries = this.mapTargetRenderableSeriesToOverview(rs, {...});
+        // if (mappedSeries) {
+        //     this.overviewSurface.renderableSeries.add(mappedSeries)
+        // }
+    }
 
-                this.overviewSurface.renderableSeries.add(...newMappedSeries);
-                // this.overviewSurface.zoomExtents();
-            }
+    onDetachSeries(rs: IRenderableSeries): void {
+        const mappedSeries = this.overviewSurface.renderableSeries.getById(rs.id);
+        this.overviewSurface.renderableSeries.remove(mappedSeries, true);
+    }
+
+    protected getTargetRenderableSeries() {
+        return this.parentSurface.renderableSeries.asArray();
+    }
+
+    protected mapTargetRenderableSeriesToOverview(
+        targetSeriesEntry: IRenderableSeries,
+        contextParams: any // TODO specify proper type, refer to params for defaultTransform
+    ) {
+        const transform = this.options?.transformRenderableSeries ?? defaultTransform;
+        return transform(targetSeriesEntry, {
+            overviewSurface: this.overviewSurface,
+            // TODO pass these from createOverview to mapTargetRenderableSeriesToOverview
+            ...contextParams,
         });
     }
 
@@ -68,6 +115,7 @@ export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
 
         const sciChartSurface = SciChartSubSurface.createSubSurface(this.parentSurface, options);
 
+        // TODO extract axis mapping into a separate method (e.g. mapAxes(originalXAxis, originalYAxis) => {overviewXAxis, overviewYAxis})
         const mainAxisId = options?.mainAxisId;
         const secondaryAxisId = options?.secondaryAxisId;
 
@@ -117,33 +165,21 @@ export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
             : originalXAxis.type === EAxisType.LogarithmicAxis
             ? new LogarithmicAxis(wasmContext, yAxisOptions)
             : new NumericAxis(wasmContext, yAxisOptions);
+        //
 
         const mainOverviewAxis = originalMainAxis.isXAxis ? xAxis : yAxis;
 
-        const defaultTransform = (rendSeries: IRenderableSeries) => {
-            // return undefined to skip
-            if (rendSeries.xAxis?.id !== originalXAxis.id || rendSeries.yAxis?.id !== originalYAxis.id) {
-                return undefined;
-            }
-            // clone the series using builder api
-            const [overviewSeries] = buildSeries(wasmContext, rendSeries.toJSON(true));
-            overviewSeries.dataSeries.delete();
-            overviewSeries.dataSeries = rendSeries.dataSeries;
-            overviewSeries.xAxisId = xAxis.id;
-            overviewSeries.yAxisId = yAxis.id;
-            return overviewSeries;
-        };
-
-        // Get series from main surface and all subcharts
-        const mainSurfaceSeries = originalSciChartSurface.renderableSeries.asArray();
-        const subChartSeries = originalSciChartSurface.subCharts.flatMap((subChart) =>
-            subChart.renderableSeries.asArray()
-        );
-        const existingSeries = [...mainSurfaceSeries, ...subChartSeries];
-
-        const renderableSeries = existingSeries
-            // @ts-ignore
-            .map(options?.transformRenderableSeries ?? defaultTransform)
+        const renderableSeries = this.getTargetRenderableSeries()
+            .map((rs) =>
+                this.mapTargetRenderableSeriesToOverview(rs, {
+                    // TODO pass required args
+                    originalXAxis,
+                    originalYAxis,
+                    overviewSurface: this.overviewSurface,
+                    overviewXAxis: xAxis,
+                    overviewYAxis: yAxis,
+                })
+            )
             .filter((rendSeries: IRenderableSeries) => rendSeries);
 
         sciChartSurface.xAxes.add(xAxis);
@@ -155,6 +191,7 @@ export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
         rangeSelectionModifier.xAxisId = xAxis.id;
         rangeSelectionModifier.yAxisId = yAxis.id;
 
+        // TODO extract visible range subscriptions into a separate method - subscribeToVisibleRangeUpdates(). This whole block from here
         rangeSelectionModifier.onSelectedAreaChanged = (selectedRange: NumberRange) => {
             if (!selectedRange.equals(originalMainAxis.visibleRange)) {
                 originalMainAxis.setVisibleRangeWithLimits(selectedRange);
@@ -186,7 +223,51 @@ export class OverviewSubSurfaceModifier extends CustomChartModifier2D {
                 rangeSelectionModifier.selectedArea = updatedSelectedRange;
             }
         });
+        // to here, would be different in OverviewModifier compared to OverviewSubSurfaceModifier
 
         return sciChartSurface;
     }
+}
+
+/** A Modifier that adds an overview as subsurface.
+ * The series to display are aggregated from other subsurfaces on the chart
+ */
+export class OverviewSubSurfaceModifier extends OverviewModifier {
+    public override onAttachSubSurface(subChart: ISciChartSubSurface): void {
+        console.log("onAttachSubSurface", subChart.id);
+        if (subChart.id === "overviewSubSurface") {
+            return;
+        }
+
+        subChart.renderableSeries.collectionChanged.subscribe((args) => {
+            if (this.overviewSurface) {
+                const newMappedSeries = args
+                    .getNewItems()
+                    // @ts-ignore
+                    .map(this.options.transformRenderableSeries)
+                    .filter((rendSeries: IRenderableSeries) => rendSeries);
+
+                this.overviewSurface.renderableSeries.add(...newMappedSeries);
+                // this.overviewSurface.zoomExtents();
+            }
+        });
+    }
+
+    onDetachSubSurface(subChart: ISciChartSubSurface): void {
+        // TODO, unsubscribe collectionChanged and remove corresponding series from overview (by Id)
+    }
+
+    // TODO considering that the OverviewModifier.createOverview would be refactored into more granular methods,
+    // here we would need only to do the relevant changes to them:
+    override getTargetRenderableSeries() {
+        return this.parentSurface.subCharts.flatMap((sc) => sc.renderableSeries.asArray());
+    }
+
+    // override mapTargetRenderableSeriesToOverview(targetSeriesEntry: IRenderableSeries, contextParams: any) {
+    //     // TODO maybe no need to override, just check if there are issues with axis ids
+    // }
+
+    // override subscribeToVisibleRangeUpdates() {
+    //     // TODO configure the two way binding between all subcharts and overview by using AxisSyncroniser
+    // }
 }
