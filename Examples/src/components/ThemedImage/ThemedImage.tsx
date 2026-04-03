9e2ad3d3d9e8bb7e86479adc362728cee0db527b
Vascool14 <andrei.vascul@lucaciu.ro>
2026-04-03 14:05:24 +0300

    fix(demo) - removed themedImage

--- Examples/src/components/ThemedImage/ThemedImage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ImgHTMLAttributes } from "react";
-
-type TThemedImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, "src"> & {
-    src: string;
-};
-
-export function ThemedImage({ src, onError, ...rest }: TThemedImageProps) {
-    return <img {...rest} src={src} onError={onError} />;
-}
